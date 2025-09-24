14470fc4c155b1dd1ac374e3c0279abf961872be
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-09-24 11:05:01 +0530

    odoomates

--- balance_sheet_report/ __manifest__.py
+++ balance_sheet_report/ __manifest__.py
@@ -1,6 +1,8 @@
 {
     'name': 'Balance Sheet Report',
     'version': '1.0',
+    'summary': 'Custom Balance Sheet Report',
+    'sequence': 10,
     'depends': ['account'],
     'data': [
         'report/balance_sheet_report.xml',

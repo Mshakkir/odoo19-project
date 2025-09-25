3baaa78000937e7fe5d68851bc42b74f944a5268
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-09-25 15:22:05 +0530

    add code

--- custom_sa_quotation/models/sale_order.py
+++ custom_sa_quotation/models/sale_order.py
@@ -3,13 +3,11 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    discount_amount = fields.Monetary(string="Discount", default=0.0)
-    freight_amount = fields.Monetary(string="Freight", default=0.0)
+    discount_amount = fields.Monetary(string="Discount", default=0.0, currency_field='currency_id')
+    freight_amount = fields.Monetary(string="Freight", default=0.0, currency_field='currency_id')
 
     @api.depends('order_line.price_total', 'discount_amount', 'freight_amount', 'order_line.tax_id')
     def _amount_all(self):
-        # Call super to preserve original computation
         super(SaleOrder, self)._amount_all()
         for order in self:
-            # Override amount_total to include discount and freight
             order.amount_total = order.amount_untaxed - order.discount_amount + order.freight_amount + order.amount_tax

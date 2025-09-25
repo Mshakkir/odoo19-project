435f6dc395ce267fd312e902a2e18c1b8c519811
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-09-25 16:16:04 +0530

    add code

--- custom_sa_quotation/models/sale_order.py
+++ custom_sa_quotation/models/sale_order.py
@@ -3,19 +3,17 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    discount_amount = fields.Monetary(string="Discount", default=0.0)
-    freight_amount = fields.Monetary(string="Freight", default=0.0)
+    discount_amount = fields.Monetary(string="Discount", default=0.0, currency_field='currency_id')
+    freight_amount = fields.Monetary(string="Freight", default=0.0, currency_field='currency_id')
 
-    @api.depends('order_line.price_total', 'order_line.tax_id', 'discount_amount', 'freight_amount')
+    @api.depends('order_line.price_subtotal', 'order_line.price_tax', 'discount_amount', 'freight_amount')
     def _amount_all(self):
         """
         Compute the amounts of the SO: amount_untaxed, amount_tax, amount_total.
         """
         for order in self:
-            amount_untaxed = amount_tax = 0.0
-            for line in order.order_line:
-                amount_untaxed += line.price_subtotal
-                amount_tax += line.price_tax
+            amount_untaxed = sum(line.price_subtotal for line in order.order_line)
+            amount_tax = sum(line.price_tax for line in order.order_line)
 
             # apply discount and freight
             amount_total = amount_untaxed + amount_tax - order.discount_amount + order.freight_amount

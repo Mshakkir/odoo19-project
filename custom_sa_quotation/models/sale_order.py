907718c223c734345c54a86b2799a736f9f8a144
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-09-25 14:20:37 +0530

    add code

--- custom_sa_quotation/models/sale_order.py
+++ custom_sa_quotation/models/sale_order.py
@@ -6,10 +6,17 @@ class SaleOrder(models.Model):
     discount_amount = fields.Monetary(string="Discount", default=0.0)
     freight_amount = fields.Monetary(string="Freight", default=0.0)
 
-    # Extend total computation
-    @api.depends('order_line.price_total', 'discount_amount', 'freight_amount')
-    def _compute_amount(self):
+    @api.depends('order_line.price_total', 'discount_amount', 'freight_amount', 'order_line.tax_id')
+    def _amount_all(self):
         for order in self:
-            untaxed = sum(line.price_total for line in order.order_line)
-            order.amount_untaxed = untaxed
-            order.amount_total = untaxed - order.discount_amount + order.freight_amount
+            untaxed_amount = sum(line.price_subtotal for line in order.order_line)
+            tax_amount = sum(line.price_tax for line in order.order_line)
+            order.update({
+                'amount_untaxed': untaxed_amount,
+                'amount_tax': tax_amount,
+                'amount_total': untaxed_amount - order.discount_amount + order.freight_amount + tax_amount,
+            })
+
+    @api.onchange('discount_amount', 'freight_amount')
+    def _onchange_discount_freight(self):
+        self._amount_all()

84db5a322364e27c40cd1310ba858d24d0895a12
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-09-26 17:02:44 +0530

    add code

--- my_invoice_custom/models/sale_order.py
+++ my_invoice_custom/models/sale_order.py
@@ -8,9 +8,7 @@ class AccountMove(models.Model):
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    discount = fields.Float(string="Discount")
-
     def _prepare_invoice(self):
-        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals = super()._prepare_invoice()
         invoice_vals['discount'] = self.discount
         return invoice_vals

59864de2e83ee9fa144eca63c058f5d863c6e736
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-09-27 12:49:10 +0530

    add code

--- custom_sa_quotation/models/sale_order.py
+++ custom_sa_quotation/models/sale_order.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = 'sale.order'
 
     discount_amount = fields.Monetary(
         string="Discount",

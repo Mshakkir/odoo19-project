fdbd4f8293426a8bbc87e7182ce7dab56d2f4a68
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-09-30 11:39:53 +0530

    add code

--- my_invoice_discount/models/account_move.py
+++ my_invoice_discount/models/account_move.py
@@ -1,19 +1,56 @@
+# from odoo import models, fields, api
+#
+# class AccountMove(models.Model):
+#     _inherit = 'account.move'
+#
+#     discount_amount = fields.Monetary(
+#         string="Discount Amount",
+#         currency_field='currency_id',
+#     )
+#     freight_amount = fields.Monetary(
+#         string="Freight Amount",
+#         currency_field='currency_id',
+#     )
+#
+#     @api.depends('line_ids.price_total', 'discount_amount', 'freight_amount')
+#     def _compute_amount(self):
+#         super(AccountMove, self)._compute_amount()
+#         for record in self:
+#             record.amount_total = record.amount_total - (record.discount_amount or 0.0) + (record.freight_amount or 0.0)
 from odoo import models, fields, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    discount_amount = fields.Monetary(
-        string="Discount Amount",
+    discount_type = fields.Selection([
+        ('amount', 'Fixed Amount'),
+        ('percent', 'Percentage'),
+    ], string="Discount Type", default='amount')
+
+    discount_value = fields.Float(string="Discount Value", default=0.0)
+    total_discount = fields.Monetary(
+        string="Discount",
         currency_field='currency_id',
+        compute="_compute_total_discount",
+        store=True,
     )
     freight_amount = fields.Monetary(
-        string="Freight Amount",
+        string="Freight",
         currency_field='currency_id',
     )
 
-    @api.depends('line_ids.price_total', 'discount_amount', 'freight_amount')
+    @api.depends('line_ids.price_total', 'discount_type', 'discount_value')
+    def _compute_total_discount(self):
+        for move in self:
+            if move.discount_type == 'percent':
+                move.total_discount = (move.amount_untaxed * move.discount_value) / 100
+            else:
+                move.total_discount = move.discount_value
+
+    @api.depends('line_ids.price_total', 'total_discount', 'freight_amount')
     def _compute_amount(self):
         super(AccountMove, self)._compute_amount()
         for record in self:
-            record.amount_total = record.amount_total - (record.discount_amount or 0.0) + (record.freight_amount or 0.0)
+            record.amount_total = (
+                record.amount_total - (record.total_discount or 0.0) + (record.freight_amount or 0.0)
+            )
--- my_invoice_discount/models/sale_order.py
+++ my_invoice_discount/models/sale_order.py
@@ -1,23 +1,63 @@
+# from odoo import models, fields, api
+#
+# class SaleOrder(models.Model):
+#     _inherit = 'sale.order'
+#
+#     discount_amount = fields.Monetary(
+#         string="Discount",
+#         default=0.0,
+#         currency_field='currency_id',
+#     )
+#     freight_amount = fields.Monetary(
+#         string="Freight",
+#         default=0.0,
+#         currency_field='currency_id',
+#     )
+#
+#     def _prepare_invoice(self):
+#         invoice_vals = super()._prepare_invoice()
+#         invoice_vals.update({
+#             'discount_amount': self.discount_amount,
+#             'freight_amount': self.freight_amount,
+#         })
+#         return invoice_vals
 from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    discount_amount = fields.Monetary(
-        string="Discount",
-        default=0.0,
-        currency_field='currency_id',
-    )
+    discount_type = fields.Selection([
+        ('amount', 'Fixed Amount'),
+        ('percent', 'Percentage'),
+    ], string="Discount Type", default='amount')
+
+    discount_value = fields.Float(string="Discount Value", default=0.0)
     freight_amount = fields.Monetary(
         string="Freight",
         default=0.0,
         currency_field='currency_id',
     )
+    total_discount = fields.Monetary(
+        string="Discount",
+        compute="_compute_total_discount",
+        store=True,
+        currency_field='currency_id'
+    )
+
+    @api.depends('discount_type', 'discount_value', 'amount_untaxed')
+    def _compute_total_discount(self):
+        for order in self:
+            if order.discount_type == 'percent':
+                order.total_discount = (order.amount_untaxed * order.discount_value) / 100
+            else:
+                order.total_discount = order.discount_value
 
     def _prepare_invoice(self):
         invoice_vals = super()._prepare_invoice()
         invoice_vals.update({
-            'discount_amount': self.discount_amount,
+            'discount_type': self.discount_type,
+            'discount_value': self.discount_value,
+            'total_discount': self.total_discount,
             'freight_amount': self.freight_amount,
         })
         return invoice_vals

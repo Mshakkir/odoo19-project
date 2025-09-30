381c2edc691d51356adf7c1e6f6cc0495bc7e7be
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-09-30 10:10:08 +0530

    add code

--- my_invoice_discount/models/sale_order.py
+++ my_invoice_discount/models/sale_order.py
@@ -1,23 +1,44 @@
+# from odoo import models, fields, api
+#
+# class SaleOrder(models.Model):
+#     _inherit = 'sale.order'
+#
+#     discount_amount = fields.Monetary(
+#         string="Discount",
+#         default=0.0,
+#         currency_field='currency_id',
+#     )
+#     freight_amount = fields.Monetary(
+#         string="Freight",
+#         default=0.0,
+#         currency_field='currency_id',
+#     )
+#
+#     def _prepare_invoice(self):
+#         invoice_vals = super()._prepare_invoice()
+#         invoice_vals.update({
+#             'discount_amount': self.discount_amount,
+#             'freight_amount': self.freight_amount,
+#         })
+#         return invoice_vals
 from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    discount_amount = fields.Monetary(
-        string="Discount",
-        default=0.0,
-        currency_field='currency_id',
+    discount_type = fields.Selection(
+        [('amount', 'Amount'), ('percent', 'Percentage')],
+        string="Discount Type",
+        default='amount'
     )
-    freight_amount = fields.Monetary(
-        string="Freight",
-        default=0.0,
-        currency_field='currency_id',
-    )
+    discount_value = fields.Float(string="Discount Value", default=0.0)
+    freight_amount = fields.Monetary(string="Freight", default=0.0, currency_field='currency_id')
 
     def _prepare_invoice(self):
         invoice_vals = super()._prepare_invoice()
         invoice_vals.update({
-            'discount_amount': self.discount_amount,
+            'discount_type': self.discount_type,
+            'discount_value': self.discount_value,
             'freight_amount': self.freight_amount,
         })
         return invoice_vals

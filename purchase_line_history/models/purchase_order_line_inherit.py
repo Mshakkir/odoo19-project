9785a22ab3948578e6b458a2947354528291566a
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-06 17:02:58 +0530

    custom

--- purchase_line_history/models/purchase_order_line_inherit.py
+++ purchase_line_history/models/purchase_order_line_inherit.py
@@ -3,17 +3,6 @@ from odoo import models, fields
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    def action_stock_history(self):
-        self.ensure_one()
-        return {
-            "name": "Stock Moves",
-            "type": "ir.actions.act_window",
-            "res_model": "stock.move",
-            "view_mode": "tree,form",
-            "domain": [("product_id", "=", self.product_id.id)],
-            "context": {"default_product_id": self.product_id.id},
-        }
-
     def action_sale_history(self):
         """ Open sale order lines related to this product """
         self.ensure_one()

1bd4be52e220fa727110a4d20b61a750619e8c8c
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-06 16:41:07 +0530

    custom

--- purchase_line_history/models/purchase_order_line_inherit.py
+++ purchase_line_history/models/purchase_order_line_inherit.py
@@ -4,14 +4,14 @@ class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     def action_stock_history(self):
-        """ Open stock moves related to this product """
         self.ensure_one()
         return {
-            'name': 'Stock History',
-            'type': 'ir.actions.act_window',
-            'res_model': 'stock.move',
-            'view_mode': 'tree,form',
-            'domain': [('product_id', '=', self.product_id.id)],
+            "name": "Stock Moves",
+            "type": "ir.actions.act_window",
+            "res_model": "stock.move",
+            "view_mode": "tree,form",
+            "domain": [("product_id", "=", self.product_id.id)],
+            "context": {"default_product_id": self.product_id.id},
         }
 
     def action_sale_history(self):

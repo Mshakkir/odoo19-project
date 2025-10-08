38657d4ebd9e093c4081e26223d5b40b3a7f81a7
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-08 15:28:47 +0530

    add code

---
+++ purchase_order_custom_report/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Custom Purchase Order Report',
+    'version': '1.0',
+    'depends': ['purchase', 'base'],
+    'data': [
+        'report/report.xml',
+        'report/report_purchase_order.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

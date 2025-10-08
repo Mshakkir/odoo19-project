f2fcd029287b199895cf71a779b0696466e99560
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-08 12:11:57 +0530

    custom

--- purchase_history_popup/models/purchase_history.py
+++ purchase_history_popup/models/purchase_history.py
@@ -4,12 +4,12 @@ class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     def open_purchase_history_popup(self):
+        # Sample action (replace with yours as needed)
         return {
             'type': 'ir.actions.act_window',
             'name': 'Purchase History',
-            'res_model': 'purchase.history.popup.wizard',
+            'res_model': 'purchase.history.popup.wizard',  # Or the wizard model's name you are using
             'view_mode': 'form',
-            'view_id': self.env.ref('purchase_history_popup.view_purchase_history_popup_wizard').id,
             'target': 'new',
             'context': {'active_id': self.id},
         }

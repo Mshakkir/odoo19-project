0d22f3a388416a28fc8d3b6ae4d16d274701ae89
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-09 11:38:00 +0530

    custom

--- purchase_rfq_history_tabs/models/purchase_order.py
+++ purchase_rfq_history_tabs/models/purchase_order.py
@@ -1,35 +1,20 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     stock_move_ids = fields.One2many(
-        'stock.move', compute='_compute_stock_moves', string='Stock Moves', readonly=True)
+        'stock.move', 'purchase_id', string='Stock Moves', readonly=True)
+
     sale_line_ids = fields.One2many(
-        'sale.order.line', compute='_compute_sale_lines', string='Sales Lines', readonly=True)
+        'sale.order.line', compute='_compute_sale_lines', string='Sale Lines', readonly=True)
+
     purchase_line_ids = fields.One2many(
-        'purchase.order.line', compute='_compute_purchase_lines', string='Purchase Lines', readonly=True)
+        'purchase.order.line', 'order_id', string='Purchase Lines', readonly=True)
 
-    @api.depends('order_line.product_id')
-    def _compute_stock_moves(self):
-        for order in self:
-            product_ids = order.order_line.mapped('product_id.id')
-            moves = self.env['stock.move'].search([('product_id', 'in', product_ids)])
-            order.stock_move_ids = moves
-
-    @api.depends('order_line.product_id')
     def _compute_sale_lines(self):
         for order in self:
-            product_ids = order.order_line.mapped('product_id.id')
-            sales = self.env['sale.order.line'].search([('product_id', 'in', product_ids)])
-            order.sale_line_ids = sales
-
-    @api.depends('order_line.product_id')
-    def _compute_purchase_lines(self):
-        for order in self:
-            product_ids = order.order_line.mapped('product_id.id')
-            purchases = self.env['purchase.order.line'].search([
-                ('product_id', 'in', product_ids),
-                ('order_id.state', '=', 'purchase'),
+            product_ids = order.order_line.mapped('product_id').ids
+            order.sale_line_ids = self.env['sale.order.line'].search([
+                ('product_id', 'in', product_ids)
             ])
-            order.purchase_line_ids = purchases

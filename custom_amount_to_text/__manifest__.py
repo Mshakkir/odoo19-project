8b5bf11bde33cd21aa81fc40b0d7618ce948b75e
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-10 15:53:03 +0530

    custom

--- custom_amount_to_text/__manifest__.py
+++ custom_amount_to_text/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Your Name",
     "depends": ["account"],
     "data": [
-        "views/account_move_views.xml",
+        # "views/account_move_views.xml",
     ],
     "installable": True,
     "application": False,

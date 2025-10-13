3f0fad7afe934bd74089016de2bac3b3ad6c9904
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-13 14:14:09 +0530

    add code

--- accounting_pdf_reports/report/report_tax.py
+++ accounting_pdf_reports/report/report_tax.py
@@ -1,6 +1,6 @@
 from odoo import api, models, _
 from odoo.exceptions import UserError
-
+from collections import defaultdict
 
 class ReportTax(models.AbstractModel):
     _name = 'report.accounting_pdf_reports.report_tax'
@@ -10,12 +10,27 @@ class ReportTax(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         if not data.get('form'):
             raise UserError(_("Form content is missing, this report cannot be printed."))
+        form = data.get('form') or {}
+        report_option = form.get('report_option', 'normal')
+        date_from = form.get('date_from')
+        date_to = form.get('date_to')
+        target_move = form.get('target_move', 'posted')
+
+        res_company = self.env.company
+
+        if report_option == 'normal':
+            lines = self.get_summary_lines(form)
+        else:
+            lines = self.get_detail_lines(form)
+
         return {
-            'data': data['form'],
-            'lines': self.get_lines(data.get('form')),
+            'data': form,
+            'lines': lines,
+            'res_company': res_company,
+            'report_option': report_option,
         }
 
-
+    # --- existing helper SQLs unchanged ---
     def _sql_from_amls_one(self):
         sql = """SELECT "account_move_line".tax_line_id, COALESCE(SUM("account_move_line".debit-"account_move_line".credit), 0)
                     FROM %s
@@ -31,7 +46,6 @@ class ReportTax(models.AbstractModel):
         return sql
 
     def _compute_from_amls(self, options, taxes):
-        #compute the tax amount
         sql = self._sql_from_amls_one()
         tables, where_clause, where_params = self.env['account.move.line']._query_get()
         query = sql % (tables, where_clause)
@@ -41,7 +55,6 @@ class ReportTax(models.AbstractModel):
             if result[0] in taxes:
                 taxes[result[0]]['tax'] = abs(result[1])
 
-        #compute the net amount
         sql2 = self._sql_from_amls_two()
         query = sql2 % (tables, where_clause)
         self.env.cr.execute(query, where_params)
@@ -50,23 +63,100 @@ class ReportTax(models.AbstractModel):
             if result[0] in taxes:
                 taxes[result[0]]['net'] = abs(result[1])
 
-    @api.model
-    def get_lines(self, options):
+    # --- summary (normal) lines: same as your get_lines() but renamed ---
+    def get_summary_lines(self, options):
         taxes = {}
         for tax in self.env['account.tax'].search([('type_tax_use', '!=', 'none')]):
+            # If tax has children, pick child items for break down (keep original logic)
             if tax.children_tax_ids:
                 for child in tax.children_tax_ids:
                     if child.type_tax_use != 'none':
                         continue
-                    taxes[child.id] = {'tax': 0, 'net': 0, 'name': child.name, 'type': tax.type_tax_use}
+                    taxes[child.id] = {'tax': 0, 'net': 0, 'name': child.name, 'type': tax.type_tax_use, 'tax_id': child.id}
             else:
-                taxes[tax.id] = {'tax': 0, 'net': 0, 'name': tax.name, 'type': tax.type_tax_use}
+                taxes[tax.id] = {'tax': 0, 'net': 0, 'name': tax.name, 'type': tax.type_tax_use, 'tax_id': tax.id}
+
         self.with_context(date_from=options['date_from'], date_to=options['date_to'],
                           state=options['target_move'],
                           strict_range=True)._compute_from_amls(options, taxes)
+
         groups = dict((tp, []) for tp in ['sale', 'purchase'])
         for tax in taxes.values():
-            # Include taxes that have either tax amount OR base (net) amount
             if tax['tax'] or tax['net']:
                 groups[tax['type']].append(tax)
+        return groups
+
+    # --- detail lines: return mapping by tax -> list of details (account_move_line level summary) ---
+    def get_detail_lines(self, options):
+        # We'll produce a dict: {'sale': [ {tax:..., details:[{account_name, partner, move, debit, credit, balance}, ...]}, ... ], 'purchase': [...] }
+        date_from = options.get('date_from')
+        date_to = options.get('date_to')
+        target_move = options.get('target_move', 'posted')
+
+        # Build domain for account.move.line using Odoo's _query_get
+        tables, where_clause, where_params = self.env['account.move.line']._query_get()
+        # Compose SQL to fetch move lines grouped by tax and account
+        # Note: this SQL will pick rows that are linked to a tax (via account_move_line_account_tax_rel)
+        sql = """
+            SELECT r.account_tax_id AS tax_id,
+                   aml.account_id,
+                   COALESCE(SUM(aml.debit),0) AS debit,
+                   COALESCE(SUM(aml.credit),0) AS credit
+            FROM %s aml
+            INNER JOIN account_move_line_account_tax_rel r ON (aml.id = r.account_move_line_id)
+            WHERE %s
+            GROUP BY r.account_tax_id, aml.account_id
+            ORDER BY r.account_tax_id
+        """ % (tables, where_clause)
+
+        self.env.cr.execute(sql, where_params)
+        rows = self.env.cr.fetchall()
+
+        # rows: [(tax_id, account_id, debit, credit), ...]
+        # Build dictionary
+        tax_map = {}
+        for tax_id, account_id, debit, credit in rows:
+            if tax_id not in tax_map:
+                tax_rec = self.env['account.tax'].browse(tax_id)
+                tax_map[tax_id] = {
+                    'tax_id': tax_id,
+                    'tax_name': tax_rec.name or '',
+                    'type': tax_rec.type_tax_use or '',
+                    'total_net': 0.0,
+                    'total_tax': 0.0,
+                    'details': []
+                }
+            account = self.env['account.account'].browse(account_id)
+            balance = float(debit or 0.0) - float(credit or 0.0)
+            tax_map[tax_id]['details'].append({
+                'account_id': account_id,
+                'account_code': account.code or '',
+                'account_name': account.name or '',
+                'debit': float(debit or 0.0),
+                'credit': float(credit or 0.0),
+                'balance': balance
+            })
+            tax_map[tax_id]['total_net'] += abs(balance)
+
+        # We also need tax amounts separately (compute using your existing SQLs)
+        taxes_data = {}
+        for tax in self.env['account.tax'].search([('type_tax_use','!=','none')]):
+            taxes_data[tax.id] = {'tax': 0.0, 'net': 0.0, 'name': tax.name, 'type': tax.type_tax_use}
+
+        # compute totals per tax (reuse compute)
+        self.with_context(date_from=options['date_from'], date_to=options['date_to'],
+                          state=options['target_move'],
+                          strict_range=True)._compute_from_amls(options, taxes_data)
+
+        # attach tax amount to our tax_map
+        for tax_id, d in tax_map.items():
+            d['total_tax'] = taxes_data.get(tax_id, {}).get('tax', 0.0)
+            d['total_net'] = taxes_data.get(tax_id, {}).get('net', d.get('total_net', 0.0))
+
+        # group by sale/purchase type
+        groups = dict((tp, []) for tp in ['sale', 'purchase'])
+        for tax_id, d in tax_map.items():
+            ttype = d.get('type') or 'sale'
+            groups.setdefault(ttype, []).append(d)
+
         return groups
--- accounting_pdf_reports/wizard/account_report.py
+++ accounting_pdf_reports/wizard/account_report.py
@@ -1,55 +1,36 @@
-from odoo import api, fields, models
+from odoo import models, api, fields
+from datetime import date
 
-
-class AccountingReport(models.TransientModel):
-    _name = "accounting.report"
+class AccountTaxReport(models.TransientModel):
+    _name = 'account.tax.report.wizard'
     _inherit = "account.common.report"
-    _description = "Accounting Report"
+    _description = 'Tax Report'
 
-    @api.model
-    def _get_account_report(self):
-        reports = []
-        if self.env.context.get('active_id'):
-            menu = self.env['ir.ui.menu'].browse(self.env.context.get('active_id')).name
-            reports = self.env['account.financial.report'].search([('name', 'ilike', menu)])
-        return reports and reports[0] or False
+    date_from = fields.Date(
+        string='Date From', required=True,
+        default=lambda self: fields.Date.to_string(date.today().replace(day=1))
+    )
+    date_to = fields.Date(
+        string='Date To', required=True,
+        default=lambda self: fields.Date.to_string(date.today())
+    )
 
-    enable_filter = fields.Boolean(string='Enable Comparison')
-    account_report_id = fields.Many2one('account.financial.report', string='Account Reports',
-                                        required=True, default=_get_account_report)
-    label_filter = fields.Char(string='Column Label', help="This label will be displayed on report to "
-                                                           "show the balance computed for the given comparison filter.")
-    filter_cmp = fields.Selection([('filter_no', 'No Filters'), ('filter_date', 'Date')],
-                                  string='Filter by', required=True, default='filter_no')
-    date_from_cmp = fields.Date(string='Date From')
-    date_to_cmp = fields.Date(string='Date To')
-    debit_credit = fields.Boolean(string='Display Debit/Credit Columns',
-                                  help="This option allows you to get more details about "
-                                       "the way your balances are computed."
-                                       " Because it is space consuming, we do not allow to"
-                                       " use it while doing a comparison.")
-
-    def _build_comparison_context(self, data):
-        result = {}
-        result['journal_ids'] = 'journal_ids' in data['form'] and data['form']['journal_ids'] or False
-        result['state'] = 'target_move' in data['form'] and data['form']['target_move'] or ''
-        if data['form']['filter_cmp'] == 'filter_date':
-            result['date_from'] = data['form']['date_from_cmp']
-            result['date_to'] = data['form']['date_to_cmp']
-            result['strict_range'] = True
-        return result
-
-    def check_report(self):
-        res = super(AccountingReport, self).check_report()
-        data = {}
-        data['form'] = self.read(['account_report_id', 'date_from_cmp', 'date_to_cmp', 'journal_ids', 'filter_cmp', 'target_move'])[0]
-        for field in ['account_report_id']:
-            if isinstance(data['form'][field], tuple):
-                data['form'][field] = data['form'][field][0]
-        comparison_context = self._build_comparison_context(data)
-        res['data']['form']['comparison_context'] = comparison_context
-        return res
+    # NEW: option to choose Normal (summary) or Details
+    report_option = fields.Selection([
+        ('normal', 'Normal (Summary)'),
+        ('details', 'Details (Full)')
+    ], string='Report Type', required=True, default='normal')
 
     def _print_report(self, data):
-        data['form'].update(self.read(['date_from_cmp', 'debit_credit', 'date_to_cmp', 'filter_cmp', 'account_report_id', 'enable_filter', 'label_filter', 'target_move'])[0])
-        return self.env.ref('accounting_pdf_reports.action_report_financial').report_action(self, data=data, config=False)
+        # Build the data dict that will be passed to the report
+        data = {
+            'form': {
+                'date_from': self.date_from,
+                'date_to': self.date_to,
+                'target_move': self.target_move,
+                'company_id': self.company_id.id if self.company_id else self.env.company.id,
+                'report_option': self.report_option,
+            }
+        }
+        # Call the same report action (the report will branch based on 'report_option')
+        return self.env.ref('accounting_pdf_reports.action_report_account_tax').report_action(self, data=data)

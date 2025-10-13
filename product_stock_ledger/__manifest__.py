1430e8ea6079e06ad92ac63834d3820aa3791349
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-13 15:13:17 +0530

    custom

--- product_stock_ledger/__manifest__.py
+++ product_stock_ledger/__manifest__.py
@@ -4,21 +4,19 @@
     "version": "1.0",
     "author": "You",
     "category": "Inventory",
-    "summary": "Product wise stock ledger report (QWeb/PDF)",
-    "depends": ["stock", "product", "account"],  # add modules you need
+    "summary": "Product-wise stock ledger report (QWeb/PDF)",
+    "depends": ["stock", "product", "account"],
     "data": [
         "views/stock_ledger_wizard_views.xml",
         "views/report_action.xml",
         "report/product_stock_ledger_templates.xml",
     ],
-    'assets': {
-        'web.assets_backend': [
-            'product_stock_ledger/static/src/scss/report_styles.scss',
+    "assets": {
+        "web.assets_backend": [
+            "product_stock_ledger/static/src/scss/report_styles.scss",
         ],
     },
-
     "installable": True,
     "application": False,
+    "license": "LGPL-3",
 }
-
-

e613b2e8dc0107ef8840975d6d4f908535037988
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-13 15:46:24 +0530

    add code

--- accounting_pdf_reports/__manifest__.py
+++ accounting_pdf_reports/__manifest__.py
@@ -39,6 +39,7 @@
         'report/report_aged_partner.xml',
         'report/report_journal_audit.xml',
         'report/report_journal_entries.xml',
+        'report/account_summary_wizard_views.xml',
 
     ],
     'pre_init_hook': '_pre_init_clean_m2m_models',

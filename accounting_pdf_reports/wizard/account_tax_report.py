755c880dc6ff54a40808b339b7cd13c0bf9090ef
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-14 12:29:04 +0530

    add code

--- accounting_pdf_reports/wizard/account_tax_report.py
+++ accounting_pdf_reports/wizard/account_tax_report.py
@@ -19,13 +19,13 @@
 #     def _print_report(self, data):
 #         return self.env.ref('accounting_pdf_reports.action_report_account_tax').report_action(self, data=data)
 
-from odoo import models, api, fields
+from odoo import models, fields, api
 from datetime import date
 
-class AccountTaxReport(models.TransientModel):
+class AccountTaxReportWizard(models.TransientModel):
     _name = 'account.tax.report.wizard'
     _inherit = "account.common.report"
-    _description = 'Tax Report'
+    _description = 'Tax Report Wizard'
 
     date_from = fields.Date(
         string='Date From', required=True,
@@ -35,12 +35,26 @@ class AccountTaxReport(models.TransientModel):
         string='Date To', required=True,
         default=lambda self: fields.Date.to_string(date.today())
     )
+    target_move = fields.Selection(
+        [('all', 'All Entries'), ('posted', 'Posted Entries')],
+        string='Target Moves', default='posted'
+    )
     detailed_summary = fields.Boolean(
         string='Detailed Summary',
         help='Enable to view summary report with clickable tax details'
     )
 
-    def _print_report(self, data):
-        # include detailed_summary in report data
+    def _print_report(self, data=None):
+        """Print report respecting detailed_summary checkbox"""
+        self.ensure_one()
+        if not data:
+            data = {'form': self.read()[0]}
         data['form']['detailed_summary'] = self.detailed_summary
+        return self.env.ref('accounting_pdf_reports.action_report_account_tax').report_action(self, data=data)
+
+    def action_summary(self):
+        """Generate report in summary (normal) mode"""
+        self.ensure_one()
+        self.detailed_summary = False  # turn off detailed summary
+        data = {'form': self.read()[0]}
         return self.env.ref('accounting_pdf_reports.action_report_account_tax').report_action(self, data=data)

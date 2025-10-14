2969d4e92771dfe4665dde9d182c212467e06528
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-14 16:06:36 +0530

    add code

--- custom_account_report/models/tax_report_wizard_inherit.py
+++ custom_account_report/models/tax_report_wizard_inherit.py
@@ -3,7 +3,6 @@ from odoo import models, api
 class AccountTaxReportWizard(models.TransientModel):
     _inherit = 'account.tax.report.wizard'
 
-    @api.multi
     def open_tax_report_details(self):
         """Open account.move list view filtered by wizard dates."""
         self.ensure_one()
@@ -11,10 +10,9 @@ class AccountTaxReportWizard(models.TransientModel):
         domain = [
             ('invoice_date', '>=', self.date_from),
             ('invoice_date', '<=', self.date_to),
-            ('move_type', 'in', ['out_invoice', 'out_refund']),  # sales invoices; adjust if you want purchases too
+            ('move_type', 'in', ['out_invoice', 'out_refund']),  # sales invoices
         ]
 
-        # Build the action dict for list view
         return {
             'type': 'ir.actions.act_window',
             'name': 'Tax Report Details',

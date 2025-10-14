6b0b5ce2f682edbcbf7316c3e77d2cc968d8dc55
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-14 16:20:50 +0530

    add code

--- custom_account_report/models/tax_report_wizard_inherit.py
+++ custom_account_report/models/tax_report_wizard_inherit.py
@@ -5,16 +5,19 @@ class AccountTaxReportWizard(models.TransientModel):
 
     def open_tax_report_details(self):
         self.ensure_one()
+
         domain = [
             ('invoice_date', '>=', self.date_from),
             ('invoice_date', '<=', self.date_to),
-            ('move_type', 'in', ['out_invoice', 'out_refund']),
+            ('move_type', 'in', ['out_invoice', 'out_refund', 'in_invoice', 'in_refund']),  # sales + purchases
         ]
+
         return {
             'type': 'ir.actions.act_window',
             'name': 'Tax Report Details',
             'res_model': 'account.move',
-            'view_mode': 'list,form',  # ✅ use 'list' instead of 'tree'
+            'view_mode': 'list,form',  # Odoo 19: list instead of tree
             'domain': domain,
             'target': 'current',
         }
+

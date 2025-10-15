ef6f22ae9568ee21236695885d4c80c4154e882e
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-15 16:10:30 +0530

    add code

--- custom_account_report/models/tax_report_detail_line.py
+++ custom_account_report/models/tax_report_detail_line.py
@@ -40,31 +40,3 @@ class TaxReportDetailLine(models.TransientModel):
             'target': 'current',
         }
 
-
-
-# from odoo import models, fields
-#
-# class TaxReportDetailLine(models.TransientModel):
-#     _name = "tax.report.detail.line"
-#     _description = "Tax Report Detail Line"
-#
-#     wizard_id = fields.Many2one('account.tax.report.wizard')
-#     type = fields.Selection([('sale', 'Sale'), ('purchase', 'Purchase')], string='Type')
-#     tax_id = fields.Many2one('account.tax', string='Tax')
-#     tax_name = fields.Char(related='tax_id.name', string='Tax Name')
-#     base_amount = fields.Monetary(string='Net Amount')
-#     tax_amount = fields.Monetary(string='Tax Amount')
-#     currency_id = fields.Many2one('res.currency', default=lambda self: self.env.company.currency_id)
-#     move_ids = fields.Many2many('account.move', string='Invoices')
-#
-#     def open_moves(self):
-#         """Open invoices related to this tax line"""
-#         self.ensure_one()
-#         return {
-#             'name': 'Invoices for Tax',
-#             'type': 'ir.actions.act_window',
-#             'res_model': 'account.move',
-#             'view_mode': 'list,form',
-#             'domain': [('id', 'in', self.move_ids.ids)],
-#             'target': 'current',
-#         }

1404828c90fd63ea9863bc39bd9226fafdf3fb74
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-15 16:22:34 +0530

    add code

--- custom_account_report/__manifest__.py
+++ custom_account_report/__manifest__.py
@@ -1,14 +1,14 @@
-{
-    'name': 'Custom Tax Report Detail',
-    'version': '1.0',
-    'category': 'Accounting',
-    'summary': 'Add detailed summary to Odoo Mates Tax Report',
-    'depends': ['account', 'accounting_pdf_reports'],
-    'data': [
-        'views/tax_report_detail_views.xml',
-        'views/tax_report_wizard_inherit.xml',
-        'views/tax_report_detail_line_views.xml'
-    ],
-    'installable': True,
-    'application': False,
-}
+# {
+#     'name': 'Custom Tax Report Detail',
+#     'version': '1.0',
+#     'category': 'Accounting',
+#     'summary': 'Add detailed summary to Odoo Mates Tax Report',
+#     'depends': ['account', 'accounting_pdf_reports'],
+#     'data': [
+#         'views/tax_report_detail_views.xml',
+#         'views/tax_report_wizard_inherit.xml',
+#         'views/tax_report_detail_line_views.xml'
+#     ],
+#     'installable': True,
+#     'application': False,
+# }

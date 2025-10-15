a5e667277727c082cb03139fdf2c1f15b9151fdd
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-15 12:53:50 +0530

    add code

--- custom_tb_report/models/tb_line.py
+++ custom_tb_report/models/tb_line.py
@@ -1,33 +1,14 @@
-# models/tb_line.py
+# models/trial_balance_line.py
 from odoo import models, fields
 
-class TrialBalanceLine(models.TransientModel):
+class TrialBalanceLine(models.Model):
     _name = 'trial.balance.line'
     _description = 'Trial Balance Line'
 
-    wizard_id = fields.Many2one('trial.balance.wizard', string='Wizard')
+    wizard_id = fields.Many2one('account.balance.report', string='Wizard')
     account_id = fields.Many2one('account.account', string='Account')
-
-    # currency field required for Monetary fields
-    currency_id = fields.Many2one(
-        'res.currency', string='Currency',
-        default=lambda self: self.env.company.currency_id.id
-    )
-
-    opening_balance = fields.Monetary(string='Opening', currency_field='currency_id')
-    debit = fields.Monetary(string='Debit', currency_field='currency_id')
-    credit = fields.Monetary(string='Credit', currency_field='currency_id')
-    ending_balance = fields.Monetary(string='Ending', currency_field='currency_id')
-
-    move_line_ids = fields.Many2many('account.move.line', string='Entries')
-
-    def open_entries(self):
-        self.ensure_one()
-        return {
-            'name': 'Journal Items',
-            'type': 'ir.actions.act_window',
-            'res_model': 'account.move.line',
-            'view_mode': 'list,form',
-            'domain': [('id', 'in', self.move_line_ids.ids)],
-            'target': 'current',
-        }
+    opening_balance = fields.Monetary(string='Opening Balance', currency_field='company_currency_id')
+    debit = fields.Monetary(string='Debit', currency_field='company_currency_id')
+    credit = fields.Monetary(string='Credit', currency_field='company_currency_id')
+    ending_balance = fields.Monetary(string='Ending Balance', currency_field='company_currency_id')
+    company_currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.company.currency_id)

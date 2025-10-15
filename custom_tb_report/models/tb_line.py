3c2ac3c7dfad9c7d70811276757028a5e1206a40
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-15 12:00:15 +0530

    add code

--- custom_tb_report/models/tb_line.py
+++ custom_tb_report/models/tb_line.py
@@ -1,15 +1,23 @@
+# models/tb_line.py
 from odoo import models, fields
 
 class TrialBalanceLine(models.TransientModel):
     _name = 'trial.balance.line'
     _description = 'Trial Balance Line'
 
-    wizard_id = fields.Many2one('trial.balance.wizard')
+    wizard_id = fields.Many2one('trial.balance.wizard', string='Wizard')
     account_id = fields.Many2one('account.account', string='Account')
-    opening_balance = fields.Monetary(string='Opening')
-    debit = fields.Monetary(string='Debit')
-    credit = fields.Monetary(string='Credit')
-    ending_balance = fields.Monetary(string='Ending')
+
+    # currency field required for Monetary fields
+    currency_id = fields.Many2one(
+        'res.currency', string='Currency',
+        default=lambda self: self.env.company.currency_id.id
+    )
+
+    opening_balance = fields.Monetary(string='Opening', currency_field='currency_id')
+    debit = fields.Monetary(string='Debit', currency_field='currency_id')
+    credit = fields.Monetary(string='Credit', currency_field='currency_id')
+    ending_balance = fields.Monetary(string='Ending', currency_field='currency_id')
 
     move_line_ids = fields.Many2many('account.move.line', string='Entries')
 

11e04b02c8609a648b0bc57992f2c93ef5d215df
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-15 16:19:13 +0530

    custom

--- product_stock_ledger/__manifest__.py
+++ product_stock_ledger/__manifest__.py
@@ -20,9 +20,6 @@
         "report/product_stock_ledger_templates.xml",
     ],
     "assets": {
-        # "web.assets_backend": [
-        #     "product_stock_ledger/static/src/js/",
-        # ],
         "web.report_assets_common": [
             "product_stock_ledger/static/src/css/report_styles.css",
         ],

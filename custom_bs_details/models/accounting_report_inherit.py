2e9c3f6f179baba34b2b8c7327031dcfb12e5015
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-16 16:44:26 +0530

    custom

--- custom_bs_details/models/accounting_report_inherit.py
+++ custom_bs_details/models/accounting_report_inherit.py
@@ -16,18 +16,18 @@ class AccountingReportInherit(models.TransientModel):
         """
         self.ensure_one()
 
-        # Collect filters
-        aml_domain = []
+        # Collect filters for domain
+        aml_domain = [('company_id', '=', self.env.company.id)]
         if self.date_from:
             aml_domain.append(('date', '>=', self.date_from))
         if self.date_to:
             aml_domain.append(('date', '<=', self.date_to))
-        if hasattr(self, 'target_move') and self.target_move == 'posted':
+        if getattr(self, 'target_move', False) and self.target_move == 'posted':
             aml_domain.append(('move_id.state', '=', 'posted'))
-        if self.journal_ids:
+        if getattr(self, 'journal_ids', False) and self.journal_ids:
             aml_domain.append(('journal_id', 'in', self.journal_ids.ids))
 
-        # Start SQL query with JOIN to account_move
+        # SQL query to aggregate debit/credit by account
         query = """
             SELECT 
                 aml.account_id AS account_id,
@@ -35,15 +35,13 @@ class AccountingReportInherit(models.TransientModel):
                 SUM(aml.credit) AS total_credit
             FROM account_move_line AS aml
             JOIN account_move AS am ON aml.move_id = am.id
-            WHERE TRUE
+            WHERE aml.company_id = %s
         """
-        params = []
+        params = [self.env.company.id]
         where_clauses = []
 
-        # Dynamically add WHERE conditions
         for clause in aml_domain:
             field, op, val = clause
-            # Prefix table aliases properly
             if field.startswith('move_id.'):
                 field = field.replace('move_id.', 'am.')
             else:
@@ -51,7 +49,7 @@ class AccountingReportInherit(models.TransientModel):
 
             if op == 'in':
                 if not val:
-                    where_clauses.append("FALSE")  # Avoid empty IN ()
+                    where_clauses.append("FALSE")
                 else:
                     where_clauses.append(f"{field} IN %s")
                     params.append(tuple(val))
@@ -66,16 +64,14 @@ class AccountingReportInherit(models.TransientModel):
 
         _logger.info("Running Balance Sheet query: %s with params %s", query, params)
 
-        # Execute query safely
         self.env.cr.execute(query, tuple(params))
         rows = self.env.cr.dictfetchall()
 
-        # Remove old temporary records
+        # Clear previous temporary records
         existing_lines = self.env['balance.sheet.line'].search([('wizard_id', '=', self.id)])
         if existing_lines:
             existing_lines.unlink()
 
-        # Create new balance sheet lines
         BalanceLine = self.env['balance.sheet.line']
         for row in rows:
             BalanceLine.create({
@@ -85,7 +81,7 @@ class AccountingReportInherit(models.TransientModel):
                 'credit': row.get('total_credit') or 0.0,
             })
 
-        # Return a tree view action to show results
+        # Return proper action
         return {
             'name': _('Balance Sheet Details'),
             'type': 'ir.actions.act_window',
@@ -95,3 +91,107 @@ class AccountingReportInherit(models.TransientModel):
             'domain': [('wizard_id', '=', self.id)],
             'context': {'default_wizard_id': self.id},
         }
+
+
+
+
+
+
+
+# # -*- coding: utf-8 -*-
+# from odoo import api, fields, models, _
+# from odoo.exceptions import UserError
+# import logging
+#
+# _logger = logging.getLogger(__name__)
+#
+#
+# class AccountingReportInherit(models.TransientModel):
+#     _inherit = 'accounting.report'  # Inherit the Odoomates balance sheet wizard
+#
+#     def action_show_details(self):
+#         """
+#         Build domain from wizard options, aggregate account_move_line by account,
+#         create balance.sheet.line transient records, and open tree view.
+#         """
+#         self.ensure_one()
+#
+#         # Collect filters
+#         aml_domain = []
+#         if self.date_from:
+#             aml_domain.append(('date', '>=', self.date_from))
+#         if self.date_to:
+#             aml_domain.append(('date', '<=', self.date_to))
+#         if hasattr(self, 'target_move') and self.target_move == 'posted':
+#             aml_domain.append(('move_id.state', '=', 'posted'))
+#         if self.journal_ids:
+#             aml_domain.append(('journal_id', 'in', self.journal_ids.ids))
+#
+#         # Start SQL query with JOIN to account_move
+#         query = """
+#             SELECT
+#                 aml.account_id AS account_id,
+#                 SUM(aml.debit) AS total_debit,
+#                 SUM(aml.credit) AS total_credit
+#             FROM account_move_line AS aml
+#             JOIN account_move AS am ON aml.move_id = am.id
+#             WHERE TRUE
+#         """
+#         params = []
+#         where_clauses = []
+#
+#         # Dynamically add WHERE conditions
+#         for clause in aml_domain:
+#             field, op, val = clause
+#             # Prefix table aliases properly
+#             if field.startswith('move_id.'):
+#                 field = field.replace('move_id.', 'am.')
+#             else:
+#                 field = f"aml.{field}"
+#
+#             if op == 'in':
+#                 if not val:
+#                     where_clauses.append("FALSE")  # Avoid empty IN ()
+#                 else:
+#                     where_clauses.append(f"{field} IN %s")
+#                     params.append(tuple(val))
+#             else:
+#                 where_clauses.append(f"{field} {op} %s")
+#                 params.append(val)
+#
+#         if where_clauses:
+#             query += " AND " + " AND ".join(where_clauses)
+#
+#         query += " GROUP BY aml.account_id ORDER BY aml.account_id"
+#
+#         _logger.info("Running Balance Sheet query: %s with params %s", query, params)
+#
+#         # Execute query safely
+#         self.env.cr.execute(query, tuple(params))
+#         rows = self.env.cr.dictfetchall()
+#
+#         # Remove old temporary records
+#         existing_lines = self.env['balance.sheet.line'].search([('wizard_id', '=', self.id)])
+#         if existing_lines:
+#             existing_lines.unlink()
+#
+#         # Create new balance sheet lines
+#         BalanceLine = self.env['balance.sheet.line']
+#         for row in rows:
+#             BalanceLine.create({
+#                 'wizard_id': self.id,
+#                 'account_id': row.get('account_id'),
+#                 'debit': row.get('total_debit') or 0.0,
+#                 'credit': row.get('total_credit') or 0.0,
+#             })
+#
+#         # Return a tree view action to show results
+#         return {
+#             'name': _('Balance Sheet Details'),
+#             'type': 'ir.actions.act_window',
+#             'res_model': 'balance.sheet.line',
+#             'view_mode': 'list,form',
+#             'target': 'current',
+#             'domain': [('wizard_id', '=', self.id)],
+#             'context': {'default_wizard_id': self.id},
+#         }
--- custom_bs_details/models/balance_sheet_line.py
+++ custom_bs_details/models/balance_sheet_line.py
@@ -7,10 +7,8 @@ class BalanceSheetLine(models.TransientModel):
     _name = 'balance.sheet.line'
     _description = 'Balance Sheet Line'
 
-    # Basic Info
     name = fields.Char(string="Name")
 
-    # Currency
     currency_id = fields.Many2one(
         'res.currency',
         string='Currency',
@@ -23,11 +21,9 @@ class BalanceSheetLine(models.TransientModel):
         default=lambda self: self.env.company.currency_id,
     )
 
-    # Relations
     wizard_id = fields.Many2one('accounting.report', string='Wizard', ondelete='cascade')
     account_id = fields.Many2one('account.account', string='Account', required=True)
 
-    # Financial Fields
     debit = fields.Monetary(string='Debit', currency_field='company_currency_id')
     credit = fields.Monetary(string='Credit', currency_field='company_currency_id')
     balance = fields.Monetary(
@@ -37,14 +33,11 @@ class BalanceSheetLine(models.TransientModel):
         currency_field='company_currency_id',
     )
 
-    # Compute Method
     @api.depends('debit', 'credit')
     def _compute_balance(self):
-        """Compute balance as debit - credit"""
         for rec in self:
             rec.balance = (rec.debit or 0.0) - (rec.credit or 0.0)
 
-    # Action Method
     def action_view_ledger(self):
         """Open journal items (account.move.line) for this account using wizard filters"""
         self.ensure_one()
@@ -52,9 +45,12 @@ class BalanceSheetLine(models.TransientModel):
         if not wizard:
             raise UserError(_('Wizard context lost.'))
 
-        domain = [('account_id', '=', self.account_id.id)]
+        domain = [
+            ('account_id', '=', self.account_id.id),
+            ('company_id', '=', self.env.company.id),
+        ]
 
-        # Apply wizard filters if available
+        # Apply wizard filters
         if getattr(wizard, 'date_from', False):
             domain.append(('date', '>=', wizard.date_from))
         if getattr(wizard, 'date_to', False):
@@ -64,7 +60,6 @@ class BalanceSheetLine(models.TransientModel):
         if getattr(wizard, 'target_move', False) and wizard.target_move == 'posted':
             domain.append(('move_id.state', '=', 'posted'))
 
-        # Return Ledger View Action
         return {
             'name': _('Ledger Entries: %s') % (self.account_id.display_name),
             'type': 'ir.actions.act_window',
@@ -73,3 +68,84 @@ class BalanceSheetLine(models.TransientModel):
             'domain': domain,
             'target': 'current',
         }
+
+
+
+
+
+
+# # -*- coding: utf-8 -*-
+# from odoo import api, fields, models, _
+# from odoo.exceptions import UserError
+#
+#
+# class BalanceSheetLine(models.TransientModel):
+#     _name = 'balance.sheet.line'
+#     _description = 'Balance Sheet Line'
+#
+#     # Basic Info
+#     name = fields.Char(string="Name")
+#
+#     # Currency
+#     currency_id = fields.Many2one(
+#         'res.currency',
+#         string='Currency',
+#         default=lambda self: self.env.company.currency_id,
+#     )
+#     company_currency_id = fields.Many2one(
+#         'res.currency',
+#         string='Company Currency',
+#         readonly=True,
+#         default=lambda self: self.env.company.currency_id,
+#     )
+#
+#     # Relations
+#     wizard_id = fields.Many2one('accounting.report', string='Wizard', ondelete='cascade')
+#     account_id = fields.Many2one('account.account', string='Account', required=True)
+#
+#     # Financial Fields
+#     debit = fields.Monetary(string='Debit', currency_field='company_currency_id')
+#     credit = fields.Monetary(string='Credit', currency_field='company_currency_id')
+#     balance = fields.Monetary(
+#         string='Balance',
+#         compute='_compute_balance',
+#         store=False,
+#         currency_field='company_currency_id',
+#     )
+#
+#     # Compute Method
+#     @api.depends('debit', 'credit')
+#     def _compute_balance(self):
+#         """Compute balance as debit - credit"""
+#         for rec in self:
+#             rec.balance = (rec.debit or 0.0) - (rec.credit or 0.0)
+#
+#     # Action Method
+#     def action_view_ledger(self):
+#         """Open journal items (account.move.line) for this account using wizard filters"""
+#         self.ensure_one()
+#         wizard = self.wizard_id
+#         if not wizard:
+#             raise UserError(_('Wizard context lost.'))
+#
+#         domain = [('account_id', '=', self.account_id.id)]
+#
+#         # Apply wizard filters if available
+#         if getattr(wizard, 'date_from', False):
+#             domain.append(('date', '>=', wizard.date_from))
+#         if getattr(wizard, 'date_to', False):
+#             domain.append(('date', '<=', wizard.date_to))
+#         if getattr(wizard, 'journal_ids', False) and wizard.journal_ids:
+#             domain.append(('journal_id', 'in', wizard.journal_ids.ids))
+#         if getattr(wizard, 'target_move', False) and wizard.target_move == 'posted':
+#             domain.append(('move_id.state', '=', 'posted'))
+#
+#         # Return Ledger View Action
+#         return {
+#             'name': _('Ledger Entries: %s') % (self.account_id.display_name),
+#             'type': 'ir.actions.act_window',
+#             'res_model': 'account.move.line',
+#             'view_mode': 'list,form',
+#             'domain': domain,
+#             'target': 'current',
+#         }

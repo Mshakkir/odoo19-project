3a19b83d41fda8466558c977185b5995810c1528
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-17 16:39:36 +0530

    add code

--- accounting_pdf_reports/report/report_trial_balance.py
+++ accounting_pdf_reports/report/report_trial_balance.py
@@ -1,3 +1,93 @@
+# import time
+# from odoo import api, models, _
+# from odoo.exceptions import UserError
+#
+#
+# class ReportTrialBalance(models.AbstractModel):
+#     _name = 'report.accounting_pdf_reports.report_trialbalance'
+#     _description = 'Trial Balance Report'
+#
+#     def _get_accounts(self, accounts, display_account):
+#         """ compute the balance, debit and credit for the provided accounts
+#             :Arguments:
+#                 `accounts`: list of accounts record,
+#                 `display_account`: it's used to display either all accounts or those accounts which balance is > 0
+#             :Returns a list of dictionary of Accounts with following key and value
+#                 `name`: Account name,
+#                 `code`: Account code,
+#                 `credit`: total amount of credit,
+#                 `debit`: total amount of debit,
+#                 `balance`: total amount of balance,
+#         """
+#
+#         account_result = {}
+#         # Prepare sql query base on selected parameters from wizard
+#         tables, where_clause, where_params = self.env['account.move.line']._query_get()
+#         tables = tables.replace('"','')
+#         if not tables:
+#             tables = 'account_move_line'
+#         wheres = [""]
+#         if where_clause.strip():
+#             wheres.append(where_clause.strip())
+#         filters = " AND ".join(wheres)
+#         # compute the balance, debit and credit for the provided accounts
+#         request = ("SELECT account_id AS id, SUM(debit) AS debit, SUM(credit) AS credit, "
+#                    "(SUM(debit) - SUM(credit)) AS balance" +\
+#                    " FROM " + tables + " WHERE account_id IN %s " + filters + " GROUP BY account_id")
+#         params = (tuple(accounts.ids),) + tuple(where_params)
+#         self.env.cr.execute(request, params)
+#         for row in self.env.cr.dictfetchall():
+#             account_result[row.pop('id')] = row
+#
+#         account_res = []
+#         for account in accounts:
+#             res = dict((fn, 0.0) for fn in ['credit', 'debit', 'balance'])
+#             currency = account.currency_id and account.currency_id or self.env.company.currency_id
+#             res['code'] = account.code
+#             res['name'] = account.name
+#             if account.id in account_result:
+#                 res['debit'] = account_result[account.id].get('debit')
+#                 res['credit'] = account_result[account.id].get('credit')
+#                 res['balance'] = account_result[account.id].get('balance')
+#             if display_account == 'all':
+#                 account_res.append(res)
+#             if display_account == 'not_zero' and not currency.is_zero(res['balance']):
+#                 account_res.append(res)
+#             if display_account == 'movement' and (not currency.is_zero(res['debit']) or not currency.is_zero(res['credit'])):
+#                 account_res.append(res)
+#         return account_res
+#
+#     @api.model
+#     def _get_report_values(self, docids, data=None):
+#         if not data.get('form') or not self.env.context.get('active_model'):
+#             raise UserError(_("Form content is missing, this report cannot be printed."))
+#
+#         model = self.env.context.get('active_model')
+#         docs = self.env[model].browse(self.env.context.get('active_ids', []))
+#         display_account = data['form'].get('display_account')
+#         accounts = docs if model == 'account.account' else self.env['account.account'].search([])
+#         context = data['form'].get('used_context')
+#         analytic_accounts = []
+#         if data['form'].get('analytic_account_ids'):
+#             analytic_account_ids = self.env['account.analytic.account'].browse(data['form'].get('analytic_account_ids'))
+#             context['analytic_account_ids'] = analytic_account_ids
+#             analytic_accounts = [account.name for account in analytic_account_ids]
+#         account_res = self.with_context(context)._get_accounts(accounts, display_account)
+#         codes = []
+#         if data['form'].get('journal_ids', False):
+#             codes = [journal.code for journal in
+#                      self.env['account.journal'].search(
+#                          [('id', 'in', data['form']['journal_ids'])])]
+#         return {
+#             'doc_ids': self.ids,
+#             'doc_model': model,
+#             'data': data['form'],
+#             'docs': docs,
+#             'print_journal': codes,
+#             'analytic_accounts': analytic_accounts,
+#             'time': time,
+#             'Accounts': account_res,
+#         }
 import time
 from odoo import api, models, _
 from odoo.exceptions import UserError
@@ -7,56 +97,62 @@ class ReportTrialBalance(models.AbstractModel):
     _name = 'report.accounting_pdf_reports.report_trialbalance'
     _description = 'Trial Balance Report'
 
+    # ------------------------------
+    # Multi-company aware account fetch
+    # ------------------------------
     def _get_accounts(self, accounts, display_account):
-        """ compute the balance, debit and credit for the provided accounts
-            :Arguments:
-                `accounts`: list of accounts record,
-                `display_account`: it's used to display either all accounts or those accounts which balance is > 0
-            :Returns a list of dictionary of Accounts with following key and value
-                `name`: Account name,
-                `code`: Account code,
-                `credit`: total amount of credit,
-                `debit`: total amount of debit,
-                `balance`: total amount of balance,
-        """
-
         account_result = {}
-        # Prepare sql query base on selected parameters from wizard
         tables, where_clause, where_params = self.env['account.move.line']._query_get()
-        tables = tables.replace('"','')
-        if not tables:
-            tables = 'account_move_line'
-        wheres = [""]
+
+        # Remove double quotes for PostgreSQL compatibility
+        tables = tables.replace('"', '') or 'account_move_line'
+        wheres = []
         if where_clause.strip():
             wheres.append(where_clause.strip())
-        filters = " AND ".join(wheres)
-        # compute the balance, debit and credit for the provided accounts
-        request = ("SELECT account_id AS id, SUM(debit) AS debit, SUM(credit) AS credit, "
-                   "(SUM(debit) - SUM(credit)) AS balance" +\
-                   " FROM " + tables + " WHERE account_id IN %s " + filters + " GROUP BY account_id")
-        params = (tuple(accounts.ids),) + tuple(where_params)
+
+        filters = " AND ".join(wheres) if wheres else ""
+
+        # 🔹 Allow multi-company: add an explicit company filter if provided in context
+        company_ids = self.env.context.get('allowed_company_ids') or [self.env.company.id]
+        company_filter = " AND company_id IN %s"
+        filters = (filters + company_filter) if filters else "WHERE company_id IN %s"
+
+        # Compute debit/credit/balance
+        request = (
+            "SELECT account_id AS id, SUM(debit) AS debit, SUM(credit) AS credit, "
+            "(SUM(debit) - SUM(credit)) AS balance "
+            f"FROM {tables} WHERE account_id IN %s {filters.replace('WHERE', 'AND', 1)} GROUP BY account_id"
+        )
+
+        # Merge parameters (accounts + company + others)
+        params = (tuple(accounts.ids), tuple(company_ids)) + tuple(where_params)
         self.env.cr.execute(request, params)
+
         for row in self.env.cr.dictfetchall():
             account_result[row.pop('id')] = row
 
+        # Prepare results
         account_res = []
         for account in accounts:
             res = dict((fn, 0.0) for fn in ['credit', 'debit', 'balance'])
-            currency = account.currency_id and account.currency_id or self.env.company.currency_id
+            currency = account.currency_id or self.env.company.currency_id
             res['code'] = account.code
             res['name'] = account.name
             if account.id in account_result:
-                res['debit'] = account_result[account.id].get('debit')
-                res['credit'] = account_result[account.id].get('credit')
-                res['balance'] = account_result[account.id].get('balance')
+                res.update(account_result[account.id])
             if display_account == 'all':
                 account_res.append(res)
-            if display_account == 'not_zero' and not currency.is_zero(res['balance']):
+            elif display_account == 'not_zero' and not currency.is_zero(res['balance']):
                 account_res.append(res)
-            if display_account == 'movement' and (not currency.is_zero(res['debit']) or not currency.is_zero(res['credit'])):
+            elif display_account == 'movement' and (
+                not currency.is_zero(res['debit']) or not currency.is_zero(res['credit'])
+            ):
                 account_res.append(res)
         return account_res
 
+    # ------------------------------
+    # Pass company context from wizard
+    # ------------------------------
     @api.model
     def _get_report_values(self, docids, data=None):
         if not data.get('form') or not self.env.context.get('active_model'):
@@ -65,19 +161,36 @@ class ReportTrialBalance(models.AbstractModel):
         model = self.env.context.get('active_model')
         docs = self.env[model].browse(self.env.context.get('active_ids', []))
         display_account = data['form'].get('display_account')
+
         accounts = docs if model == 'account.account' else self.env['account.account'].search([])
-        context = data['form'].get('used_context')
+
+        # 🔹 Capture selected companies from the user/session
+        company_ids = self.env.context.get('allowed_company_ids') or [self.env.company.id]
+
+        # Rebuild context for _query_get with our allowed companies
+        context = dict(data['form'].get('used_context', {}))
+        context['allowed_company_ids'] = company_ids
+        context['company_ids'] = company_ids
+
         analytic_accounts = []
         if data['form'].get('analytic_account_ids'):
-            analytic_account_ids = self.env['account.analytic.account'].browse(data['form'].get('analytic_account_ids'))
-            context['analytic_account_ids'] = analytic_account_ids
-            analytic_accounts = [account.name for account in analytic_account_ids]
+            analytic_account_ids = self.env['account.analytic.account'].browse(
+                data['form'].get('analytic_account_ids')
+            )
+            context['analytic_account_ids'] = analytic_account_ids.ids
+            analytic_accounts = [acc.name for acc in analytic_account_ids]
+
         account_res = self.with_context(context)._get_accounts(accounts, display_account)
+
         codes = []
         if data['form'].get('journal_ids', False):
-            codes = [journal.code for journal in
-                     self.env['account.journal'].search(
-                         [('id', 'in', data['form']['journal_ids'])])]
+            codes = [
+                journal.code
+                for journal in self.env['account.journal'].search(
+                    [('id', 'in', data['form']['journal_ids'])]
+                )
+            ]
+
         return {
             'doc_ids': self.ids,
             'doc_model': model,
--- custom_tb_report/models/tb_wizard.py
+++ custom_tb_report/models/tb_wizard.py
@@ -1,18 +1,7 @@
-from odoo import models, fields, api
-import logging
-
-_logger = logging.getLogger(__name__)
-
+from odoo import models
 
 class AccountBalanceReportInherit(models.TransientModel):
-    _inherit = 'account.balance.report'
-
-    # Add new field for "All Companies" option
-    include_all_companies = fields.Boolean(
-        string='All Companies',
-        default=False,
-        help='Include transactions from all companies'
-    )
+    _inherit = 'account.balance.report'  # <- inherit Trial Balance wizard
 
     def open_trial_balance(self):
         self.ensure_one()
@@ -20,98 +9,44 @@ class AccountBalanceReportInherit(models.TransientModel):
         # Clear previous TB lines
         self.env['trial.balance.line'].search([('wizard_id', '=', self.id)]).unlink()
 
-        # Determine which companies to include
-        if self.include_all_companies:
-            # Get all companies
-            companies = self.env['res.company'].search([])
-            company_ids = companies.ids
-            _logger.info(f"Processing ALL companies: {[c.name for c in companies]}")
-        else:
-            # Use only the selected company from wizard
-            company_ids = [self.company_id.id] if self.company_id else [self.env.company.id]
-            company_name = self.env['res.company'].browse(company_ids[0]).name
-            _logger.info(f"Processing single company: {company_name}")
-
-        # Get all accounts (don't filter by company - accounts might be shared)
         accounts = self.env['account.account'].search([])
 
         for account in accounts:
-            # Build domain for move lines
-            domain = [
+            # Get all posted move lines for this account
+            move_lines = self.env['account.move.line'].search([
                 ('account_id', '=', account.id),
                 ('move_id.state', '=', 'posted'),
-                ('company_id', 'in', company_ids),
-            ]
-
-            # Get all posted move lines for this account
-            move_lines = self.env['account.move.line'].search(domain)
-
-            # Skip if no move lines found
-            if not move_lines:
-                continue
-
-            # DEBUG: Log move lines details for specific accounts
-            if account.code in ['102011', '104041', '201002']:
-                _logger.info(f"\n=== Account {account.code} - {account.name} ===")
-                _logger.info(f"Total move lines found: {len(move_lines)}")
-
-                # Group by company to see breakdown
-                for company_id in company_ids:
-                    comp_lines = move_lines.filtered(lambda l: l.company_id.id == company_id)
-                    if comp_lines:
-                        company_name = self.env['res.company'].browse(company_id).name
-                        comp_debit = sum(comp_lines.mapped('debit'))
-                        comp_credit = sum(comp_lines.mapped('credit'))
-                        _logger.info(f"  Company: {company_name}")
-                        _logger.info(f"    Lines: {len(comp_lines)}")
-                        _logger.info(f"    Debit: {comp_debit}, Credit: {comp_credit}")
-
-                        # Show individual lines
-                        for line in comp_lines[:5]:  # Show first 5 lines
-                            _logger.info(
-                                f"      Move: {line.move_id.name}, Date: {line.date}, Dr: {line.debit}, Cr: {line.credit}")
+            ])
 
             # Opening balance: sum of (debit - credit) before date_from
             if self.date_from:
                 opening_lines = move_lines.filtered(lambda l: l.date < self.date_from)
-                opening = sum(opening_lines.mapped('debit')) - sum(opening_lines.mapped('credit'))
             else:
-                opening = 0.0
+                opening_lines = move_lines.browse([])  # no opening lines if no date_from
+            opening = sum(opening_lines.mapped(lambda l: l.debit - l.credit))
 
             # Lines within the period
             if self.date_from and self.date_to:
                 period_lines = move_lines.filtered(lambda l: self.date_from <= l.date <= self.date_to)
-            elif self.date_to:
-                period_lines = move_lines.filtered(lambda l: l.date <= self.date_to)
-            elif self.date_from:
-                period_lines = move_lines.filtered(lambda l: l.date >= self.date_from)
             else:
                 period_lines = move_lines
-
             debit = sum(period_lines.mapped('debit'))
             credit = sum(period_lines.mapped('credit'))
+
             ending = opening + debit - credit
 
-            # Only create TB line if there's activity
-            if opening != 0 or debit != 0 or credit != 0:
-                self.env['trial.balance.line'].create({
-                    'wizard_id': self.id,
-                    'account_id': account.id,
-                    'opening_balance': opening,
-                    'debit': debit,
-                    'credit': credit,
-                    'ending_balance': ending,
-                })
-
-        # Build report title
-        if self.include_all_companies:
-            report_title = 'Trial Balance - All Companies'
-        else:
-            company_name = self.company_id.name if self.company_id else self.env.company.name
-            report_title = f'Trial Balance - {company_name}'
+            # Create TB line
+            self.env['trial.balance.line'].create({
+                'wizard_id': self.id,
+                'account_id': account.id,
+                'opening_balance': opening,
+                'debit': debit,
+                'credit': credit,
+                'ending_balance': ending,
+            })
 
         return {
-            'name': report_title,
+            'name': 'Trial Balance',
             'type': 'ir.actions.act_window',
             'res_model': 'trial.balance.line',
             'views': [
@@ -121,63 +56,3 @@ class AccountBalanceReportInherit(models.TransientModel):
             'target': 'current',
             'domain': [('wizard_id', '=', self.id)],
         }
-
-
-# from odoo import models
-#
-# class AccountBalanceReportInherit(models.TransientModel):
-#     _inherit = 'account.balance.report'  # <- inherit Trial Balance wizard
-#
-#     def open_trial_balance(self):
-#         self.ensure_one()
-#
-#         # Clear previous TB lines
-#         self.env['trial.balance.line'].search([('wizard_id', '=', self.id)]).unlink()
-#
-#         accounts = self.env['account.account'].search([])
-#
-#         for account in accounts:
-#             # Get all posted move lines for this account
-#             move_lines = self.env['account.move.line'].search([
-#                 ('account_id', '=', account.id),
-#                 ('move_id.state', '=', 'posted'),
-#             ])
-#
-#             # Opening balance: sum of (debit - credit) before date_from
-#             if self.date_from:
-#                 opening_lines = move_lines.filtered(lambda l: l.date < self.date_from)
-#             else:
-#                 opening_lines = move_lines.browse([])  # no opening lines if no date_from
-#             opening = sum(opening_lines.mapped(lambda l: l.debit - l.credit))
-#
-#             # Lines within the period
-#             if self.date_from and self.date_to:
-#                 period_lines = move_lines.filtered(lambda l: self.date_from <= l.date <= self.date_to)
-#             else:
-#                 period_lines = move_lines
-#             debit = sum(period_lines.mapped('debit'))
-#             credit = sum(period_lines.mapped('credit'))
-#
-#             ending = opening + debit - credit
-#
-#             # Create TB line
-#             self.env['trial.balance.line'].create({
-#                 'wizard_id': self.id,
-#                 'account_id': account.id,
-#                 'opening_balance': opening,
-#                 'debit': debit,
-#                 'credit': credit,
-#                 'ending_balance': ending,
-#             })
-#
-#         return {
-#             'name': 'Trial Balance',
-#             'type': 'ir.actions.act_window',
-#             'res_model': 'trial.balance.line',
-#             'views': [
-#                 (self.env.ref('custom_tb_report.view_trial_balance_line_list').id, 'list'),
-#                 (self.env.ref('custom_tb_report.view_trial_balance_line_form').id, 'form'),
-#             ],
-#             'target': 'current',
-#             'domain': [('wizard_id', '=', self.id)],
-#         }

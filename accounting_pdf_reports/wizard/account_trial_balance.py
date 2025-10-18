3ed9f4714ed1fc7e57f089f7f7e6c8f3c32884d6
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-18 14:56:21 +0530

    add code

--- accounting_pdf_reports/wizard/account_trial_balance.py
+++ accounting_pdf_reports/wizard/account_trial_balance.py
@@ -1,5 +1,30 @@
+# from odoo import fields, models, api
+#
+#
+# class AccountBalanceReport(models.TransientModel):
+#     _name = 'account.balance.report'
+#     _inherit = "account.common.account.report"
+#     _description = 'Trial Balance Report'
+#
+#     journal_ids = fields.Many2many(
+#         'account.journal', 'account_balance_report_journal_rel',
+#         'account_id', 'journal_id',
+#         string='Journals', required=True, default=[]
+#     )
+#     analytic_account_ids = fields.Many2many(
+#         'account.analytic.account',
+#         'account_trial_balance_analytic_rel', string='Analytic Accounts'
+#     )
+#
+#     def _get_report_data(self, data):
+#         data = self.pre_print_report(data)
+#         records = self.env[data['model']].browse(data.get('ids', []))
+#         return records, data
+#
+#     def _print_report(self, data):
+#         records, data = self._get_report_data(data)
+#         return self.env.ref('accounting_pdf_reports.action_report_trial_balance').report_action(records, data=data)
 from odoo import fields, models, api
-
 
 class AccountBalanceReport(models.TransientModel):
     _name = 'account.balance.report'
@@ -11,9 +36,11 @@ class AccountBalanceReport(models.TransientModel):
         'account_id', 'journal_id',
         string='Journals', required=True, default=[]
     )
+
     analytic_account_ids = fields.Many2many(
         'account.analytic.account',
-        'account_trial_balance_analytic_rel', string='Analytic Accounts'
+        'account_trial_balance_analytic_rel', string='Analytic Accounts',
+        help="Optional: filter report by warehouse/analytic account"
     )
 
     def _get_report_data(self, data):

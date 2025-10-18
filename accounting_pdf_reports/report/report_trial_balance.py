8111fd204339fd9db4db605706d023a846839714
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-18 11:12:19 +0530

    add code

--- accounting_pdf_reports/report/report_trial_balance.py
+++ accounting_pdf_reports/report/report_trial_balance.py
@@ -8,19 +8,31 @@ class ReportTrialBalance(models.AbstractModel):
     _description = 'Trial Balance Report'
 
     def _get_accounts(self, accounts, display_account):
-        """ compute the balance, debit and credit for the provided accounts """
+        """ compute the balance, debit and credit for the provided accounts
+            :Arguments:
+                `accounts`: list of accounts record,
+                `display_account`: it's used to display either all accounts or those accounts which balance is > 0
+            :Returns a list of dictionary of Accounts with following key and value
+                `name`: Account name,
+                `code`: Account code,
+                `credit`: total amount of credit,
+                `debit`: total amount of debit,
+                `balance`: total amount of balance,
+        """
+
         account_result = {}
+        # Prepare sql query base on selected parameters from wizard
         tables, where_clause, where_params = self.env['account.move.line']._query_get()
-        tables = tables.replace('"', '')
+        tables = tables.replace('"','')
         if not tables:
             tables = 'account_move_line'
         wheres = [""]
         if where_clause.strip():
             wheres.append(where_clause.strip())
         filters = " AND ".join(wheres)
-
+        # compute the balance, debit and credit for the provided accounts
         request = ("SELECT account_id AS id, SUM(debit) AS debit, SUM(credit) AS credit, "
-                   "(SUM(debit) - SUM(credit)) AS balance"
+                   "(SUM(debit) - SUM(credit)) AS balance" +\
                    " FROM " + tables + " WHERE account_id IN %s " + filters + " GROUP BY account_id")
         params = (tuple(accounts.ids),) + tuple(where_params)
         self.env.cr.execute(request, params)
@@ -30,7 +42,7 @@ class ReportTrialBalance(models.AbstractModel):
         account_res = []
         for account in accounts:
             res = dict((fn, 0.0) for fn in ['credit', 'debit', 'balance'])
-            currency = account.currency_id or self.env.company.currency_id
+            currency = account.currency_id and account.currency_id or self.env.company.currency_id
             res['code'] = account.code
             res['name'] = account.name
             if account.id in account_result:
@@ -54,43 +66,25 @@ class ReportTrialBalance(models.AbstractModel):
         docs = self.env[model].browse(self.env.context.get('active_ids', []))
         display_account = data['form'].get('display_account')
         accounts = docs if model == 'account.account' else self.env['account.account'].search([])
-
-        # 🔹 Get selected journals from wizard
-        journal_ids = data['form'].get('journal_ids', [])
-        journals = self.env['account.journal'].browse(journal_ids)
-
-        company_balances = []
-        for company in self.env.companies:
-            # journals for this company only
-            company_journals = journals.filtered(lambda j: j.company_id == company)
-            if not company_journals:
-                continue
-
-            context = dict(data['form'].get('used_context', {}))
-            context.update({
-                'allowed_company_ids': [company.id],
-                'force_company': company.id,
-                'active_test': False,
-            })
-
-            account_res = self.with_context(context)._get_accounts(accounts, display_account)
-            company_balances.append({
-                'company': company,
-                'accounts': account_res,
-                'journals': company_journals.mapped('code'),
-            })
-
+        context = data['form'].get('used_context')
         analytic_accounts = []
         if data['form'].get('analytic_account_ids'):
             analytic_account_ids = self.env['account.analytic.account'].browse(data['form'].get('analytic_account_ids'))
-            analytic_accounts = [a.name for a in analytic_account_ids]
-
+            context['analytic_account_ids'] = analytic_account_ids
+            analytic_accounts = [account.name for account in analytic_account_ids]
+        account_res = self.with_context(context)._get_accounts(accounts, display_account)
+        codes = []
+        if data['form'].get('journal_ids', False):
+            codes = [journal.code for journal in
+                     self.env['account.journal'].search(
+                         [('id', 'in', data['form']['journal_ids'])])]
         return {
             'doc_ids': self.ids,
             'doc_model': model,
             'data': data['form'],
             'docs': docs,
-            'company_balances': company_balances,
+            'print_journal': codes,
             'analytic_accounts': analytic_accounts,
             'time': time,
+            'Accounts': account_res,
         }
--- accounting_pdf_reports/wizard/account_trial_balance.py
+++ accounting_pdf_reports/wizard/account_trial_balance.py
@@ -1,31 +1,4 @@
-# from odoo import fields, models, api
-#
-#
-# class AccountBalanceReport(models.TransientModel):
-#     _name = 'account.balance.report'
-#     _inherit = "account.common.account.report"
-#     _description = 'Trial Balance Report'
-#
-#     journal_ids = fields.Many2many(
-#         'account.journal', 'account_balance_report_journal_rel',
-#         'account_id', 'journal_id',
-#         string='Journals', required=True, default=[]
-#     )
-#     analytic_account_ids = fields.Many2many(
-#         'account.analytic.account',
-#         'account_trial_balance_analytic_rel', string='Analytic Accounts'
-#     )
-#
-#     def _get_report_data(self, data):
-#         data = self.pre_print_report(data)
-#         records = self.env[data['model']].browse(data.get('ids', []))
-#         return records, data
-#
-#     def _print_report(self, data):
-#         records, data = self._get_report_data(data)
-#         return self.env.ref('accounting_pdf_reports.action_report_trial_balance').report_action(records, data=data)
-from lxml import etree
-from odoo import api, fields, models
+from odoo import fields, models, api
 
 
 class AccountBalanceReport(models.TransientModel):
@@ -36,38 +9,12 @@ class AccountBalanceReport(models.TransientModel):
     journal_ids = fields.Many2many(
         'account.journal', 'account_balance_report_journal_rel',
         'account_id', 'journal_id',
-        string='Journals', required=True, default=[],
-        domain=[]  # <– ensure no default restriction
+        string='Journals', required=True, default=[]
     )
     analytic_account_ids = fields.Many2many(
         'account.analytic.account',
         'account_trial_balance_analytic_rel', string='Analytic Accounts'
     )
-
-    # ---------- FIXED PART ----------
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
-        """Make the Journals field show journals from all companies the user can access."""
-        res = super().fields_view_get(view_id=view_id, view_type=view_type,
-                                      toolbar=toolbar, submenu=submenu)
-        if view_type == 'form' and res.get('arch'):
-            doc = etree.XML(res['arch'])
-
-            # ✅ Use user.company_ids (all allowed companies)
-            allowed_company_ids = self.env.user.company_ids.ids
-            if not allowed_company_ids:
-                allowed_company_ids = self.env['res.company'].search([]).ids
-
-            # ✅ Build domain for journals from all allowed companies
-            domain = "[('company_id', 'in', %s)]" % (allowed_company_ids,)
-
-            # ✅ Inject new domain into the field node
-            for node in doc.xpath("//field[@name='journal_ids']"):
-                node.set('domain', domain)
-
-            res['arch'] = etree.tostring(doc, encoding='unicode')
-        return res
-    # ---------- END FIXED PART ----------
 
     def _get_report_data(self, data):
         data = self.pre_print_report(data)
@@ -75,19 +22,5 @@ class AccountBalanceReport(models.TransientModel):
         return records, data
 
     def _print_report(self, data):
-        # Get selected companies from UI (multi-company top bar)
-        selected_company_ids = self.env.companies.ids
-
-        # Add them to the report context
-        data = self.pre_print_report(data)
-        records = self.env[data['model']].browse(data.get('ids', []))
-
-        # Add all selected companies to the report context
-        ctx = dict(self.env.context)
-        ctx.update({'allowed_company_ids': selected_company_ids, 'force_company': False})
-
-        # Use with_context so report action reads all companies
-        return self.env.ref(
-            'accounting_pdf_reports.action_report_trial_balance'
-        ).with_context(ctx).report_action(records, data=data)
-
+        records, data = self._get_report_data(data)
+        return self.env.ref('accounting_pdf_reports.action_report_trial_balance').report_action(records, data=data)

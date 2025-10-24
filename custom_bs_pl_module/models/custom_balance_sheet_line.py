8cf54af9409ac3bef8649c200cd895aae0fecaad
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-24 14:14:18 +0530

    custom

--- custom_bs_pl_module/models/custom_balance_sheet_line.py
+++ custom_bs_pl_module/models/custom_balance_sheet_line.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, api
 
 class CustomBalanceSheetLine(models.TransientModel):
     _name = 'custom.balance.sheet.line'
@@ -10,8 +10,6 @@ class CustomBalanceSheetLine(models.TransientModel):
     debit = fields.Monetary(string='Debit', currency_field='currency_id', readonly=True)
     credit = fields.Monetary(string='Credit', currency_field='currency_id', readonly=True)
     balance = fields.Monetary(string='Balance', currency_field='currency_id', readonly=True)
-    account_id = fields.Many2one('account.account', string='Account', readonly=True)
-
     currency_id = fields.Many2one(
         'res.currency',
         string='Currency',
@@ -20,20 +18,18 @@ class CustomBalanceSheetLine(models.TransientModel):
     )
 
     @api.model
-    def default_get(self, fields):
-        res = super().default_get(fields)
-        return res
-
     def action_view_ledger(self):
+        """Open general ledger view for this account"""
         self.ensure_one()
-        if not self.account_id:
-            return False
+        account = self.env['account.account'].search([('name', '=', self.name)], limit=1)
+        if not account:
+            return
+
         return {
-            'name': f'Ledger: {self.account_id.display_name}',
             'type': 'ir.actions.act_window',
+            'name': f'Ledger: {account.name}',
             'res_model': 'account.move.line',
             'view_mode': 'tree,form',
-            'domain': [('account_id', '=', self.account_id.id)],
-            'context': {'default_account_id': self.account_id.id},
-            'target': 'current',
+            'domain': [('account_id', '=', account.id)],
+            'context': {'default_account_id': account.id},
         }

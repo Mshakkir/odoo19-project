4bc94d10160e452b674f091bf312925e78c1b298
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-24 14:35:50 +0530

    odoomates

--- accounting_pdf_reports/report/report_tax.py
+++ accounting_pdf_reports/report/report_tax.py
@@ -1,3 +1,76 @@
+# from odoo import api, models, _
+# from odoo.exceptions import UserError
+#
+#
+# class ReportTax(models.AbstractModel):
+#     _name = 'report.accounting_pdf_reports.report_tax'
+#     _description = 'Tax Report'
+#
+#     @api.model
+#     def _get_report_values(self, docids, data=None):
+#         if not data.get('form'):
+#             raise UserError(_("Form content is missing, this report cannot be printed."))
+#         return {
+#             'data': data['form'],
+#             'lines': self.get_lines(data.get('form')),
+#         }
+#
+#
+#     def _sql_from_amls_one(self):
+#         sql = """SELECT "account_move_line".tax_line_id, COALESCE(SUM("account_move_line".debit-"account_move_line".credit), 0)
+#                     FROM %s
+#                     WHERE %s GROUP BY "account_move_line".tax_line_id"""
+#         return sql
+#
+#     def _sql_from_amls_two(self):
+#         sql = """SELECT r.account_tax_id, COALESCE(SUM("account_move_line".debit-"account_move_line".credit), 0)
+#                  FROM %s
+#                  INNER JOIN account_move_line_account_tax_rel r ON ("account_move_line".id = r.account_move_line_id)
+#                  INNER JOIN account_tax t ON (r.account_tax_id = t.id)
+#                  WHERE %s GROUP BY r.account_tax_id"""
+#         return sql
+#
+#     def _compute_from_amls(self, options, taxes):
+#         #compute the tax amount
+#         sql = self._sql_from_amls_one()
+#         tables, where_clause, where_params = self.env['account.move.line']._query_get()
+#         query = sql % (tables, where_clause)
+#         self.env.cr.execute(query, where_params)
+#         results = self.env.cr.fetchall()
+#         for result in results:
+#             if result[0] in taxes:
+#                 taxes[result[0]]['tax'] = abs(result[1])
+#
+#         #compute the net amount
+#         sql2 = self._sql_from_amls_two()
+#         query = sql2 % (tables, where_clause)
+#         self.env.cr.execute(query, where_params)
+#         results = self.env.cr.fetchall()
+#         for result in results:
+#             if result[0] in taxes:
+#                 taxes[result[0]]['net'] = abs(result[1])
+#
+#     @api.model
+#     def get_lines(self, options):
+#         taxes = {}
+#         for tax in self.env['account.tax'].search([('type_tax_use', '!=', 'none')]):
+#             if tax.children_tax_ids:
+#                 for child in tax.children_tax_ids:
+#                     if child.type_tax_use != 'none':
+#                         continue
+#                     taxes[child.id] = {'tax': 0, 'net': 0, 'name': child.name, 'type': tax.type_tax_use}
+#             else:
+#                 taxes[tax.id] = {'tax': 0, 'net': 0, 'name': tax.name, 'type': tax.type_tax_use}
+#         self.with_context(date_from=options['date_from'], date_to=options['date_to'],
+#                           state=options['target_move'],
+#                           strict_range=True)._compute_from_amls(options, taxes)
+#         groups = dict((tp, []) for tp in ['sale', 'purchase'])
+#         for tax in taxes.values():
+#             # Include taxes that have either tax amount OR base (net) amount
+#             if tax['tax'] or tax['net']:
+#                 groups[tax['type']].append(tax)
+#         return groups
+
 from odoo import api, models, _
 from odoo.exceptions import UserError
 
@@ -17,8 +90,9 @@ class ReportTax(models.AbstractModel):
 
     def _sql_from_amls_one(self):
         sql = """SELECT "account_move_line".tax_line_id, COALESCE(SUM("account_move_line".debit-"account_move_line".credit), 0)
-                    FROM %s
-                    WHERE %s GROUP BY "account_move_line".tax_line_id"""
+                 FROM %s
+                 WHERE %s
+                 GROUP BY "account_move_line".tax_line_id"""
         return sql
 
     def _sql_from_amls_two(self):
@@ -26,13 +100,24 @@ class ReportTax(models.AbstractModel):
                  FROM %s
                  INNER JOIN account_move_line_account_tax_rel r ON ("account_move_line".id = r.account_move_line_id)
                  INNER JOIN account_tax t ON (r.account_tax_id = t.id)
-                 WHERE %s GROUP BY r.account_tax_id"""
+                 WHERE %s
+                 GROUP BY r.account_tax_id"""
         return sql
 
     def _compute_from_amls(self, options, taxes):
-        # compute the tax amount
-        sql = self._sql_from_amls_one()
+        warehouse_id = options.get('warehouse_id')
         tables, where_clause, where_params = self.env['account.move.line']._query_get()
+
+        # 🔹 Filter by analytic account of selected warehouse
+        if warehouse_id:
+            warehouse = self.env['stock.warehouse'].browse(warehouse_id)
+            analytic_account = warehouse.analytic_account_id
+            if analytic_account:
+                where_clause += ' AND account_move_line.analytic_account_id = %s'
+                where_params.append(analytic_account.id)
+
+        # 🔸 Compute tax amount
+        sql = self._sql_from_amls_one()
         query = sql % (tables, where_clause)
         self.env.cr.execute(query, where_params)
         results = self.env.cr.fetchall()
@@ -40,7 +125,7 @@ class ReportTax(models.AbstractModel):
             if result[0] in taxes:
                 taxes[result[0]]['tax'] = abs(result[1])
 
-        # compute the net amount
+        # 🔸 Compute net amount
         sql2 = self._sql_from_amls_two()
         query = sql2 % (tables, where_clause)
         self.env.cr.execute(query, where_params)
@@ -60,35 +145,16 @@ class ReportTax(models.AbstractModel):
                     taxes[child.id] = {'tax': 0, 'net': 0, 'name': child.name, 'type': tax.type_tax_use}
             else:
                 taxes[tax.id] = {'tax': 0, 'net': 0, 'name': tax.name, 'type': tax.type_tax_use}
-        self.with_context(date_from=options['date_from'], date_to=options['date_to'],
-                          state=options['target_move'],
-                          strict_range=True)._compute_from_amls(options, taxes)
+
+        self.with_context(
+            date_from=options['date_from'],
+            date_to=options['date_to'],
+            state=options['target_move'],
+            strict_range=True
+        )._compute_from_amls(options, taxes)
+
         groups = dict((tp, []) for tp in ['sale', 'purchase'])
         for tax in taxes.values():
-            # Include taxes that have either tax amount OR base (net) amount
             if tax['tax'] or tax['net']:
                 groups[tax['type']].append(tax)
         return groups
-        # ✅ FIX: Analytic filter using subquery (Odoo 19+ compatible)
-        analytic_account_ids = self.env.context.get('analytic_account_ids')
-        analytic_filter = ""
-        analytic_params = ()
-
-        if analytic_account_ids:
-            analytic_filter = (
-                " AND (id IN (SELECT move_id FROM account_analytic_line WHERE account_id IN %s)"
-                " OR id NOT IN (SELECT move_id FROM account_analytic_line))"
-            )
-
-            analytic_params = (tuple(a.id for a in analytic_account_ids),)
-
-        filters = " AND ".join(wheres)
-
-        # ✅ Safe SQL query
-        request = (
-                "SELECT account_id AS id, SUM(debit) AS debit, SUM(credit) AS credit, "
-                "(SUM(debit) - SUM(credit)) AS balance "
-                f"FROM {tables} "
-                "WHERE account_id IN %s " + filters + analytic_filter +
-                " GROUP BY account_id"
-        )
--- accounting_pdf_reports/wizard/account_tax_report.py
+++ accounting_pdf_reports/wizard/account_tax_report.py
@@ -1,3 +1,23 @@
+# from odoo import models, api, fields
+# from datetime import date
+#
+#
+# class AccountTaxReport(models.TransientModel):
+#     _name = 'account.tax.report.wizard'
+#     _inherit = "account.common.report"
+#     _description = 'Tax Report'
+#
+#     date_from = fields.Date(
+#         string='Date From', required=True,
+#         default=lambda self: fields.Date.to_string(date.today().replace(day=1))
+#     )
+#     date_to = fields.Date(
+#         string='Date To', required=True,
+#         default=lambda self: fields.Date.to_string(date.today())
+#     )
+#
+#     def _print_report(self, data):
+#         return self.env.ref('accounting_pdf_reports.action_report_account_tax').report_action(self, data=data)
 from odoo import models, api, fields
 from datetime import date
 
@@ -7,10 +27,6 @@ class AccountTaxReport(models.TransientModel):
     _inherit = "account.common.report"
     _description = 'Tax Report'
 
-    analytic_account_ids = fields.Many2many(
-        'account.analytic.account',
-        'account_trial_balance_analytic_rel', string='Warehouse(Analytic Accounts) '
-    )
     date_from = fields.Date(
         string='Date From', required=True,
         default=lambda self: fields.Date.to_string(date.today().replace(day=1))
@@ -20,5 +36,16 @@ class AccountTaxReport(models.TransientModel):
         default=lambda self: fields.Date.to_string(date.today())
     )
 
+    # 🔹 NEW: Warehouse filter
+    warehouse_id = fields.Many2one('stock.warehouse', string="Warehouse")
+
     def _print_report(self, data):
+        data = {
+            'form': {
+                'date_from': self.date_from,
+                'date_to': self.date_to,
+                'target_move': self.target_move,
+                'warehouse_id': self.warehouse_id.id if self.warehouse_id else False,
+            }
+        }
         return self.env.ref('accounting_pdf_reports.action_report_account_tax').report_action(self, data=data)

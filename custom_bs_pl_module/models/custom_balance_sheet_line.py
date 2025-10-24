7363acd1a02fabb87aca8f0b4038544e16ab981d
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-24 12:11:18 +0530

    custom

--- custom_bs_pl_module/models/custom_balance_sheet_line.py
+++ custom_bs_pl_module/models/custom_balance_sheet_line.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class CustomBalanceSheetLine(models.TransientModel):
     _name = 'custom.balance.sheet.line'
@@ -6,11 +6,11 @@ class CustomBalanceSheetLine(models.TransientModel):
     _order = 'account_type, name'
 
     name = fields.Char(string='Account', required=True)
-    account_id = fields.Many2one('account.account', string='Account')  # 👈 Added this
     account_type = fields.Char(string='Type', readonly=True)
     debit = fields.Monetary(string='Debit', currency_field='currency_id', readonly=True)
     credit = fields.Monetary(string='Credit', currency_field='currency_id', readonly=True)
     balance = fields.Monetary(string='Balance', currency_field='currency_id', readonly=True)
+    account_id = fields.Many2one('account.account', string='Account', readonly=True)
 
     currency_id = fields.Many2one(
         'res.currency',
@@ -19,11 +19,22 @@ class CustomBalanceSheetLine(models.TransientModel):
         readonly=True,
     )
 
+    @api.model
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        return res
+
     def action_view_ledger(self):
-        """Open general ledger entries for the account"""
+        """Open ledger view filtered by this account"""
         self.ensure_one()
         if not self.account_id:
             return False
-        action = self.env.ref('account.action_move_line_select').read()[0]
-        action['domain'] = [('account_id', '=', self.account_id.id)]
-        return action
+        return {
+            'name': f'Ledger: {self.account_id.display_name}',
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move.line',
+            'view_mode': 'tree,form',
+            'domain': [('account_id', '=', self.account_id.id)],
+            'context': {'default_account_id': self.account_id.id},
+            'target': 'current',
+        }

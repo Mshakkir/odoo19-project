1b6317fb520a200aabb46d293a7ec21b6a15006e
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-24 11:54:47 +0530

    custom

--- custom_bs_pl_module/models/custom_balance_sheet_line.py
+++ custom_bs_pl_module/models/custom_balance_sheet_line.py
@@ -6,6 +6,7 @@ class CustomBalanceSheetLine(models.TransientModel):
     _order = 'account_type, name'
 
     name = fields.Char(string='Account', required=True)
+    account_id = fields.Many2one('account.account', string='Account')  # 👈 Added this
     account_type = fields.Char(string='Type', readonly=True)
     debit = fields.Monetary(string='Debit', currency_field='currency_id', readonly=True)
     credit = fields.Monetary(string='Credit', currency_field='currency_id', readonly=True)
@@ -17,3 +18,12 @@ class CustomBalanceSheetLine(models.TransientModel):
         default=lambda self: self.env.company.currency_id,
         readonly=True,
     )
+
+    def action_view_ledger(self):
+        """Open general ledger entries for the account"""
+        self.ensure_one()
+        if not self.account_id:
+            return False
+        action = self.env.ref('account.action_move_line_select').read()[0]
+        action['domain'] = [('account_id', '=', self.account_id.id)]
+        return action

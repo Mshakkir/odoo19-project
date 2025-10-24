f423161899f19335e81876ba7ba8374a9b5da7ae
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-24 10:50:29 +0530

    custom

--- custom_bs_pl_module/models/custom_balance_sheet_line.py
+++ custom_bs_pl_module/models/custom_balance_sheet_line.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class CustomBalanceSheetLine(models.TransientModel):
     _name = 'custom.balance.sheet.line'
@@ -8,7 +8,6 @@ class CustomBalanceSheetLine(models.TransientModel):
     name = fields.Char(string='Account', required=True)
     account_id = fields.Many2one('account.account', string='Account Ref', readonly=True)
 
-    # Use Char instead of Selection to allow dynamic account type values (e.g. 'asset_cash', 'asset_fixed')
     account_type = fields.Char(string='Type', readonly=True)
 
     debit = fields.Monetary(string='Debit', currency_field='currency_id', readonly=True)

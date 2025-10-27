4dbc7f6a5da3354a89f8f532edebfb919016eb21
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-27 15:28:10 +0530

    odoomates

--- account_tax_report_warehouse/__manifest__.py
+++ account_tax_report_warehouse/__manifest__.py
@@ -3,9 +3,10 @@
     'version': '1.0',
     'summary': 'Adds analytic/warehouse filter to Tax Report',
     'author': 'Jayaraj KP',
-    'depends': ['accounting_pdf_reports','analytic'],  # ✅ depends on Odoo Mates module
+    'depends': ['accounting_pdf_reports', 'analytic'],  # ✅ depends on Odoo Mates module
     'data': [
         'views/account_tax_report_wizard_view.xml',
+        'views/tax_report_detail_line_view.xml',
     ],
     'license': 'LGPL-3',
     'installable': True,
--- custom_account_report/__manifest__.py
+++ custom_account_report/__manifest__.py
@@ -6,7 +6,7 @@
     'depends': ['account', 'accounting_pdf_reports'],
     'data': [
         'views/tax_report_wizard_inherit.xml',
-        # 'views/tax_report_detail_line_views.xml',
+        'views/tax_report_detail_line_views.xml',
     ],
     'installable': True,
     'application': False,

790cd9d53b7a3512ef54761701c4e10c248c00d4
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-27 14:57:15 +0530

    odoomates

--- account_tax_report_warehouse/models/account_tax_report_wizard_inherit.py
+++ account_tax_report_warehouse/models/account_tax_report_wizard_inherit.py
@@ -1,33 +1,3 @@
-# from odoo import api, fields, models
-#
-# class AccountTaxReportWizard(models.TransientModel):
-#     _inherit = "account.tax.report.wizard"
-#
-#     analytic_account_ids = fields.Many2many(
-#         'account.analytic.account',
-#         string="Analytic Accounts (Warehouses)",
-#         help="Filter Tax Report based on selected warehouse analytic accounts."
-#     )
-#
-#     @api.model
-#     def _get_report_values(self, docids, data=None):
-#         """Inject analytic filter into report context."""
-#         res = super()._get_report_values(docids, data=data)
-#         if data and data.get('form') and data['form'].get('analytic_account_ids'):
-#             analytic_ids = data['form']['analytic_account_ids']
-#             res['data']['form']['analytic_account_ids'] = analytic_ids
-#         return res
-# def _print_report(self):
-#     form_data = self.read(['date_from', 'date_to', 'target_move', 'analytic_account_ids'])[0]
-#
-#     # Normalize analytic_account_ids
-#     analytic_ids = form_data.get('analytic_account_ids', [])
-#     if analytic_ids and isinstance(analytic_ids[0], (tuple, list)):
-#         analytic_ids = analytic_ids[0][2]  # get IDs from [(6, 0, [ids])]
-#     form_data['analytic_account_ids'] = analytic_ids
-#
-#     return self.env.ref('accounting_pdf_reports.action_report_account_tax').report_action(self, data={'form': form_data})
-#
 from odoo import api, fields, models
 
 
--- custom_account_report/models/tax_report_detail.py
+++ custom_account_report/models/tax_report_detail.py
@@ -1,34 +1,34 @@
-# models/tax_report_detail.py
-from odoo import models, fields, api
-
-class TaxReportDetail(models.TransientModel):
-    _name = 'tax.report.detail'
-    _description = 'Tax Report Detail'
-
-    tax_id = fields.Many2one('account.tax', string="Tax")
-    invoice_id = fields.Many2one('account.move', string="Invoice")
-    partner_id = fields.Many2one('res.partner', string="Customer/Vendor")
-    date = fields.Date(string="Invoice Date")
-    amount = fields.Monetary(string="Amount", currency_field='currency_id')
-    currency_id = fields.Many2one('res.currency', string="Currency")
-
-    @api.model
-    def get_tax_details(self, tax_id, date_from=None, date_to=None):
-        domain = [('tax_line_ids.tax_id', '=', tax_id)]
-        if date_from:
-            domain.append(('invoice_date', '>=', date_from))
-        if date_to:
-            domain.append(('invoice_date', '<=', date_to))
-        moves = self.env['account.move'].search(domain)
-        lines = []
-        for move in moves:
-            for line in move.tax_line_ids.filtered(lambda l: l.tax_id.id == tax_id):
-                lines.append({
-                    'tax_id': tax_id,
-                    'invoice_id': move.id,
-                    'partner_id': move.partner_id.id,
-                    'date': move.invoice_date,
-                    'amount': line.amount,
-                    'currency_id': move.currency_id.id,
-                })
-        return lines
+# # models/tax_report_detail.py
+# from odoo import models, fields, api
+#
+# class TaxReportDetail(models.TransientModel):
+#     _name = 'tax.report.detail'
+#     _description = 'Tax Report Detail'
+#
+#     tax_id = fields.Many2one('account.tax', string="Tax")
+#     invoice_id = fields.Many2one('account.move', string="Invoice")
+#     partner_id = fields.Many2one('res.partner', string="Customer/Vendor")
+#     date = fields.Date(string="Invoice Date")
+#     amount = fields.Monetary(string="Amount", currency_field='currency_id')
+#     currency_id = fields.Many2one('res.currency', string="Currency")
+#
+#     @api.model
+#     def get_tax_details(self, tax_id, date_from=None, date_to=None):
+#         domain = [('tax_line_ids.tax_id', '=', tax_id)]
+#         if date_from:
+#             domain.append(('invoice_date', '>=', date_from))
+#         if date_to:
+#             domain.append(('invoice_date', '<=', date_to))
+#         moves = self.env['account.move'].search(domain)
+#         lines = []
+#         for move in moves:
+#             for line in move.tax_line_ids.filtered(lambda l: l.tax_id.id == tax_id):
+#                 lines.append({
+#                     'tax_id': tax_id,
+#                     'invoice_id': move.id,
+#                     'partner_id': move.partner_id.id,
+#                     'date': move.invoice_date,
+#                     'amount': line.amount,
+#                     'currency_id': move.currency_id.id,
+#                 })
+#         return lines

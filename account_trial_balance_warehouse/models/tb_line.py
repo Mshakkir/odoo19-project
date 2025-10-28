fb236e5d81b32b2c49fad8e88d407f614cef56b8
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-28 14:23:01 +0530

    odoomates

---
+++ account_trial_balance_warehouse/models/tb_line.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class TrialBalanceLine(models.TransientModel):
+    _name = 'trial.balance.line'
+    _description = 'Trial Balance Line'
+
+    wizard_id = fields.Many2one('account.balance.report', string='Wizard')
+    account_id = fields.Many2one('account.account', string='Account')
+    opening_balance = fields.Monetary(string='Opening Balance', currency_field='company_currency_id')
+    debit = fields.Monetary(string='Debit', currency_field='company_currency_id')
+    credit = fields.Monetary(string='Credit', currency_field='company_currency_id')
+    ending_balance = fields.Monetary(string='Ending Balance', currency_field='company_currency_id')
+    company_currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.company.currency_id)

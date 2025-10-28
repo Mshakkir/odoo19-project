8aedc6c9a17f4f9faeb167282b8a97d77f1bae98
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-28 16:54:24 +0530

    custom

--- custom_bs_pl_module/report/report_balance_sheet_pdf.py
+++ custom_bs_pl_module/report/report_balance_sheet_pdf.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-from odoo import models, api
-
-class ReportBalanceSheetPDF(models.AbstractModel):
-    _name = 'report.custom_bs_pl_module.report_balance_sheet_pdf'
-    _description = 'Custom Balance Sheet PDF Report'
-
-    @api.model
-    def _get_report_values(self, docids, data=None):
-        docs = self.env['accounting.report'].browse(docids)
-        return {
-            'doc_ids': docids,
-            'doc_model': 'accounting.report',
-            'docs': docs,
-            'data': data,
-        }
+# # -*- coding: utf-8 -*-
+# from odoo import models, api
+#
+# class ReportBalanceSheetPDF(models.AbstractModel):
+#     _name = 'report.custom_bs_pl_module.report_balance_sheet_pdf'
+#     _description = 'Custom Balance Sheet PDF Report'
+#
+#     @api.model
+#     def _get_report_values(self, docids, data=None):
+#         docs = self.env['accounting.report'].browse(docids)
+#         return {
+#             'doc_ids': docids,
+#             'doc_model': 'accounting.report',
+#             'docs': docs,
+#             'data': data,
+#         }
--- custom_bs_pl_module/report/report_profit_loss_pdf.py
+++ custom_bs_pl_module/report/report_profit_loss_pdf.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-from odoo import models, api
-
-class ReportProfitLossPDF(models.AbstractModel):
-    _name = 'report.custom_bs_pl_module.report_profit_loss_pdf'
-    _description = 'Custom Profit and Loss PDF Report'
-
-    @api.model
-    def _get_report_values(self, docids, data=None):
-        docs = self.env['accounting.report'].browse(docids)
-        return {
-            'doc_ids': docids,
-            'doc_model': 'accounting.report',
-            'docs': docs,
-            'data': data,
-        }
+# # -*- coding: utf-8 -*-
+# from odoo import models, api
+#
+# class ReportProfitLossPDF(models.AbstractModel):
+#     _name = 'report.custom_bs_pl_module.report_profit_loss_pdf'
+#     _description = 'Custom Profit and Loss PDF Report'
+#
+#     @api.model
+#     def _get_report_values(self, docids, data=None):
+#         docs = self.env['accounting.report'].browse(docids)
+#         return {
+#             'doc_ids': docids,
+#             'doc_model': 'accounting.report',
+#             'docs': docs,
+#             'data': data,
+#         }

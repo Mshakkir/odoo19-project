6270576c598ae604d683e80476b20ef8a8f94b21
shakkir <shakkirtshakkirt86827@gmail.com>
2025-10-29 16:15:08 +0530

    custom

--- custom_bs_pl_module/report/report_balance_sheet_pdf.py
+++ custom_bs_pl_module/report/report_balance_sheet_pdf.py
@@ -7,7 +7,26 @@ class ReportBalanceSheetPDF(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
+        """Pass all accounting report data (including warehouse/analytic info) to QWeb PDF."""
         docs = self.env['accounting.report'].browse(docids)
+
+        # ✅ Prepare warehouse analytic info (single or multiple)
+        warehouse_label = ''
+        for doc in docs:
+            # If Many2many (multiple warehouses)
+            if hasattr(doc, 'warehouse_analytic_ids') and doc.warehouse_analytic_ids:
+                warehouse_label = ', '.join(doc.warehouse_analytic_ids.mapped('name'))
+
+            # If single field
+            elif hasattr(doc, 'warehouse_analytic_id') and doc.warehouse_analytic_id:
+                warehouse_label = doc.warehouse_analytic_id.name
+
+        # ✅ Inject the label into report data
+        if data is None:
+            data = {}
+
+        data['warehouse_analytic_label'] = warehouse_label
+
         return {
             'doc_ids': docids,
             'doc_model': 'accounting.report',

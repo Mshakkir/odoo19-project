c8fd53dd68b7fa32bf00d0b794b69ea711a65695
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-30 14:44:02 +0530

    odoomates

---
+++ analytic_general_ledger/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Analytic Based General Ledger',
+    'version': '1.0',
+    'depends': ['accounting_pdf_reports', 'analytic'],
+    'author': 'Your Name',
+    'category': 'Accounting',
+    'summary': 'General Ledger filtered by Analytic Accounts',
+    'description': 'Adds analytic account filter and column in General Ledger report.',
+    'data': [
+        'views/general_ledger_analytic_view.xml',
+        'report/report_general_ledger_analytic.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

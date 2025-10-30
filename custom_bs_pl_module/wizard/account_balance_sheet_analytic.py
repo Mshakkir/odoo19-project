4b0cd1e48b90776480892a5d3548d311e7ca17f6
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-30 15:26:08 +0530

    odoomates

--- custom_bs_pl_module/wizard/account_balance_sheet_analytic.py
+++ custom_bs_pl_module/wizard/account_balance_sheet_analytic.py
@@ -11,26 +11,23 @@ class AccountingReportAnalytic(models.TransientModel):
     warehouse_analytic_ids = fields.Many2many(
         'account.analytic.account',
         string='Warehouse Analytic Accounts',
-        help="Filter balance sheet by analytic accounts (warehouses)."
+        help="Filter Balance Sheet / P&L by analytic accounts (warehouses)."
     )
 
-    def _get_filters(self):
+    def _get_filter_values(self):
         """Extend filter values to include analytic accounts."""
-        res = super()._get_filters()
+        # call the existing filter method in Odoo Mates' accounting.report
+        res = super()._get_filter_values()
         res['warehouse_analytic_ids'] = self.warehouse_analytic_ids.ids
         return res
 
     def _print_report(self, data):
         """Override report printing to include analytic filters."""
-        data = self._get_filters()
-        data.update({
-            'form': self.read([
-                'date_from',
-                'date_to',
-                'warehouse_analytic_ids',
-            ])[0]
-        })
-        # ✅ Use existing Odoo Mates Balance Sheet action
-        return self.env.ref('accounting_pdf_reports.action_report_financial') \
-            .with_context(landscape=True) \
-            .report_action(self, data=data)
+        data = self._get_filter_values()
+
+        _logger.info("Printing financial report with analytic filters: %s", data)
+
+        # use Odoo Mates' report action reference
+        return self.env.ref(
+            'accounting_pdf_reports.action_report_financial'
+        ).with_context(landscape=True).report_action(self, data=data)

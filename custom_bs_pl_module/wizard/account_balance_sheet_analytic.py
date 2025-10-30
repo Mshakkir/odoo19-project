da380916d87ba68b355d6932f311a940d976849a
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-30 15:19:52 +0530

    odoomates

---
+++ custom_bs_pl_module/wizard/account_balance_sheet_analytic.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class AccountingReportAnalytic(models.TransientModel):
+    _inherit = 'accounting.report'
+
+    warehouse_analytic_ids = fields.Many2many(
+        'account.analytic.account',
+        string='Warehouse Analytic Accounts',
+        help="Filter balance sheet by analytic accounts (warehouses)."
+    )
+
+    def _get_filter_values(self):
+        """Extend filter values to include analytic accounts."""
+        res = super()._get_filter_values()
+        res['warehouse_analytic_ids'] = self.warehouse_analytic_ids.ids
+        return res
+
+    def _print_report(self, data):
+        """Override report printing to include analytic filters."""
+        data = self._get_filter_values()
+        # 👇 Use existing Odoo Mates balance sheet report action
+        return self.env.ref('accounting_pdf_reports.action_report_financial').with_context(landscape=True).report_action(self, data=data)

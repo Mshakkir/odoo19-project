4736cfc235ccee81dbb2101d1b4da24fa05e9812
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-30 15:37:05 +0530

    odoomates

--- custom_bs_pl_module/wizard/account_balance_sheet_analytic.py
+++ custom_bs_pl_module/wizard/account_balance_sheet_analytic.py
@@ -4,35 +4,40 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-
 class AccountingReportAnalytic(models.TransientModel):
     _inherit = 'accounting.report'
 
     warehouse_analytic_ids = fields.Many2many(
         'account.analytic.account',
         string='Warehouse Analytic Accounts',
-        help="Filter Balance Sheet / P&L by analytic accounts (warehouses)."
+        help="Filter balance sheet by analytic accounts (warehouses)."
     )
 
     def _get_filter_values(self):
-        """Build a dictionary in the format expected by accounting_pdf_reports."""
-        form_data = {
-            'date_from': self.date_from,
-            'date_to': self.date_to,
-            'target_move': self.target_move,
-            'warehouse_analytic_ids': self.warehouse_analytic_ids.ids,
-            'company_id': self.company_id.id if self.company_id else False,
-            # 👇 REQUIRED FIELD for accounting_pdf_reports
-            'account_report_id': [self.account_report_id.id] if self.account_report_id else False,
-        }
-        return {'form': form_data}
+        """Extend filter values to include analytic accounts."""
+        res = super()._get_filter_values()
+        res['warehouse_analytic_ids'] = self.warehouse_analytic_ids.ids
+        return res
+
+    def _build_contexts(self, data):
+        """Extend the default context with warehouse analytic filters."""
+        result = super()._build_contexts(data)
+        result['warehouse_analytic_ids'] = data.get('warehouse_analytic_ids', [])
+        return result
 
     def _print_report(self, data):
-        """Override report printing to include analytic filters."""
-        data = self._get_filter_values()
-        _logger.info("Printing report with filters: %s", data)
+        """
+        Properly pass data to Odoo Mates accounting_pdf_reports action.
+        """
+        # Get original filter values
+        data = self.pre_print_report(data)
+        data['form'] = self.read()[0]  # Odoo Mates report expects 'form' dict
+        data['form'].update(self._get_filter_values())
 
-        # ✅ Use existing Odoo Mates balance sheet action
-        return self.env.ref(
-            'accounting_pdf_reports.action_report_financial'
-        ).with_context(landscape=True).report_action(self, data=data)
+        # Include analytic filters in context
+        data['form']['warehouse_analytic_ids'] = self.warehouse_analytic_ids.ids
+
+        _logger.info("✅ Sending data to report: %s", data)
+
+        # Use Odoo Mates report action
+        return self.env.ref('accounting_pdf_reports.action_report_financial').report_action(self, data=data)

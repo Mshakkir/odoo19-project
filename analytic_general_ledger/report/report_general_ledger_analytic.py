07ece2d7fb91f2be9813eeb4e91d2c90e4ab6ff8
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-10-31 15:31:24 +0530

    odoomates

--- analytic_general_ledger/report/report_general_ledger_analytic.py
+++ analytic_general_ledger/report/report_general_ledger_analytic.py
@@ -1,4 +1,7 @@
+import time
 from odoo import api, models, _
+from odoo.exceptions import UserError
+
 
 class ReportGeneralLedgerAnalytic(models.AbstractModel):
     _name = 'report.analytic_general_ledger.report_general_ledger_analytic'
@@ -27,24 +30,23 @@ class ReportGeneralLedgerAnalytic(models.AbstractModel):
         filters = " AND " + where_clause if where_clause else ""
         filters = filters.replace('account_move_line__move_id', 'm').replace('account_move_line', 'l')
 
-        sql = ('''
-            SELECT l.id AS lid, l.account_id AS account_id, 
-                   l.date AS ldate, j.code AS lcode, l.currency_id, 
-                   l.amount_currency, aa.name AS analytic_account,
-                   l.ref AS lref, l.name AS lname, COALESCE(l.debit,0) AS debit, 
-                   COALESCE(l.credit,0) AS credit, 
-                   (l.debit - l.credit) AS balance,
-                   m.name AS move_name, c.symbol AS currency_code, 
-                   p.name AS partner_name
-              FROM account_move_line l
-              JOIN account_move m ON (l.move_id=m.id)
-         LEFT JOIN res_currency c ON (l.currency_id=c.id)
-         LEFT JOIN res_partner p ON (l.partner_id=p.id)
-              JOIN account_journal j ON (l.journal_id=j.id)
-              JOIN account_account acc ON (l.account_id = acc.id)
-         LEFT JOIN account_analytic_account aa ON (l.analytic_account_id = aa.id)
-             WHERE l.account_id IN %s ''' + filters + '''
-          ORDER BY ''' + sql_sort)
+        sql = ('''SELECT l.id AS lid, l.account_id AS account_id, 
+            l.date AS ldate, j.code AS lcode, l.currency_id, 
+            l.amount_currency, aa.name AS analytic_account_id,
+            l.ref AS lref, l.name AS lname, COALESCE(l.debit,0) AS debit, 
+            COALESCE(l.credit,0) AS credit, 
+            (l.debit - l.credit) AS balance,
+            m.name AS move_name, c.symbol AS currency_code, 
+            p.name AS partner_name
+            FROM account_move_line l
+            JOIN account_move m ON (l.move_id=m.id)
+            LEFT JOIN res_currency c ON (l.currency_id=c.id)
+            LEFT JOIN res_partner p ON (l.partner_id=p.id)
+            JOIN account_journal j ON (l.journal_id=j.id)
+            JOIN account_account acc ON (l.account_id = acc.id)
+            LEFT JOIN account_analytic_account aa ON (l.analytic_account_id = aa.id)
+            WHERE l.account_id IN %s ''' + filters + '''
+            ORDER BY ''' + sql_sort)
 
         params = (tuple(accounts.ids),) + tuple(where_params)
         cr.execute(sql, params)
@@ -54,14 +56,9 @@ class ReportGeneralLedgerAnalytic(models.AbstractModel):
 
         account_res = []
         for account in accounts:
-            res = {
-                'code': account.code,
-                'name': account.name,
-                'debit': 0.0,
-                'credit': 0.0,
-                'balance': 0.0,
-                'move_lines': move_lines[account.id],
-            }
+            res = {'code': account.code, 'name': account.name,
+                   'debit': 0.0, 'credit': 0.0, 'balance': 0.0,
+                   'move_lines': move_lines[account.id]}
             for line in res['move_lines']:
                 res['debit'] += line['debit']
                 res['credit'] += line['credit']
@@ -74,39 +71,5 @@ class ReportGeneralLedgerAnalytic(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         res = super()._get_report_values(docids, data)
         res['analytic_account_ids'] = self.env['account.analytic.account'].browse(
-            data['form'].get('analytic_account_ids', [])
-        )
+            data['form'].get('analytic_account_ids', []))
         return res
-
-
-# ============================
-# Safe separate transient class for action
-# ============================
-
-from odoo import models
-
-class GeneralLedgerWizard(models.TransientModel):
-    _inherit = 'account.report.general.ledger'
-
-    def action_show_details(self):
-        """Open detailed journal items"""
-        try:
-            tree_view = self.env.ref('account.view_move_line_tree').id
-            form_view = self.env.ref('account.view_move_line_form').id
-        except ValueError:
-            # fallback if the references are missing (custom Odoo versions)
-            tree_view = False
-            form_view = False
-
-        return {
-            'type': 'ir.actions.act_window',
-            'name': _('General Ledger Details'),
-            'res_model': 'account.move.line',
-            'domain': [],
-            'target': 'current',
-            'views': [
-                (tree_view, 'tree'),
-                (form_view, 'form')
-            ] if tree_view and form_view else [],
-            'view_mode': 'tree,form',
-        }

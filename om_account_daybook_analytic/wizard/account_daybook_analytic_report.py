ea503fadf8a0131cbe7e8ebb97e2fd938e051100
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-03 15:11:35 +0530

    odoomates

--- om_account_daybook_analytic/wizard/account_daybook_analytic_report.py
+++ om_account_daybook_analytic/wizard/account_daybook_analytic_report.py
@@ -1,94 +1,45 @@
-from odoo import fields, models, api, _
-from odoo.exceptions import UserError
-from datetime import date
+from odoo import api, fields, models
 
 
-class AccountDayBookAnalyticReport(models.TransientModel):
-    _name = "account.daybook.analytic.report"
-    _description = "Day Book Report with Analytic Accounts"
+class AccountDaybookReportWizard(models.TransientModel):
+    _name = 'report.om_daybook_analytic.report_daybook_analytic_combined'
+    _description = 'Daybook Analytic Report Wizard'
 
-    date_from = fields.Date(
-        string='Start Date',
-        default=date.today(),
-        required=True
-    )
-    date_to = fields.Date(
-        string='End Date',
-        default=date.today(),
-        required=True
-    )
-    target_move = fields.Selection(
-        [('posted', 'Posted Entries'), ('all', 'All Entries')],
-        string='Target Moves',
-        required=True,
-        default='posted'
-    )
+    date_from = fields.Date(string='Start Date', required=True)
+    date_to = fields.Date(string='End Date', required=True)
+    target_move = fields.Selection([
+        ('all', 'All Entries'),
+        ('posted', 'Posted Entries')
+    ], string='Target Moves', default='all', required=True)
     journal_ids = fields.Many2many(
-        'account.journal',
-        string='Journals',
-        required=True,
-        default=lambda self: self.env['account.journal'].search([])
-    )
+        'account.journal', string='Journals', required=False)
+    company_id = fields.Many2one(
+        'res.company', string='Company', required=True,
+        default=lambda self: self.env.company)
     analytic_account_ids = fields.Many2many(
-        'account.analytic.account',
-        'daybook_analytic_account_rel',
-        'daybook_id',
-        'analytic_account_id',
-        string='Analytic Accounts'
-    )
-    report_type = fields.Selection(
-        [
-            ('separate', 'Separate Reports (One per Analytic Account)'),
-            ('combined', 'Combined Report (All Analytic Accounts Together)'),
-            ('all', 'All Entries (No Analytic Filter)')
-        ],
-        string='Report Type',
-        required=True,
-        default='combined'
-    )
+        'account.analytic.account', string='Analytic Accounts')
 
-    @api.constrains('date_from', 'date_to')
-    def _check_dates(self):
-        for record in self:
-            if record.date_from > record.date_to:
-                raise UserError(_('Start Date must be before End Date.'))
+    def _prepare_report_data(self):
+        """Prepare the data dict to pass to the report."""
+        return {
+            'date_from': self.date_from,
+            'date_to': self.date_to,
+            'target_move': self.target_move,
+            'journal_ids': self.journal_ids.ids,
+            'company_id': self.company_id.id,
+            'analytic_account_ids': self.analytic_account_ids.ids,
+        }
 
-    def _build_comparison_context(self, data):
-        result = {}
-        result['journal_ids'] = data['form'].get('journal_ids', False)
-        result['state'] = data['form'].get('target_move', '')
-        result['date_from'] = data['form']['date_from']
-        result['date_to'] = data['form']['date_to']
-        result['analytic_account_ids'] = data['form'].get('analytic_account_ids', [])
-        result['report_type'] = data['form'].get('report_type', 'combined')
-        return result
+    def print_report_separate(self):
+        """Print separate report per analytic account."""
+        data = self._prepare_report_data()
+        return self.env.ref(
+            'om_daybook_analytic.action_report_daybook_analytic_separate'
+        ).report_action(self, data=data)
 
-    def check_report(self):
-        self.ensure_one()
-
-        # Validate analytic account selection for separate/combined reports
-        if self.report_type in ['separate', 'combined'] and not self.analytic_account_ids:
-            raise UserError(_('Please select at least one Analytic Account for Separate or Combined reports.'))
-
-        data = {}
-        data['form'] = self.read([
-            'target_move',
-            'date_from',
-            'date_to',
-            'journal_ids',
-            'analytic_account_ids',
-            'report_type'
-        ])[0]
-
-        comparison_context = self._build_comparison_context(data)
-        data['form']['comparison_context'] = comparison_context
-
-        # Choose the appropriate report based on report_type
-        if self.report_type == 'separate':
-            return self.env.ref(
-                'om_account_daybook_analytic.action_report_daybook_analytic_separate'
-            ).report_action(self, data=data)
-        else:
-            return self.env.ref(
-                'om_account_daybook_analytic.action_report_daybook_analytic_combined'
-            ).report_action(self, data=data)
+    def print_report_combined(self):
+        """Print combined report of all selected analytic accounts."""
+        data = self._prepare_report_data()
+        return self.env.ref(
+            'om_daybook_analytic.action_report_daybook_analytic_combined'
+        ).report_action(self, data=data)

4549a668338d2e0d8efe2ed14dd91168800e356d
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-03 16:52:38 +0530

    odoomates

--- om_account_daybook_analytic/models/daybook_analytic_wizard.py
+++ om_account_daybook_analytic/models/daybook_analytic_wizard.py
@@ -21,35 +21,54 @@ class AccountDaybookAnalyticWizard(models.TransientModel):
         if self.analytic_account_ids:
             domain.append(('analytic_account_id', 'in', self.analytic_account_ids.ids))
 
-        # Find available views for account.move.line
-        tree_view = self.env['ir.ui.view'].search([
+        # Try to get the standard Journal Items action
+        try:
+            action = self.env.ref('account.action_account_moves_all_a').sudo().read()[0]
+            action['domain'] = domain
+            action['context'] = dict(self.env.context, create=False, edit=False)
+            action['name'] = 'Analytic Account Details'
+            return action
+        except:
+            pass
+
+        # Fallback: Try other common actions
+        try:
+            action = self.env.ref('account.action_move_line_select').sudo().read()[0]
+            action['domain'] = domain
+            action['context'] = dict(self.env.context, create=False, edit=False)
+            action['name'] = 'Analytic Account Details'
+            return action
+        except:
+            pass
+
+        # Last resort: Search for ANY tree view
+        IrView = self.env['ir.ui.view'].sudo()
+        tree_view = IrView.search([
             ('model', '=', 'account.move.line'),
             ('type', '=', 'tree')
-        ], limit=1)
-
-        form_view = self.env['ir.ui.view'].search([
-            ('model', '=', 'account.move.line'),
-            ('type', '=', 'form')
-        ], limit=1)
+        ], limit=1, order='id asc')
 
         if not tree_view:
-            raise UserError("No tree view found for Journal Items. Please check your account module installation.")
+            # Try 'list' type instead of 'tree'
+            tree_view = IrView.search([
+                ('model', '=', 'account.move.line'),
+                ('type', '=', 'list')
+            ], limit=1, order='id asc')
 
-        action = {
+        if not tree_view:
+            raise UserError(
+                "No list/tree view found for Journal Items (account.move.line). "
+                "This might be a version compatibility issue. "
+                "Please contact your system administrator."
+            )
+
+        return {
             'name': 'Analytic Account Details',
             'type': 'ir.actions.act_window',
             'res_model': 'account.move.line',
+            'view_mode': 'tree,form',
+            'views': [(tree_view.id, 'tree'), (False, 'form')],
             'domain': domain,
             'target': 'current',
             'context': {'create': False},
         }
-
-        # Build views list
-        views = [(tree_view.id, 'tree')]
-        if form_view:
-            views.append((form_view.id, 'form'))
-
-        action['views'] = views
-        action['view_mode'] = ','.join([v[1] for v in views])
-
-        return action

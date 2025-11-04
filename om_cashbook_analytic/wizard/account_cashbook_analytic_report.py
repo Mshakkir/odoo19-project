e2e38b28d631a6045cedf1e8b0ee7d3613cf2c88
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-04 16:50:00 +0530

    odoomates

--- om_cashbook_analytic/wizard/account_cashbook_analytic_report.py
+++ om_cashbook_analytic/wizard/account_cashbook_analytic_report.py
@@ -1,23 +1,52 @@
-# from odoo import api, fields, models
-#
-# class AccountCashbookAnalyticWizard(models.TransientModel):
-#     _inherit = 'account.cashbook.report'
-#
-#     analytic_account_ids = fields.Many2many(
-#         'account.analytic.account',
-#         'rel_daybook_analytic_account_rel',   # short name to avoid long table name
-#         'wizard_id',
-#         'analytic_id',
-#         string='Analytic Accounts'
-#     )
-#
-#     def _print_report(self, data):
-#         """Extend original report filter to include analytic accounts"""
-#         data = super(AccountCashbookAnalyticWizard, self)._print_report(data)
-#
-#         if self.analytic_account_ids:
-#             data['form']['analytic_account_ids'] = self.analytic_account_ids.ids
-#         else:
-#             data['form']['analytic_account_ids'] = []
-#
-#         return data
+from odoo import fields, models, api, _
+
+
+class AccountCashBookReport(models.TransientModel):
+    _inherit = "account.cashbook.report"
+
+    analytic_account_ids = fields.Many2many(
+        'account.analytic.account',
+        'cashbook_analytic_account_rel',
+        'report_id',
+        'analytic_account_id',
+        string='Analytic Accounts',
+        help='Filter by specific analytic accounts. Leave empty for all.'
+    )
+
+    report_type = fields.Selection([
+        ('combined', 'Combined Report'),
+        ('separate', 'Separate by Analytic Account'),
+    ], string='Report Type', default='combined', required=True,
+        help='Combined: Show all transactions grouped by analytic account\n'
+             'Separate: Generate individual reports for each analytic account')
+
+    group_by_analytic = fields.Boolean(
+        string='Group by Analytic Account',
+        default=True,
+        help='When enabled, transactions will be grouped by analytic account'
+    )
+
+    def _build_comparison_context(self, data):
+        """Override to add analytic account context"""
+        result = super()._build_comparison_context(data)
+        result['analytic_account_ids'] = data['form'].get('analytic_account_ids', [])
+        result['report_type'] = data['form'].get('report_type', 'combined')
+        result['group_by_analytic'] = data['form'].get('group_by_analytic', True)
+        return result
+
+    def check_report(self):
+        """Override to use the new report with analytic accounts"""
+        data = {}
+        data['form'] = self.read([
+            'target_move', 'date_from', 'date_to', 'journal_ids',
+            'account_ids', 'sortby', 'initial_balance', 'display_account',
+            'analytic_account_ids', 'report_type', 'group_by_analytic'
+        ])[0]
+
+        comparison_context = self._build_comparison_context(data)
+        data['form']['comparison_context'] = comparison_context
+
+        # Use the new report action with analytic accounts
+        return self.env.ref(
+            'account_cashbook_analytic.action_report_cash_book_analytic'
+        ).report_action(self, data=data)

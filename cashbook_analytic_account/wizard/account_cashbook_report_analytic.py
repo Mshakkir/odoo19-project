9a301aa17ca4fcc2d91ac1ca213d8c9d96ba7d64
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-05 16:16:11 +0530

    odoomates

--- cashbook_analytic_account/wizard/account_cashbook_report_analytic.py
+++ cashbook_analytic_account/wizard/account_cashbook_report_analytic.py
@@ -58,7 +58,7 @@ class AccountCashBookReportAnalytic(models.TransientModel):
             'type': 'ir.actions.act_window',
             'name': 'Cashbook Analytic Details',
             'res_model': 'account.move.line',
-            'view_mode': 'list,form',
+            'view_mode': 'list',
             'domain': domain,
             'context': {'search_default_group_by_move_id': 1},
             'target': 'current',

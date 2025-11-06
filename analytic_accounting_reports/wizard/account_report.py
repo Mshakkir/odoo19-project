e296ad80126a0bbaa4ea539a7d638553b99f1f30
shakkir <shakkirtshakkirt86827@gmail.com>
2025-11-06 10:47:12 +0530

    custom

--- analytic_accounting_reports/wizard/account_report.py
+++ analytic_accounting_reports/wizard/account_report.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
-
+from collections import OrderedDict
 
 class AccountingReport(models.TransientModel):
     _inherit = "accounting.report"

381d2e6b0cfb5598d912872ec4723fb58f8ceca3
shakkir <shakkirtshakkirt86827@gmail.com>
2025-11-10 15:50:01 +0530

    custom

--- product_warehouse_selection/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Product Warehouse Selection',
-    'version': '1.0',
-    'depends': ['stock'],
-    'category': 'Inventory',
-    'data': [
-        'views/product_template_views.xml',
-    ],
-}

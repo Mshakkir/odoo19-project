7aca199523294b25bc1d2c598066e3ca99c96239
shakkir <shakkirtshakkirt86827@gmail.com>
2025-11-11 15:52:43 +0530

    custom

--- my_invoice_custom/models/account_move.py
+++ my_invoice_custom/models/account_move.py
@@ -1,19 +1,3 @@
-# from odoo import models, fields
-#
-# class AccountMove(models.Model):
-#     _inherit = "account.move"
-#
-#     discount = fields.Float(string="Discount")
-#
-# class SaleOrder(models.Model):
-#     _inherit = "sale.order"
-#
-#     def _prepare_invoice(self):
-#         invoice_vals = super()._prepare_invoice()
-#         invoice_vals['discount'] = self.discount
-#         return invoice_vals
-
-
 from odoo import models, fields
 
 

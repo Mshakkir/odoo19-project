d054adb3c83f979f1abdfa4cd4352ffafa0503d5
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-11 15:44:01 +0530

    odoomates

--- awb_extension/models/stock_picking.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    awb_number = fields.Char(string="Air Waybill No.")
-    carrier_name = fields.Char(string="Carrier")

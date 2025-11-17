75f1f4afd952afe1e941724b80000e843d2277f1
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-17 16:01:08 +0530

    odoomates

--- custom_sa_quotation/models/sale_order_line.py
+++ custom_sa_quotation/models/sale_order_line.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    delivery_time = fields.Char(
+    delivery_period = fields.Char(
         string="Delivery Time",
         help="Example: 2-3 Weeks, 1 Week, Immediate"
     )

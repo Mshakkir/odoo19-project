c494a610e2ba54f5f5f89e71365c6ec09ed09705
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-25 14:46:08 +0530

    odoomates

--- account_invoice_fixed_discount/__manifest__.py
+++ account_invoice_fixed_discount/__manifest__.py
@@ -14,6 +14,7 @@
     "data": [
         "security/res_groups.xml",
         "views/account_move_view.xml",
+        "data/product_data.xml",
         "views/sale_order_view.xml",
         "reports/report_account_invoice.xml",
         "reports/report_sale_order.xml",

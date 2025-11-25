49af5d912eb312c9d306f2730b6277bfed0043e0
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-25 14:41:46 +0530

    odoomates

--- account_invoice_fixed_discount/models/account_move.py
+++ account_invoice_fixed_discount/models/account_move.py
@@ -1,3 +1,90 @@
+# # Copyright 2017 ForgeFlow S.L.
+# # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+#
+# from odoo import api, fields, models
+# from odoo.tools.float_utils import float_is_zero
+#
+#
+# class AccountMove(models.Model):
+#     _inherit = "account.move"
+#
+#     global_discount_fixed = fields.Monetary(
+#         string="Global Discount (Fixed)",
+#         default=0.0,
+#         currency_field="currency_id",
+#         help="Apply a fixed discount to the entire invoice. This will be distributed proportionally across all invoice lines.",
+#         tracking=True,
+#         states={'posted': [('readonly', True)]},
+#     )
+#
+#     @api.onchange('global_discount_fixed')
+#     def _onchange_global_discount_fixed(self):
+#         """Distribute the global discount proportionally across all invoice lines."""
+#         if not self.invoice_line_ids:
+#             return
+#
+#         currency = self.currency_id or self.company_id.currency_id
+#
+#         if float_is_zero(self.global_discount_fixed, precision_rounding=currency.rounding):
+#             # Clear individual line discounts if global discount is removed
+#             for line in self.invoice_line_ids.filtered(lambda l: not l.display_type):
+#                 line.discount_fixed = 0.0
+#                 line.discount = 0.0
+#                 # Trigger recomputation to reset amounts
+#                 line._compute_totals()
+#             return
+#
+#         # Calculate total before any discount (only for product lines)
+#         product_lines = self.invoice_line_ids.filtered(lambda l: not l.display_type)
+#         total_before_discount = sum(line.quantity * line.price_unit for line in product_lines)
+#
+#         if float_is_zero(total_before_discount, precision_rounding=currency.rounding):
+#             return
+#
+#         # Distribute global discount proportionally
+#         for line in product_lines:
+#             line_subtotal = line.quantity * line.price_unit
+#             if not float_is_zero(line_subtotal, precision_rounding=currency.rounding):
+#                 # Calculate proportional discount for this line
+#                 line_proportion = line_subtotal / total_before_discount
+#                 line.discount_fixed = self.global_discount_fixed * line_proportion
+#                 # Trigger the onchange to update discount percentage and amounts
+#                 line._onchange_discount_fixed()
+#
+#     def write(self, vals):
+#         """Ensure global discount is applied when saving."""
+#         res = super().write(vals)
+#
+#         # If global_discount_fixed is being updated, trigger the distribution
+#         if 'global_discount_fixed' in vals:
+#             for move in self:
+#                 if move.state != 'posted':  # Only apply if not posted
+#                     move._onchange_global_discount_fixed()
+#                     # Force recalculation of invoice totals
+#                     product_lines = move.invoice_line_ids.filtered(lambda l: not l.display_type)
+#                     product_lines._compute_totals()
+#
+#         return res
+#
+#     @api.model_create_multi
+#     def create(self, vals_list):
+#         """Ensure global discount is applied when creating."""
+#         moves = super().create(vals_list)
+#
+#         for move in moves:
+#             if move.global_discount_fixed and move.state != 'posted':
+#                 move._onchange_global_discount_fixed()
+#                 # Force recalculation of invoice totals
+#                 product_lines = move.invoice_line_ids.filtered(lambda l: not l.display_type)
+#                 product_lines._compute_totals()
+#
+#         return moves
+#
+#
+#
+#
+
+
 # Copyright 2017 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
@@ -12,74 +99,65 @@ class AccountMove(models.Model):
         string="Global Discount (Fixed)",
         default=0.0,
         currency_field="currency_id",
-        help="Apply a fixed discount to the entire invoice. This will be distributed proportionally across all invoice lines.",
+        help="Apply a fixed discount to the entire invoice. This will appear as a separate discount line.",
         tracking=True,
         states={'posted': [('readonly', True)]},
     )
 
     @api.onchange('global_discount_fixed')
     def _onchange_global_discount_fixed(self):
-        """Distribute the global discount proportionally across all invoice lines."""
-        if not self.invoice_line_ids:
-            return
+        """Add or update a global discount line in the invoice."""
+        # Find existing global discount line
+        discount_line = self.invoice_line_ids.filtered(lambda l: l.is_global_discount_line)
 
         currency = self.currency_id or self.company_id.currency_id
 
         if float_is_zero(self.global_discount_fixed, precision_rounding=currency.rounding):
-            # Clear individual line discounts if global discount is removed
-            for line in self.invoice_line_ids.filtered(lambda l: not l.display_type):
-                line.discount_fixed = 0.0
-                line.discount = 0.0
-                # Trigger recomputation to reset amounts
-                line._compute_totals()
+            # Remove discount line if discount is zero
+            if discount_line:
+                self.invoice_line_ids = [(3, discount_line.id)]
             return
 
-        # Calculate total before any discount (only for product lines)
-        product_lines = self.invoice_line_ids.filtered(lambda l: not l.display_type)
-        total_before_discount = sum(line.quantity * line.price_unit for line in product_lines)
+        # Get discount product
+        discount_product = self.env.ref('account_invoice_fixed_discount.product_global_discount',
+                                        raise_if_not_found=False)
 
-        if float_is_zero(total_before_discount, precision_rounding=currency.rounding):
+        if not discount_product:
             return
 
-        # Distribute global discount proportionally
-        for line in product_lines:
-            line_subtotal = line.quantity * line.price_unit
-            if not float_is_zero(line_subtotal, precision_rounding=currency.rounding):
-                # Calculate proportional discount for this line
-                line_proportion = line_subtotal / total_before_discount
-                line.discount_fixed = self.global_discount_fixed * line_proportion
-                # Trigger the onchange to update discount percentage and amounts
-                line._onchange_discount_fixed()
+        # Get income account
+        account = discount_product.property_account_income_id or discount_product.categ_id.property_account_income_categ_id
+        if not account:
+            account = self.env['account.account'].search([
+                ('account_type', '=', 'income'),
+                ('company_id', '=', self.company_id.id)
+            ], limit=1)
 
-    def write(self, vals):
-        """Ensure global discount is applied when saving."""
-        res = super().write(vals)
+        discount_values = {
+            'product_id': discount_product.id,
+            'name': 'Global Discount',
+            'quantity': 1,
+            'price_unit': -self.global_discount_fixed,  # Negative amount for discount
+            'account_id': account.id,
+            'is_global_discount_line': True,
+            'sequence': 9999,  # Put at the end
+        }
 
-        # If global_discount_fixed is being updated, trigger the distribution
-        if 'global_discount_fixed' in vals:
-            for move in self:
-                if move.state != 'posted':  # Only apply if not posted
-                    move._onchange_global_discount_fixed()
-                    # Force recalculation of invoice totals
-                    product_lines = move.invoice_line_ids.filtered(lambda l: not l.display_type)
-                    product_lines._compute_totals()
-
-        return res
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        """Ensure global discount is applied when creating."""
-        moves = super().create(vals_list)
-
-        for move in moves:
-            if move.global_discount_fixed and move.state != 'posted':
-                move._onchange_global_discount_fixed()
-                # Force recalculation of invoice totals
-                product_lines = move.invoice_line_ids.filtered(lambda l: not l.display_type)
-                product_lines._compute_totals()
-
-        return moves
+        if discount_line:
+            # Update existing line
+            discount_line.write({
+                'price_unit': -self.global_discount_fixed,
+            })
+        else:
+            # Create new line
+            self.invoice_line_ids = [(0, 0, discount_values)]
 
 
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
-
+    is_global_discount_line = fields.Boolean(
+        string="Is Global Discount Line",
+        default=False,
+        help="Identifies this line as the global discount line"
+    )
--- account_invoice_fixed_discount/models/sale_order.py
+++ account_invoice_fixed_discount/models/sale_order.py
@@ -1,3 +1,88 @@
+# # Copyright 2017 ForgeFlow S.L.
+# # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+#
+# from odoo import api, fields, models
+# from odoo.tools.float_utils import float_is_zero
+#
+#
+# class SaleOrder(models.Model):
+#     _inherit = "sale.order"
+#
+#     global_discount_fixed = fields.Monetary(
+#         string="Global Discount (Fixed)",
+#         default=0.0,
+#         currency_field="currency_id",
+#         help="Apply a fixed discount to the entire order. This will be distributed proportionally across all order lines.",
+#         tracking=True,
+#     )
+#
+#     @api.onchange('global_discount_fixed')
+#     def _onchange_global_discount_fixed(self):
+#         """Distribute the global discount proportionally across all order lines."""
+#         if not self.order_line:
+#             return
+#
+#         currency = self.currency_id or self.company_id.currency_id
+#
+#         if float_is_zero(self.global_discount_fixed, precision_rounding=currency.rounding):
+#             # Clear individual line discounts if global discount is removed
+#             for line in self.order_line:
+#                 line.discount_fixed = 0.0
+#                 line.discount = 0.0
+#                 # Trigger recomputation to reset amounts
+#                 line._compute_amount()
+#             return
+#
+#         # Calculate total before any discount
+#         total_before_discount = sum(line.product_uom_qty * line.price_unit for line in self.order_line)
+#
+#         if float_is_zero(total_before_discount, precision_rounding=currency.rounding):
+#             return
+#
+#         # Distribute global discount proportionally
+#         for line in self.order_line:
+#             line_subtotal = line.product_uom_qty * line.price_unit
+#             if not float_is_zero(line_subtotal, precision_rounding=currency.rounding):
+#                 # Calculate proportional discount for this line
+#                 line_proportion = line_subtotal / total_before_discount
+#                 line.discount_fixed = self.global_discount_fixed * line_proportion
+#                 # Trigger the onchange to update discount percentage and amounts
+#                 line._onchange_discount_fixed()
+#
+#     def write(self, vals):
+#         """Ensure global discount is applied when saving."""
+#         res = super().write(vals)
+#
+#         # If global_discount_fixed is being updated, trigger the distribution
+#         if 'global_discount_fixed' in vals:
+#             for order in self:
+#                 order._onchange_global_discount_fixed()
+#                 # Force recalculation of order totals
+#                 order.order_line._compute_amount()
+#
+#         return res
+#
+#     @api.model_create_multi
+#     def create(self, vals_list):
+#         """Ensure global discount is applied when creating."""
+#         orders = super().create(vals_list)
+#
+#         for order in orders:
+#             if order.global_discount_fixed:
+#                 order._onchange_global_discount_fixed()
+#                 # Force recalculation of order totals
+#                 order.order_line._compute_amount()
+#
+#         return orders
+#
+#     def _prepare_invoice(self):
+#         """Pass the global discount to the invoice."""
+#         res = super()._prepare_invoice()
+#         res['global_discount_fixed'] = self.global_discount_fixed
+#         return res
+#
+
+
 # Copyright 2017 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
@@ -12,68 +97,49 @@ class SaleOrder(models.Model):
         string="Global Discount (Fixed)",
         default=0.0,
         currency_field="currency_id",
-        help="Apply a fixed discount to the entire order. This will be distributed proportionally across all order lines.",
+        help="Apply a fixed discount to the entire order. This will appear as a separate discount line.",
         tracking=True,
     )
 
     @api.onchange('global_discount_fixed')
     def _onchange_global_discount_fixed(self):
-        """Distribute the global discount proportionally across all order lines."""
-        if not self.order_line:
-            return
+        """Add or update a global discount line in the order."""
+        # Find existing global discount line
+        discount_line = self.order_line.filtered(lambda l: l.is_global_discount_line)
 
         currency = self.currency_id or self.company_id.currency_id
 
         if float_is_zero(self.global_discount_fixed, precision_rounding=currency.rounding):
-            # Clear individual line discounts if global discount is removed
-            for line in self.order_line:
-                line.discount_fixed = 0.0
-                line.discount = 0.0
-                # Trigger recomputation to reset amounts
-                line._compute_amount()
+            # Remove discount line if discount is zero
+            if discount_line:
+                self.order_line = [(3, discount_line.id)]
             return
 
-        # Calculate total before any discount
-        total_before_discount = sum(line.product_uom_qty * line.price_unit for line in self.order_line)
+        # Create or update discount line
+        discount_product = self.env.ref('account_invoice_fixed_discount.product_global_discount',
+                                        raise_if_not_found=False)
 
-        if float_is_zero(total_before_discount, precision_rounding=currency.rounding):
+        if not discount_product:
+            # Fallback: try to find any service product or create error message
             return
 
-        # Distribute global discount proportionally
-        for line in self.order_line:
-            line_subtotal = line.product_uom_qty * line.price_unit
-            if not float_is_zero(line_subtotal, precision_rounding=currency.rounding):
-                # Calculate proportional discount for this line
-                line_proportion = line_subtotal / total_before_discount
-                line.discount_fixed = self.global_discount_fixed * line_proportion
-                # Trigger the onchange to update discount percentage and amounts
-                line._onchange_discount_fixed()
+        discount_values = {
+            'product_id': discount_product.id,
+            'name': 'Global Discount',
+            'product_uom_qty': 1,
+            'price_unit': -self.global_discount_fixed,  # Negative amount for discount
+            'is_global_discount_line': True,
+            'sequence': 9999,  # Put at the end
+        }
 
-    def write(self, vals):
-        """Ensure global discount is applied when saving."""
-        res = super().write(vals)
-
-        # If global_discount_fixed is being updated, trigger the distribution
-        if 'global_discount_fixed' in vals:
-            for order in self:
-                order._onchange_global_discount_fixed()
-                # Force recalculation of order totals
-                order.order_line._compute_amount()
-
-        return res
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        """Ensure global discount is applied when creating."""
-        orders = super().create(vals_list)
-
-        for order in orders:
-            if order.global_discount_fixed:
-                order._onchange_global_discount_fixed()
-                # Force recalculation of order totals
-                order.order_line._compute_amount()
-
-        return orders
+        if discount_line:
+            # Update existing line
+            discount_line.write({
+                'price_unit': -self.global_discount_fixed,
+            })
+        else:
+            # Create new line
+            self.order_line = [(0, 0, discount_values)]
 
     def _prepare_invoice(self):
         """Pass the global discount to the invoice."""
@@ -81,3 +147,12 @@ class SaleOrder(models.Model):
         res['global_discount_fixed'] = self.global_discount_fixed
         return res
 
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    is_global_discount_line = fields.Boolean(
+        string="Is Global Discount Line",
+        default=False,
+        help="Identifies this line as the global discount line"
+    )

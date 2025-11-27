2ab818c51cc0d5df6d7898c0226b74f808ed5be7
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-11-27 15:16:10 +0530

    odoomates

--- account_invoice_fixed_discount/models/sale_order_line.py
+++ account_invoice_fixed_discount/models/sale_order_line.py
@@ -1,103 +1,3 @@
-# # Copyright 2017 ForgeFlow S.L.
-# # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-#
-# from odoo import api, fields, models
-# from odoo.tools.float_utils import float_is_zero
-#
-#
-# class SaleOrderLine(models.Model):
-#     _inherit = "sale.order.line"
-#
-#     discount_fixed = fields.Monetary(
-#         string="Discount (Fixed)",
-#         default=0.0,
-#         currency_field="currency_id",
-#         help=(
-#             "Apply a fixed total discount to this line. "
-#             "This is a total discount amount, not per unit."
-#         ),
-#     )
-#
-#     @api.depends("product_uom_qty", "discount", "price_unit", "tax_ids", "discount_fixed")
-#     def _compute_amount(self):
-#         """Compute the amounts of the SO line with fixed discount support."""
-#         for line in self:
-#             # Check if we have a fixed discount
-#             has_fixed_discount = line.discount_fixed and not float_is_zero(
-#                 line.discount_fixed,
-#                 precision_rounding=line.currency_id.rounding if line.currency_id else 0.01
-#             )
-#
-#             if has_fixed_discount:
-#                 # Calculate subtotal before discount
-#                 subtotal_before_discount = line.product_uom_qty * line.price_unit
-#
-#                 # Apply fixed discount to the total
-#                 subtotal_after_discount = subtotal_before_discount - line.discount_fixed
-#
-#                 # Calculate effective price per unit after discount
-#                 if line.product_uom_qty and not float_is_zero(
-#                     line.product_uom_qty,
-#                     precision_rounding=line.currency_id.rounding if line.currency_id else 0.01
-#                 ):
-#                     effective_price_unit = subtotal_after_discount / line.product_uom_qty
-#                 else:
-#                     effective_price_unit = line.price_unit
-#
-#                 if line.tax_ids:
-#                     taxes = line.tax_ids.compute_all(
-#                         effective_price_unit,
-#                         line.order_id.currency_id,
-#                         line.product_uom_qty,
-#                         product=line.product_id,
-#                         partner=line.order_id.partner_shipping_id,
-#                     )
-#                     line.price_tax = taxes["total_included"] - taxes["total_excluded"]
-#                     line.price_total = taxes["total_included"]
-#                     line.price_subtotal = taxes["total_excluded"]
-#                 else:
-#                     line.price_tax = 0.0
-#                     line.price_total = subtotal_after_discount
-#                     line.price_subtotal = subtotal_after_discount
-#             else:
-#                 # Use standard Odoo computation for lines without fixed discount
-#                 super(SaleOrderLine, line)._compute_amount()
-#
-#     @api.onchange('discount_fixed', 'price_unit', 'product_uom_qty')
-#     def _onchange_discount_fixed(self):
-#         """Auto-calculate and display the percentage discount when fixed discount is entered."""
-#         if self.discount_fixed and not float_is_zero(
-#             self.discount_fixed,
-#             precision_rounding=self.currency_id.rounding if self.currency_id else 0.01
-#         ):
-#             # Calculate the percentage discount for display purposes
-#             calculated_discount = self._get_discount_from_fixed_discount()
-#             # Update discount percentage WITHOUT clearing discount_fixed
-#             self.discount = calculated_discount
-#
-#     def _get_discount_from_fixed_discount(self):
-#         """Calculate the discount percentage from the fixed total discount amount."""
-#         self.ensure_one()
-#         currency = self.currency_id or self.order_id.currency_id or self.company_id.currency_id
-#
-#         rounding = currency.rounding if currency else 0.01
-#
-#         if float_is_zero(self.discount_fixed, precision_rounding=rounding):
-#             return 0.0
-#
-#         subtotal = self.product_uom_qty * self.price_unit
-#
-#         if float_is_zero(subtotal, precision_rounding=rounding):
-#             return 0.0
-#
-#         return (self.discount_fixed / subtotal) * 100
-#
-#     def _prepare_invoice_line(self, **optional_values):
-#         """Pass the fixed discount to the invoice line."""
-#         res = super()._prepare_invoice_line(**optional_values)
-#         res["discount_fixed"] = self.discount_fixed
-#         return res
-
 # Copyright 2017 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
@@ -117,6 +17,20 @@ class SaleOrderLine(models.Model):
             "This is a total discount amount, not per unit."
         ),
     )
+
+    # NEW FIELD: Amount before any discount
+    price_subtotal_before_discount = fields.Monetary(
+        string="Amount (Before Discount)",
+        compute="_compute_amount_before_discount",
+        store=True,
+        currency_field="currency_id",
+    )
+
+    @api.depends("product_uom_qty", "price_unit")
+    def _compute_amount_before_discount(self):
+        """Calculate the amount before any discount is applied."""
+        for line in self:
+            line.price_subtotal_before_discount = line.product_uom_qty * line.price_unit
 
     @api.depends("product_uom_qty", "discount", "price_unit", "tax_ids", "discount_fixed")
     def _compute_amount(self):

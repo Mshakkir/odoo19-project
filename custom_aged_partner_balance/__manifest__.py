c6c62238dc7e9ffc42a4dcf3527ec4d96b8faf1d
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-02 11:51:19 +0530

    odoomates

--- custom_aged_partner_balance/__manifest__.py
+++ custom_aged_partner_balance/__manifest__.py
@@ -23,8 +23,6 @@
     'data': [
         'security/ir.model.access.csv',
         'reports/report_aged_partner_custom.xml',
-        # 'wizard/account_aged_trial_balance_custom_view.xml',
-        'reports/report_definition.xml',
         # 'views/aged_partner_wizard_view.xml',  # Uncomment if you add wizard customization
     ],
     'installable': True,

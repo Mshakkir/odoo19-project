aba11b3e57086968e948a04f2b765bf503b881be
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-02 15:00:25 +0530

    odoomates

--- custom_aged_partner_balance/models/aged_partner.py
+++ custom_aged_partner_balance/models/aged_partner.py
@@ -4,42 +4,19 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 
-class AccountAgedTrialBalance(models.TransientModel):
-    _name = 'account.aged.trial.balance'
-    _inherit = 'account.common.partner.report'
-    _description = 'Account Aged Trial balance Report'
+class AccountAgedTrialBalanceInherited(models.TransientModel):
+    """
+    Inherit OdooMates Aged Trial Balance Wizard
+    Add Show Details functionality
+    """
+    _inherit = 'account.aged.trial.balance'
 
-    period_length = fields.Integer(string='Period Length (days)', required=True, default=30)
-    journal_ids = fields.Many2many('account.journal', string='Journals', required=True)
-    date_from = fields.Date(default=lambda *a: time.strftime('%Y-%m-%d'))
-    partner_ids = fields.Many2many('res.partner', string='Partners')
-
-    def _get_report_data(self, data):
-        res = {}
-        data = self.pre_print_report(data)
-        data['form'].update(self.read(['period_length'])[0])
-        period_length = data['form']['period_length']
-        if period_length <= 0:
-            raise UserError(_('You must set a period length greater than 0.'))
-        if not data['form']['date_from']:
-            raise UserError(_('You must set a start date.'))
-        start = data['form']['date_from']
-        for i in range(5)[::-1]:
-            stop = start - relativedelta(days=period_length - 1)
-            res[str(i)] = {
-                'name': (i != 0 and (str((5 - (i + 1)) * period_length) + '-' + str((5 - i) * period_length)) or (
-                        '+' + str(4 * period_length))),
-                'stop': start.strftime('%Y-%m-%d'),
-                'start': (i != 0 and stop.strftime('%Y-%m-%d') or False),
-            }
-            start = stop - relativedelta(days=1)
-        data['form'].update(res)
-        return data
-
-    def _print_report(self, data):
-        data = self._get_report_data(data)
-        return self.env.ref('accounting_pdf_reports.action_report_aged_partner_balance'). \
-            with_context(landscape=True).report_action(self, data=data)
+    # Add partner_ids field if not exists in parent
+    partner_ids = fields.Many2many(
+        'res.partner',
+        string='Partners',
+        help='Filter by specific partners. Leave empty for all partners.'
+    )
 
     def show_details(self):
         """
@@ -47,7 +24,7 @@ class AccountAgedTrialBalance(models.TransientModel):
         """
         self.ensure_one()
 
-        # Get report data
+        # Get report data using parent method
         data = {
             'ids': self.ids,
             'model': self._name,
@@ -60,11 +37,11 @@ class AccountAgedTrialBalance(models.TransientModel):
 
         # Determine account types based on result_selection
         if self.result_selection == 'customer':
-            account_type = ['receivable']
+            account_type = ['asset_receivable']
         elif self.result_selection == 'supplier':
-            account_type = ['payable']
+            account_type = ['liability_payable']
         else:
-            account_type = ['payable', 'receivable']
+            account_type = ['liability_payable', 'asset_receivable']
 
         # Get partner move lines with details
         movelines, total, dummy = report._get_partner_move_lines(
@@ -77,7 +54,8 @@ class AccountAgedTrialBalance(models.TransientModel):
 
         # Create detail records
         detail_obj = self.env['account.aged.detail.line']
-        detail_obj.search([]).unlink()  # Clear previous details
+        # Clear previous details for this wizard
+        detail_obj.search([('wizard_id', '=', self.id)]).unlink()
 
         detail_lines = []
         for partner_data in movelines:
@@ -96,7 +74,7 @@ class AccountAgedTrialBalance(models.TransientModel):
             }
             detail_lines.append(detail_obj.create(vals))
 
-        # Open tree view with details
+        # Open list view with details
         return {
             'name': _('Aged Balance Details - %s') % (
                 'Receivable' if self.result_selection == 'customer'
@@ -106,7 +84,10 @@ class AccountAgedTrialBalance(models.TransientModel):
             'type': 'ir.actions.act_window',
             'res_model': 'account.aged.detail.line',
             'view_mode': 'list,form',
-            'view_id': self.env.ref('custom_aged_partner_balance.view_aged_detail_tree').id,
+            'views': [
+                (self.env.ref('custom_aged_partner_balance.view_aged_detail_list').id, 'list'),
+                (self.env.ref('custom_aged_partner_balance.view_aged_detail_form').id, 'form')
+            ],
             'domain': [('wizard_id', '=', self.id)],
             'context': {
                 'default_wizard_id': self.id,
@@ -118,6 +99,7 @@ class AccountAgedTrialBalance(models.TransientModel):
                     '4': data['form']['4']['name'],
                 },
                 'result_selection': self.result_selection,
+                'date_from': self.date_from,
             },
             'target': 'current',
         }
@@ -129,15 +111,22 @@ class AccountAgedDetailLine(models.TransientModel):
     _description = 'Aged Balance Detail Line'
     _order = 'total desc, partner_name'
 
-    wizard_id = fields.Many2one('account.aged.trial.balance', string='Wizard', ondelete='cascade')
+    wizard_id = fields.Many2one(
+        'account.aged.trial.balance',
+        string='Wizard',
+        ondelete='cascade',
+        required=True
+    )
     partner_id = fields.Many2one('res.partner', string='Partner')
-    partner_name = fields.Char(string='Partner Name')
+    partner_name = fields.Char(string='Partner Name', required=True)
+
     trust = fields.Selection([
         ('good', 'Good Debtor'),
         ('normal', 'Normal Debtor'),
         ('bad', 'Bad Debtor')
     ], string='Trust', default='normal')
 
+    # Aging columns
     not_due = fields.Float(string='Not Due', digits='Account')
     period_0 = fields.Float(string='Period 0', digits='Account')
     period_1 = fields.Float(string='Period 1', digits='Account')
@@ -147,24 +136,100 @@ class AccountAgedDetailLine(models.TransientModel):
     total = fields.Float(string='Total', digits='Account')
 
     # Additional partner information
-    email = fields.Char(related='partner_id.email', string='Email')
-    phone = fields.Char(related='partner_id.phone', string='Phone')
-    vat = fields.Char(related='partner_id.vat', string='Tax ID')
+    email = fields.Char(related='partner_id.email', string='Email', readonly=True)
+    phone = fields.Char(related='partner_id.phone', string='Phone', readonly=True)
+    mobile = fields.Char(related='partner_id.mobile', string='Mobile', readonly=True)
+    vat = fields.Char(related='partner_id.vat', string='Tax ID', readonly=True)
+    street = fields.Char(related='partner_id.street', string='Street', readonly=True)
+    city = fields.Char(related='partner_id.city', string='City', readonly=True)
+    country_id = fields.Many2one(related='partner_id.country_id', string='Country', readonly=True)
+
+    # Computed fields for better analysis
+    overdue_amount = fields.Float(
+        string='Total Overdue',
+        compute='_compute_overdue',
+        store=True,
+        help='Sum of all overdue periods (0-4)'
+    )
+
+    is_overdue = fields.Boolean(
+        string='Has Overdue',
+        compute='_compute_overdue',
+        store=True
+    )
+
+    @api.depends('period_0', 'period_1', 'period_2', 'period_3', 'period_4')
+    def _compute_overdue(self):
+        for record in self:
+            record.overdue_amount = (
+                    record.period_0 + record.period_1 +
+                    record.period_2 + record.period_3 + record.period_4
+            )
+            record.is_overdue = record.overdue_amount != 0
 
     def action_view_partner_ledger(self):
         """Open partner ledger for detailed transactions"""
         self.ensure_one()
+
+        # Determine account types
+        if self.wizard_id.result_selection == 'customer':
+            account_types = ['asset_receivable']
+        elif self.wizard_id.result_selection == 'supplier':
+            account_types = ['liability_payable']
+        else:
+            account_types = ['asset_receivable', 'liability_payable']
+
         return {
             'name': _('Partner Ledger - %s') % self.partner_name,
             'type': 'ir.actions.act_window',
             'res_model': 'account.move.line',
-            'view_mode': 'tree',
+            'view_mode': 'list,form',
             'domain': [
                 ('partner_id', '=', self.partner_id.id),
-                ('account_id.account_type', 'in',
-                 ['asset_receivable'] if self.wizard_id.result_selection == 'customer'
-                 else ['liability_payable'] if self.wizard_id.result_selection == 'supplier'
-                 else ['asset_receivable', 'liability_payable']),
+                ('account_id.account_type', 'in', account_types),
+                ('parent_state', '=', 'posted') if self.wizard_id.target_move == 'posted' else ('id', '!=', False),
             ],
-            'context': {'default_partner_id': self.partner_id.id},
+            'context': {
+                'default_partner_id': self.partner_id.id,
+                'search_default_group_by_move': 1,
+            },
+        }
+
+    def action_view_partner_invoices(self):
+        """Open partner invoices"""
+        self.ensure_one()
+
+        # Determine move types
+        if self.wizard_id.result_selection == 'customer':
+            move_types = ['out_invoice', 'out_refund']
+        elif self.wizard_id.result_selection == 'supplier':
+            move_types = ['in_invoice', 'in_refund']
+        else:
+            move_types = ['out_invoice', 'out_refund', 'in_invoice', 'in_refund']
+
+        return {
+            'name': _('Invoices - %s') % self.partner_name,
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move',
+            'view_mode': 'list,form',
+            'domain': [
+                ('partner_id', '=', self.partner_id.id),
+                ('move_type', 'in', move_types),
+                ('state', '=', 'posted') if self.wizard_id.target_move == 'posted' else ('id', '!=', False),
+            ],
+            'context': {
+                'default_partner_id': self.partner_id.id,
+            },
+        }
+
+    def action_open_partner(self):
+        """Open partner form"""
+        self.ensure_one()
+        return {
+            'name': _('Partner: %s') % self.partner_name,
+            'type': 'ir.actions.act_window',
+            'res_model': 'res.partner',
+            'view_mode': 'form',
+            'res_id': self.partner_id.id,
+            'target': 'current',
         }

e7c81d38567ef6844c2e3a4bf04c50a922307dfc
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-06 16:15:07 +0530

    odoomates

--- custom_partner_ledger/models/partner_ledger_detail.py
+++ custom_partner_ledger/models/partner_ledger_detail.py
@@ -1,190 +1,3 @@
-# # -*- coding: utf-8 -*-
-# from odoo import api, fields, models, _
-#
-#
-# class PartnerLedgerDetail(models.TransientModel):
-#     _name = 'partner.ledger.detail'
-#     _description = 'Partner Ledger Detail View'
-#     _order = 'partner_id, date, id'
-#
-#     partner_id = fields.Many2one('res.partner', string='Partner', readonly=True)
-#     date = fields.Date(string='Date', readonly=True)
-#     move_id = fields.Many2one('account.move', string='Journal Entry', readonly=True)
-#     journal_id = fields.Many2one('account.journal', string='Journal', readonly=True)
-#     account_id = fields.Many2one('account.account', string='Account', readonly=True)
-#     name = fields.Char(string='Label', readonly=True)
-#     ref = fields.Char(string='Reference', readonly=True)
-#     debit = fields.Monetary(string='Debit', readonly=True, currency_field='company_currency_id')
-#     credit = fields.Monetary(string='Credit', readonly=True, currency_field='company_currency_id')
-#     balance = fields.Monetary(string='Balance', readonly=True, currency_field='company_currency_id')
-#     amount_currency = fields.Monetary(string='Amount Currency', readonly=True, currency_field='currency_id')
-#     currency_id = fields.Many2one('res.currency', string='Currency', readonly=True)
-#     company_currency_id = fields.Many2one('res.currency', string='Company Currency', readonly=True)
-#     reconcile_id = fields.Many2one('account.full.reconcile', string='Reconcile', readonly=True)
-#     move_state = fields.Selection([
-#         ('draft', 'Draft'),
-#         ('posted', 'Posted'),
-#     ], string='Status', readonly=True)
-#
-#     # For opening balance line
-#     is_opening_balance = fields.Boolean(string='Is Opening Balance', readonly=True)
-#
-#     @api.model
-#     def get_partner_ledger_details(self, wizard_data):
-#         """
-#         Generate partner ledger detail records based on wizard filters
-#         """
-#         # Clear existing records for this user
-#         self.search([]).unlink()
-#
-#         # Get data from wizard
-#         date_from = wizard_data.get('date_from')
-#         date_to = wizard_data.get('date_to')
-#         partner_ids = wizard_data.get('partner_ids', [])
-#         journal_ids = wizard_data.get('journal_ids', [])
-#         target_move = wizard_data.get('target_move', 'posted')
-#         result_selection = wizard_data.get('result_selection', 'customer')
-#         reconciled = wizard_data.get('reconciled', False)
-#         amount_currency = wizard_data.get('amount_currency', False)
-#
-#         # Determine account types
-#         if result_selection == 'supplier':
-#             account_type = ['liability_payable']
-#         elif result_selection == 'customer':
-#             account_type = ['asset_receivable']
-#         else:
-#             account_type = ['liability_payable', 'asset_receivable']
-#
-#         # Get accounts
-#         accounts = self.env['account.account'].search([('account_type', 'in', account_type)])
-#         account_ids = accounts.ids
-#
-#         # Get move states
-#         if target_move == 'posted':
-#             move_state = ['posted']
-#         else:
-#             move_state = ['draft', 'posted']
-#
-#         # Build domain for move lines
-#         domain = [
-#             ('account_id', 'in', account_ids),
-#             ('move_id.state', 'in', move_state),
-#         ]
-#
-#         # Add partner filter
-#         if partner_ids:
-#             domain.append(('partner_id', 'in', partner_ids))
-#         else:
-#             domain.append(('partner_id', '!=', False))
-#
-#         # Add date filters
-#         if date_from:
-#             domain.append(('date', '>=', date_from))
-#
-#         if date_to:
-#             domain.append(('date', '<=', date_to))
-#
-#         # Add journal filter
-#         if journal_ids:
-#             domain.append(('journal_id', 'in', journal_ids))
-#
-#         # Add reconcile filter
-#         if not reconciled:
-#             domain.append(('full_reconcile_id', '=', False))
-#
-#         # Get move lines
-#         move_lines = self.env['account.move.line'].search(domain, order='partner_id, date, id')
-#
-#         # Create detail records
-#         detail_records = []
-#         partners = move_lines.mapped('partner_id')
-#         company_currency = self.env.company.currency_id
-#
-#         for partner in partners:
-#             partner_lines = move_lines.filtered(lambda l: l.partner_id == partner)
-#
-#             # Calculate opening balance if date_from is set
-#             if date_from:
-#                 opening_balance = self._calculate_opening_balance(
-#                     partner, date_from, account_ids, move_state, reconciled, journal_ids
-#                 )
-#
-#                 if opening_balance != 0:
-#                     detail_records.append({
-#                         'partner_id': partner.id,
-#                         'date': date_from,
-#                         'name': 'Opening Balance',
-#                         'balance': opening_balance,
-#                         'debit': opening_balance if opening_balance > 0 else 0,
-#                         'credit': abs(opening_balance) if opening_balance < 0 else 0,
-#                         'company_currency_id': company_currency.id,
-#                         'is_opening_balance': True,
-#                     })
-#
-#             # Add all move lines for this partner
-#             running_balance = opening_balance if date_from else 0
-#             for line in partner_lines:
-#                 running_balance += line.debit - line.credit
-#
-#                 vals = {
-#                     'partner_id': partner.id,
-#                     'date': line.date,
-#                     'move_id': line.move_id.id,
-#                     'journal_id': line.journal_id.id,
-#                     'account_id': line.account_id.id,
-#                     'name': line.name or line.move_id.name,
-#                     'ref': line.move_id.ref or line.ref,
-#                     'debit': line.debit,
-#                     'credit': line.credit,
-#                     'balance': running_balance,
-#                     'company_currency_id': company_currency.id,
-#                     'reconcile_id': line.full_reconcile_id.id if line.full_reconcile_id else False,
-#                     'move_state': line.move_id.state,
-#                     'is_opening_balance': False,
-#                 }
-#
-#                 # Add currency information if needed
-#                 if amount_currency and line.currency_id != company_currency:
-#                     vals.update({
-#                         'amount_currency': line.amount_currency,
-#                         'currency_id': line.currency_id.id,
-#                     })
-#
-#                 detail_records.append(vals)
-#
-#         # Create records
-#         if detail_records:
-#             self.create(detail_records)
-#
-#         return True
-#
-#     def _calculate_opening_balance(self, partner, date_from, account_ids, move_state, reconciled, journal_ids):
-#         """
-#         Calculate opening balance for a partner before date_from
-#         """
-#         domain = [
-#             ('partner_id', '=', partner.id),
-#             ('account_id', 'in', account_ids),
-#             ('date', '<', date_from),
-#             ('move_id.state', 'in', move_state),
-#         ]
-#
-#         if journal_ids:
-#             domain.append(('journal_id', 'in', journal_ids))
-#
-#         if not reconciled:
-#             domain.append(('full_reconcile_id', '=', False))
-#
-#         lines = self.env['account.move.line'].search(domain)
-#
-#         total_debit = sum(lines.mapped('debit'))
-#         total_credit = sum(lines.mapped('credit'))
-#
-#         return total_debit - total_credit
-#
-
-
-# -*- coding: utf-8 -*-
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 
@@ -199,6 +12,7 @@ class PartnerLedgerDetail(models.TransientModel):
     move_id = fields.Many2one('account.move', string='Journal Entry', readonly=True)
     journal_id = fields.Many2one('account.journal', string='Journal', readonly=True)
     account_id = fields.Many2one('account.account', string='Account', readonly=True)
+    analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account', readonly=True)
     name = fields.Char(string='Label', readonly=True)
     ref = fields.Char(string='Reference', readonly=True)
     debit = fields.Monetary(string='Debit', readonly=True, currency_field='company_currency_id')
@@ -273,6 +87,7 @@ class PartnerLedgerDetail(models.TransientModel):
         date_to = wizard_data.get('date_to')
         partner_ids = wizard_data.get('partner_ids', [])
         journal_ids = wizard_data.get('journal_ids', [])
+        analytic_account_ids = wizard_data.get('analytic_account_ids', [])
         target_move = wizard_data.get('target_move', 'posted')
         result_selection = wizard_data.get('result_selection', 'customer')
         reconciled = wizard_data.get('reconciled', False)
@@ -319,12 +134,27 @@ class PartnerLedgerDetail(models.TransientModel):
         if journal_ids:
             domain.append(('journal_id', 'in', journal_ids))
 
+        # Add analytic account filter
+        if analytic_account_ids:
+            domain.append(('analytic_distribution', '!=', False))
+
         # Add reconcile filter
         if not reconciled:
             domain.append(('full_reconcile_id', '=', False))
 
         # Get move lines
         move_lines = self.env['account.move.line'].search(domain, order='partner_id, date, id')
+
+        # Filter by analytic accounts if specified
+        if analytic_account_ids:
+            filtered_lines = self.env['account.move.line']
+            for line in move_lines:
+                if line.analytic_distribution:
+                    # Check if any of the selected analytic accounts are in the distribution
+                    line_analytic_ids = [int(k) for k in line.analytic_distribution.keys()]
+                    if any(analytic_id in line_analytic_ids for analytic_id in analytic_account_ids):
+                        filtered_lines |= line
+            move_lines = filtered_lines
 
         # Create detail records
         detail_records = []
@@ -338,7 +168,7 @@ class PartnerLedgerDetail(models.TransientModel):
             opening_balance = 0
             if date_from:
                 opening_balance = self._calculate_opening_balance(
-                    partner, date_from, account_ids, move_state, reconciled, journal_ids
+                    partner, date_from, account_ids, move_state, reconciled, journal_ids, analytic_account_ids
                 )
 
                 if opening_balance != 0:
@@ -358,12 +188,21 @@ class PartnerLedgerDetail(models.TransientModel):
             for line in partner_lines:
                 running_balance += line.debit - line.credit
 
+                # Get the primary analytic account from distribution
+                analytic_account_id = False
+                if line.analytic_distribution:
+                    # Get the first analytic account from the distribution
+                    analytic_ids = [int(k) for k in line.analytic_distribution.keys()]
+                    if analytic_ids:
+                        analytic_account_id = analytic_ids[0]
+
                 vals = {
                     'partner_id': partner.id,
                     'date': line.date,
                     'move_id': line.move_id.id,
                     'journal_id': line.journal_id.id,
                     'account_id': line.account_id.id,
+                    'analytic_account_id': analytic_account_id,
                     'name': line.name or line.move_id.name,
                     'ref': line.move_id.ref or line.ref,
                     'debit': line.debit,
@@ -390,7 +229,7 @@ class PartnerLedgerDetail(models.TransientModel):
 
         return True
 
-    def _calculate_opening_balance(self, partner, date_from, account_ids, move_state, reconciled, journal_ids):
+    def _calculate_opening_balance(self, partner, date_from, account_ids, move_state, reconciled, journal_ids, analytic_account_ids=None):
         """
         Calculate opening balance for a partner before date_from
         """
@@ -407,7 +246,20 @@ class PartnerLedgerDetail(models.TransientModel):
         if not reconciled:
             domain.append(('full_reconcile_id', '=', False))
 
+        if analytic_account_ids:
+            domain.append(('analytic_distribution', '!=', False))
+
         lines = self.env['account.move.line'].search(domain)
+
+        # Filter by analytic accounts if specified
+        if analytic_account_ids:
+            filtered_lines = self.env['account.move.line']
+            for line in lines:
+                if line.analytic_distribution:
+                    line_analytic_ids = [int(k) for k in line.analytic_distribution.keys()]
+                    if any(analytic_id in line_analytic_ids for analytic_id in analytic_account_ids):
+                        filtered_lines |= line
+            lines = filtered_lines
 
         total_debit = sum(lines.mapped('debit'))
         total_credit = sum(lines.mapped('credit'))
--- custom_partner_ledger/models/partner_ledger_wizard.py
+++ custom_partner_ledger/models/partner_ledger_wizard.py
@@ -1,50 +1,3 @@
-# # -*- coding: utf-8 -*-
-# from odoo import fields, models, api, _
-#
-#
-# class AccountPartnerLedgerCustom(models.TransientModel):
-#     _inherit = "account.report.partner.ledger"
-#     _description = "Custom Account Partner Ledger"
-#
-#     def action_show_details(self):
-#         """
-#         Show partner ledger details in a dedicated list view
-#         """
-#         self.ensure_one()
-#
-#         # Prepare wizard data
-#         wizard_data = {
-#             'date_from': self.date_from,
-#             'date_to': self.date_to,
-#             'journal_ids': self.journal_ids.ids if self.journal_ids else [],
-#             'target_move': self.target_move,
-#             'result_selection': self.result_selection,
-#             'partner_ids': self.partner_ids.ids if self.partner_ids else [],
-#             'amount_currency': self.amount_currency,
-#             'reconciled': self.reconciled,
-#         }
-#
-#         # Generate detail records
-#         detail_model = self.env['partner.ledger.detail']
-#         detail_model.get_partner_ledger_details(wizard_data)
-#
-#         # Return action to open detail view
-#         return {
-#             'name': _('Partner Ledger Details'),
-#             'type': 'ir.actions.act_window',
-#             'res_model': 'partner.ledger.detail',
-#             'view_mode': 'list',
-#             'view_id': self.env.ref('custom_partner_ledger.view_partner_ledger_detail_tree').id,
-#             'search_view_id': self.env.ref('custom_partner_ledger.view_partner_ledger_detail_search').id,
-#             'context': {
-#                 'search_default_group_by_partner': 1,
-#                 'search_default_filter_transactions': 1,
-#             },
-#             'target': 'current',
-#         }
-
-
-# -*- coding: utf-8 -*-
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api, _
 
@@ -52,6 +5,12 @@ from odoo import fields, models, api, _
 class AccountPartnerLedgerCustom(models.TransientModel):
     _inherit = "account.report.partner.ledger"
     _description = "Custom Account Partner Ledger"
+
+    analytic_account_ids = fields.Many2many(
+        'account.analytic.account',
+        string='Analytic Accounts (Warehouses)',
+        help='Filter by specific analytic accounts/warehouses'
+    )
 
     def action_show_details(self):
         """
@@ -69,6 +28,7 @@ class AccountPartnerLedgerCustom(models.TransientModel):
             'partner_ids': self.partner_ids.ids if self.partner_ids else [],
             'amount_currency': self.amount_currency,
             'reconciled': self.reconciled,
+            'analytic_account_ids': self.analytic_account_ids.ids if self.analytic_account_ids else [],
         }
 
         # Generate detail records
@@ -88,3 +48,13 @@ class AccountPartnerLedgerCustom(models.TransientModel):
             },
             'target': 'current',
         }
+
+    def _print_report(self, data):
+        """
+        Override to pass analytic account data to the report
+        """
+        data = self.pre_print_report(data)
+        data['form'].update({
+            'analytic_account_ids': self.analytic_account_ids.ids,
+        })
+        return self.env.ref('accounting_pdf_reports.action_report_partnerledger').report_action(self, data=data)

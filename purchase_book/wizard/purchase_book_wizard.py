9b50b6b2d006ecb4120ed56ae8aef64c21f15aa7
shakkir <shakkirtshakkirt86827@gmail.com>
2025-12-08 12:15:02 +0530

    custom

--- purchase_book/wizard/purchase_book_wizard.py
+++ purchase_book/wizard/purchase_book_wizard.py
@@ -32,6 +32,12 @@ class PurchaseBookWizard(models.TransientModel):
 
     partner_ids = fields.Many2many('res.partner', string='Vendors')
 
+    analytic_account_ids = fields.Many2many(
+        'account.analytic.account',
+        string='Analytic Accounts',
+        help='Filter by Analytic Accounts (Cost Centers/Projects)'
+    )
+
     include_expense = fields.Boolean(string='Include Expense Purchases', default=True)
 
     company_id = fields.Many2one('res.company', string='Company',
@@ -68,10 +74,10 @@ class PurchaseBookWizard(models.TransientModel):
             return self._get_combined_data()
 
     def _get_purchase_data(self):
-        """Fetch purchase order data based on filters"""
+        """Fetch purchase order data based on filters with analytic accounts"""
         self.ensure_one()
 
-        # Search for posted vendor bills (purchase invoices)
+        # Base domain for posted vendor bills
         domain = [
             ('invoice_date', '>=', self.date_from),
             ('invoice_date', '<=', self.date_to),
@@ -83,33 +89,84 @@ class PurchaseBookWizard(models.TransientModel):
         if self.partner_ids:
             domain.append(('partner_id', 'in', self.partner_ids.ids))
 
+        # If analytic accounts are selected, filter invoice lines by analytic account
+        if self.analytic_account_ids:
+            # Find invoices that have lines with selected analytic accounts
+            invoice_lines = self.env['account.move.line'].search([
+                ('analytic_distribution', '!=', False),
+                ('parent_state', '=', 'posted'),
+                ('move_id.move_type', '=', 'in_invoice'),
+                ('move_id.invoice_date', '>=', self.date_from),
+                ('move_id.invoice_date', '<=', self.date_to),
+            ])
+
+            # Filter lines by analytic accounts (checking analytic_distribution JSON field)
+            filtered_invoice_ids = set()
+            for line in invoice_lines:
+                if line.analytic_distribution:
+                    # analytic_distribution is stored as JSON: {analytic_account_id: percentage}
+                    analytic_ids = [int(aid) for aid in line.analytic_distribution.keys()]
+                    if any(aid in self.analytic_account_ids.ids for aid in analytic_ids):
+                        filtered_invoice_ids.add(line.move_id.id)
+
+            if filtered_invoice_ids:
+                domain.append(('id', 'in', list(filtered_invoice_ids)))
+            else:
+                # No invoices found with selected analytic accounts
+                return []
+
         invoices = self.env['account.move'].search(domain, order='invoice_date asc, name asc')
 
         data = []
         for invoice in invoices:
-            # Calculate amounts
-            gross_total = sum(line.price_subtotal for line in invoice.invoice_line_ids)
-            trade_disc = 0.0  # Calculate discount if your lines have discount field
-            for line in invoice.invoice_line_ids:
+            # If analytic filter is active, only sum lines with matching analytic accounts
+            if self.analytic_account_ids:
+                relevant_lines = invoice.invoice_line_ids.filtered(
+                    lambda l: l.analytic_distribution and any(
+                        int(aid) in self.analytic_account_ids.ids
+                        for aid in l.analytic_distribution.keys()
+                    )
+                )
+            else:
+                relevant_lines = invoice.invoice_line_ids
+
+            if not relevant_lines:
+                continue
+
+            # Calculate amounts from relevant lines only
+            gross_total = sum(line.price_subtotal for line in relevant_lines)
+            trade_disc = 0.0
+            for line in relevant_lines:
                 if line.discount:
                     discount_amount = (line.price_unit * line.quantity * line.discount) / 100
                     trade_disc += discount_amount
 
             net_total = gross_total - trade_disc
-            tax_amount = invoice.amount_tax
-            grand_total = invoice.amount_total
+            tax_amount = sum((line.price_total - line.price_subtotal) for line in relevant_lines)
+            grand_total = net_total + tax_amount
+
+            # Get analytic account names
+            analytic_names = []
+            if self.analytic_account_ids:
+                for line in relevant_lines:
+                    if line.analytic_distribution:
+                        for aid in line.analytic_distribution.keys():
+                            analytic = self.env['account.analytic.account'].browse(int(aid))
+                            if analytic.name not in analytic_names:
+                                analytic_names.append(analytic.name)
 
             data.append({
                 'date': invoice.invoice_date,
                 'vendor': invoice.partner_id.name,
                 'invoice_ref': invoice.ref or invoice.name,
-                'po_number': ', '.join(invoice.invoice_line_ids.mapped('purchase_line_id.order_id.name')) or '',
+                'po_number': ', '.join(relevant_lines.mapped('purchase_line_id.order_id.name')) or '',
+                'analytic_account': ', '.join(analytic_names) if analytic_names else '',
                 'gross': gross_total,
                 'trade_disc': trade_disc,
                 'net_total': net_total,
                 'add_disc': 0.0,
                 'add_cost': 0.0,
-                'round_off': invoice.amount_residual_signed if hasattr(invoice, 'amount_residual_signed') else 0.0,
+                'round_off': 0.0,
                 'adj_amount': 0.0,
                 'tax_amount': tax_amount,
                 'grand_total': grand_total,
@@ -118,7 +175,7 @@ class PurchaseBookWizard(models.TransientModel):
         return data
 
     def _get_return_data(self):
-        """Fetch purchase return (refund) data based on filters"""
+        """Fetch purchase return (refund) data based on filters with analytic accounts"""
         self.ensure_one()
 
         domain = [
@@ -132,25 +189,72 @@ class PurchaseBookWizard(models.TransientModel):
         if self.partner_ids:
             domain.append(('partner_id', 'in', self.partner_ids.ids))
 
+        # Filter by analytic accounts if selected
+        if self.analytic_account_ids:
+            invoice_lines = self.env['account.move.line'].search([
+                ('analytic_distribution', '!=', False),
+                ('parent_state', '=', 'posted'),
+                ('move_id.move_type', '=', 'in_refund'),
+                ('move_id.invoice_date', '>=', self.date_from),
+                ('move_id.invoice_date', '<=', self.date_to),
+            ])
+
+            filtered_invoice_ids = set()
+            for line in invoice_lines:
+                if line.analytic_distribution:
+                    analytic_ids = [int(aid) for aid in line.analytic_distribution.keys()]
+                    if any(aid in self.analytic_account_ids.ids for aid in analytic_ids):
+                        filtered_invoice_ids.add(line.move_id.id)
+
+            if filtered_invoice_ids:
+                domain.append(('id', 'in', list(filtered_invoice_ids)))
+            else:
+                return []
+
         refunds = self.env['account.move'].search(domain, order='invoice_date asc, name asc')
 
         data = []
         for refund in refunds:
-            gross_total = sum(line.price_subtotal for line in refund.invoice_line_ids)
+            # Filter lines by analytic accounts
+            if self.analytic_account_ids:
+                relevant_lines = refund.invoice_line_ids.filtered(
+                    lambda l: l.analytic_distribution and any(
+                        int(aid) in self.analytic_account_ids.ids
+                        for aid in l.analytic_distribution.keys()
+                    )
+                )
+            else:
+                relevant_lines = refund.invoice_line_ids
+
+            if not relevant_lines:
+                continue
+
+            gross_total = sum(line.price_subtotal for line in relevant_lines)
             trade_disc = 0.0
-            for line in refund.invoice_line_ids:
+            for line in relevant_lines:
                 if line.discount:
                     discount_amount = (line.price_unit * line.quantity * line.discount) / 100
                     trade_disc += discount_amount
 
             net_total = gross_total - trade_disc
-            tax_amount = refund.amount_tax
-            grand_total = refund.amount_total
+            tax_amount = sum((line.price_total - line.price_subtotal) for line in relevant_lines)
+            grand_total = net_total + tax_amount
+
+            # Get analytic account names
+            analytic_names = []
+            if self.analytic_account_ids:
+                for line in relevant_lines:
+                    if line.analytic_distribution:
+                        for aid in line.analytic_distribution.keys():
+                            analytic = self.env['account.analytic.account'].browse(int(aid))
+                            if analytic.name not in analytic_names:
+                                analytic_names.append(analytic.name)
 
             data.append({
                 'date': refund.invoice_date,
                 'vendor': refund.partner_id.name,
                 'invoice_ref': refund.ref or refund.name,
+                'analytic_account': ', '.join(analytic_names) if analytic_names else '',
                 'gross': gross_total,
                 'trade_disc': trade_disc,
                 'net_total': net_total,
@@ -165,7 +269,7 @@ class PurchaseBookWizard(models.TransientModel):
         return data
 
     def _get_combined_data(self):
-        """Fetch both purchase and return data"""
+        """Fetch both purchase and return data with analytic filtering"""
         self.ensure_one()
 
         purchase_data = self._get_purchase_data()
@@ -204,4 +308,5 @@ class PurchaseBookWizard(models.TransientModel):
             'date_to': self.date_to,
             'report_type': self.report_type,
             'company': self.company_id,
+            'analytic_accounts': self.analytic_account_ids,
         }

e8dd46d74d0ed1602424d46a5eebfb5aec18f593
shakkir <shakkirtshakkirt86827@gmail.com>
2025-12-09 15:26:00 +0530

    custom

---
+++ custom_payment_receipt/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom Payment Receipt',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Custom Payment Receipt with Header and Footer',
+    'description': """
+        Custom Payment Receipt Report
+        ==============================
+        - Custom header with company details (English & Arabic)
+        - Custom footer with branch details
+        - Separate layouts for customer and vendor payments
+    """,
+    'author': 'Your Company',
+    'website': 'https://www.yourcompany.com',
+    'depends': ['account'],
+    'data': [
+        'views/report_payment_receipt.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}

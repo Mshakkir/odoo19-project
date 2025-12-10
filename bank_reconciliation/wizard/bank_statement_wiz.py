adfcf9443bb4fb5b6fd83f9d7348b1aff8ef41d5
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-10 11:48:27 +0530

    odoomates

--- bank_reconciliation/wizard/bank_statement_wiz.py
+++ bank_reconciliation/wizard/bank_statement_wiz.py
@@ -1,3 +1,219 @@
+# # -*- coding: utf-8 -*-
+# from odoo import api, fields, models, _
+# from odoo.exceptions import UserError
+#
+#
+# class BankStatement(models.Model):
+#     _name = 'bank.statement'
+#     _description = 'Bank Statement Reconciliation'
+#
+#     journal_id = fields.Many2one(
+#         'account.journal',
+#         string='Bank Journal',
+#         domain=[('type', '=', 'bank')],
+#         required=True
+#     )
+#     account_id = fields.Many2one(
+#         'account.account',
+#         string='Bank Account',
+#         compute='_compute_account_id',
+#         store=True,
+#         readonly=True
+#     )
+#     date_from = fields.Date(string='Date From', required=True)
+#     date_to = fields.Date(string='Date To', required=True)
+#
+#     statement_line_ids = fields.Many2many(
+#         'account.move.line',
+#         'bank_statement_line_rel',
+#         'statement_id',
+#         'line_id',
+#         string='Available Lines',
+#         compute='_compute_statement_lines',
+#         store=False
+#     )
+#
+#     statement_lines = fields.One2many(
+#         'account.move.line',
+#         'bank_statement_id',
+#         string='Statement Lines',
+#         context={'tree_view_ref': 'bank_reconciliation.view_bank_statement_move_line_tree'}
+#     )
+#
+#     gl_balance = fields.Monetary(
+#         string='Balance as per Company Books',
+#         readonly=True,
+#         compute='_compute_amount',
+#         store=False
+#     )
+#     bank_balance = fields.Monetary(
+#         string='Balance as per Bank',
+#         readonly=True,
+#         compute='_compute_amount',
+#         store=False
+#     )
+#     balance_difference = fields.Monetary(
+#         string='Amounts not Reflected in Bank',
+#         readonly=True,
+#         compute='_compute_amount',
+#         store=False
+#     )
+#     currency_id = fields.Many2one(
+#         'res.currency',
+#         string='Currency',
+#         compute='_compute_currency_id',
+#         store=True,
+#         readonly=True
+#     )
+#     company_id = fields.Many2one(
+#         'res.company',
+#         string='Company',
+#         default=lambda self: self.env.company,
+#         readonly=True
+#     )
+#     name = fields.Char(
+#         string='Reference',
+#         default='/',
+#         readonly=True,
+#         copy=False
+#     )
+#     state = fields.Selection([
+#         ('draft', 'Draft'),
+#         ('done', 'Done'),
+#     ], string='Status', default='draft', readonly=True)
+#
+#     @api.depends('journal_id')
+#     def _compute_account_id(self):
+#         """Compute bank account from journal"""
+#         for record in self:
+#             if record.journal_id and record.journal_id.default_account_id:
+#                 record.account_id = record.journal_id.default_account_id
+#             else:
+#                 record.account_id = False
+#
+#     @api.depends('journal_id')
+#     def _compute_currency_id(self):
+#         """Compute currency from journal"""
+#         for record in self:
+#             if record.journal_id:
+#                 record.currency_id = (
+#                         record.journal_id.currency_id or
+#                         record.journal_id.company_id.currency_id
+#                 )
+#             else:
+#                 record.currency_id = self.env.company.currency_id
+#
+#     @api.depends('journal_id', 'date_from', 'date_to', 'account_id')
+#     def _compute_statement_lines(self):
+#         """Load unreconciled move lines based on journal and date range"""
+#         for record in self:
+#             if not record.journal_id or not record.account_id:
+#                 record.statement_line_ids = [(5, 0, 0)]
+#                 continue
+#
+#             # Build domain for unreconciled lines
+#             domain = [
+#                 ('account_id', '=', record.account_id.id),
+#                 ('statement_date', '=', False),
+#                 ('parent_state', '=', 'posted'),
+#             ]
+#
+#             if record.date_from:
+#                 domain.append(('date', '>=', record.date_from))
+#             if record.date_to:
+#                 domain.append(('date', '<=', record.date_to))
+#
+#             # Load lines
+#             lines = self.env['account.move.line'].search(domain)
+#             record.statement_line_ids = [(6, 0, lines.ids)]
+#
+#     @api.onchange('statement_line_ids')
+#     def _onchange_statement_line_ids(self):
+#         """Update statement_lines when computed lines change"""
+#         if self.statement_line_ids and not self.statement_lines:
+#             self.statement_lines = [(6, 0, self.statement_line_ids.ids)]
+#
+#     @api.depends('statement_lines.statement_date', 'account_id')
+#     def _compute_amount(self):
+#         """Calculate GL balance, bank balance, and difference"""
+#         for record in self:
+#             gl_balance = 0.0
+#             bank_balance = 0.0
+#             current_update = 0.0
+#
+#             if not record.account_id:
+#                 record.gl_balance = 0.0
+#                 record.bank_balance = 0.0
+#                 record.balance_difference = 0.0
+#                 continue
+#
+#             # Calculate GL balance (all posted entries for this account)
+#             domain = [
+#                 ('account_id', '=', record.account_id.id),
+#                 ('parent_state', '=', 'posted')
+#             ]
+#
+#             if record.date_to:
+#                 domain.append(('date', '<=', record.date_to))
+#
+#             lines = self.env['account.move.line'].search(domain)
+#             gl_balance = sum(line.debit - line.credit for line in lines)
+#
+#             # Calculate bank balance (previously reconciled entries)
+#             domain = [
+#                 ('account_id', '=', record.account_id.id),
+#                 ('id', 'not in', record.statement_lines.ids),
+#                 ('statement_date', '!=', False),
+#                 ('parent_state', '=', 'posted')
+#             ]
+#
+#             if record.date_to:
+#                 domain.append(('statement_date', '<=', record.date_to))
+#
+#             lines = self.env['account.move.line'].search(domain)
+#             bank_balance = sum(line.balance for line in lines)
+#
+#             # Add currently updated entries
+#             current_update = sum(
+#                 line.debit - line.credit
+#                 for line in record.statement_lines
+#                 if line.statement_date
+#             )
+#
+#             record.gl_balance = gl_balance
+#             record.bank_balance = bank_balance + current_update
+#             record.balance_difference = record.gl_balance - record.bank_balance
+#
+#     def action_save_reconciliation(self):
+#         """Save the reconciliation and close the wizard"""
+#         self.ensure_one()
+#
+#         # Mark as done
+#         self.write({'state': 'done'})
+#
+#         # Generate sequence name if not set
+#         if self.name == '/':
+#             self.name = self.env['ir.sequence'].next_by_code('bank.statement') or '/'
+#
+#         return {'type': 'ir.actions.act_window_close'}
+#
+#     @api.model_create_multi
+#     def create(self, vals_list):
+#         """Override create to generate sequence and load lines"""
+#         for vals in vals_list:
+#             if vals.get('name', '/') == '/':
+#                 vals['name'] = self.env['ir.sequence'].next_by_code('bank.statement') or '/'
+#
+#         records = super(BankStatement, self).create(vals_list)
+#
+#         # Load statement lines after creation
+#         for record in records:
+#             if record.statement_line_ids and not record.statement_lines:
+#                 record.statement_lines = [(6, 0, record.statement_line_ids.ids)]
+#
+#         return records
+
+
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
@@ -23,21 +239,10 @@ class BankStatement(models.Model):
     date_from = fields.Date(string='Date From', required=True)
     date_to = fields.Date(string='Date To', required=True)
 
-    statement_line_ids = fields.Many2many(
-        'account.move.line',
-        'bank_statement_line_rel',
-        'statement_id',
-        'line_id',
-        string='Available Lines',
-        compute='_compute_statement_lines',
-        store=False
-    )
-
     statement_lines = fields.One2many(
         'account.move.line',
         'bank_statement_id',
-        string='Statement Lines',
-        context={'tree_view_ref': 'bank_reconciliation.view_bank_statement_move_line_tree'}
+        string='Statement Lines'
     )
 
     gl_balance = fields.Monetary(
@@ -103,37 +308,46 @@ class BankStatement(models.Model):
             else:
                 record.currency_id = self.env.company.currency_id
 
-    @api.depends('journal_id', 'date_from', 'date_to', 'account_id')
-    def _compute_statement_lines(self):
+    @api.onchange('journal_id', 'date_from', 'date_to')
+    def _onchange_load_lines(self):
         """Load unreconciled move lines based on journal and date range"""
-        for record in self:
-            if not record.journal_id or not record.account_id:
-                record.statement_line_ids = [(5, 0, 0)]
-                continue
+        if not self.journal_id or not self.date_from or not self.date_to:
+            self.statement_lines = [(5, 0, 0)]
+            return
 
-            # Build domain for unreconciled lines
-            domain = [
-                ('account_id', '=', record.account_id.id),
-                ('statement_date', '=', False),
-                ('parent_state', '=', 'posted'),
-            ]
+        if not self.account_id:
+            return {
+                'warning': {
+                    'title': _('Configuration Error'),
+                    'message': _('Please configure a default account for this journal.')
+                }
+            }
 
-            if record.date_from:
-                domain.append(('date', '>=', record.date_from))
-            if record.date_to:
-                domain.append(('date', '<=', record.date_to))
+        # Build domain for unreconciled lines
+        domain = [
+            ('account_id', '=', self.account_id.id),
+            ('statement_date', '=', False),
+            ('parent_state', '=', 'posted'),
+            ('date', '>=', self.date_from),
+            ('date', '<=', self.date_to),
+        ]
 
-            # Load lines
-            lines = self.env['account.move.line'].search(domain)
-            record.statement_line_ids = [(6, 0, lines.ids)]
+        # Search for lines
+        lines = self.env['account.move.line'].search(domain)
 
-    @api.onchange('statement_line_ids')
-    def _onchange_statement_line_ids(self):
-        """Update statement_lines when computed lines change"""
-        if self.statement_line_ids and not self.statement_lines:
-            self.statement_lines = [(6, 0, self.statement_line_ids.ids)]
+        if not lines:
+            self.statement_lines = [(5, 0, 0)]
+            return {
+                'warning': {
+                    'title': _('No Transactions Found'),
+                    'message': _('No unreconciled transactions found for the selected date range.')
+                }
+            }
 
-    @api.depends('statement_lines.statement_date', 'account_id')
+        # Update statement lines
+        self.statement_lines = [(6, 0, lines.ids)]
+
+    @api.depends('statement_lines.statement_date', 'account_id', 'date_to')
     def _compute_amount(self):
         """Calculate GL balance, bank balance, and difference"""
         for record in self:
@@ -147,7 +361,7 @@ class BankStatement(models.Model):
                 record.balance_difference = 0.0
                 continue
 
-            # Calculate GL balance (all posted entries for this account)
+            # Calculate GL balance (all posted entries for this account up to date_to)
             domain = [
                 ('account_id', '=', record.account_id.id),
                 ('parent_state', '=', 'posted')
@@ -173,7 +387,7 @@ class BankStatement(models.Model):
             lines = self.env['account.move.line'].search(domain)
             bank_balance = sum(line.balance for line in lines)
 
-            # Add currently updated entries
+            # Add currently updated entries (lines in this statement that are marked)
             current_update = sum(
                 line.debit - line.credit
                 for line in record.statement_lines
@@ -185,30 +399,22 @@ class BankStatement(models.Model):
             record.balance_difference = record.gl_balance - record.bank_balance
 
     def action_save_reconciliation(self):
-        """Save the reconciliation and close the wizard"""
+        """Save the reconciliation and mark as done"""
         self.ensure_one()
-
-        # Mark as done
         self.write({'state': 'done'})
-
-        # Generate sequence name if not set
-        if self.name == '/':
-            self.name = self.env['ir.sequence'].next_by_code('bank.statement') or '/'
-
         return {'type': 'ir.actions.act_window_close'}
+
+    def action_load_lines(self):
+        """Manual action to reload lines"""
+        self.ensure_one()
+        self._onchange_load_lines()
+        return True
 
     @api.model_create_multi
     def create(self, vals_list):
-        """Override create to generate sequence and load lines"""
+        """Override create to generate sequence"""
         for vals in vals_list:
             if vals.get('name', '/') == '/':
                 vals['name'] = self.env['ir.sequence'].next_by_code('bank.statement') or '/'
 
-        records = super(BankStatement, self).create(vals_list)
-
-        # Load statement lines after creation
-        for record in records:
-            if record.statement_line_ids and not record.statement_lines:
-                record.statement_lines = [(6, 0, record.statement_line_ids.ids)]
-
-        return records
+        return super(BankStatement, self).create(vals_list)

d51f97781d4c55089d1dbe2761806859ff10f516
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-13 15:04:11 +0530

    odoomates

--- bank_reconciliation/models/bank_reconciliation_report.py
+++ bank_reconciliation/models/bank_reconciliation_report.py
@@ -10,15 +10,22 @@ class BankReconciliationReport(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         docs = self.env['bank.statement'].browse(docids)
 
+        # Calculate totals and counts
+        cleared_lines = docs.line_ids.filtered(lambda l: l.statement_date)
+        pending_lines = docs.line_ids.filtered(lambda l: not l.statement_date)
+
+        total_cleared = sum(cleared_lines.mapped(lambda x: x.debit - x.credit))
+        total_pending = sum(pending_lines.mapped(lambda x: x.debit - x.credit))
+        cleared_count = len(cleared_lines)
+        pending_count = len(pending_lines)
+
         return {
             'doc_ids': docids,
             'doc_model': 'bank.statement',
             'docs': docs,
             'company': self.env.company,
-            'total_cleared': sum(
-                docs.line_ids.filtered(lambda l: l.statement_date).mapped(lambda x: x.debit - x.credit)),
-            'total_pending': sum(
-                docs.line_ids.filtered(lambda l: not l.statement_date).mapped(lambda x: x.debit - x.credit)),
-            'cleared_count': len(docs.line_ids.filtered(lambda l: l.statement_date)),
-            'pending_count': len(docs.line_ids.filtered(lambda l: not l.statement_date)),
+            'total_cleared': total_cleared,
+            'total_pending': total_pending,
+            'cleared_count': cleared_count,
+            'pending_count': pending_count,
         }

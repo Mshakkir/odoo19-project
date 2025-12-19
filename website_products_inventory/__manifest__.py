9b3f9294901c0a7fd853955619a4a61ed8134f76
shakkir <shakkirtshakkirt86827@gmail.com>
2025-12-19 15:06:34 +0530

    custom

---
+++ website_products_inventory/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Website Products with Inventory',
+    'version': '19.0.1.0.0',
+    'category': 'Website',
+    'depends': ['website', 'website_sale', 'stock'],
+    'data': [
+        'views/website_products_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

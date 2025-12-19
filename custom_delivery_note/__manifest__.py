0984d13185d47fa24eeeee919563ea09f5729f69
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-19 10:03:24 +0530

    odoomates

--- custom_delivery_note/__manifest__.py
+++ custom_delivery_note/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Custom Delivery Note",
     "version": "1.0",
-    "depends": ["stock"],
+    "depends": ["stock", "delivery"],
     "data": [
         "report/report.xml",
         "report/report_delivery_note_custom.xml",

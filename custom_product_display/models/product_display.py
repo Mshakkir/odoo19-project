8254d4d9462479a3c2ce34795bf6a273a6b0494a
shakkir <shakkirtshakkirt86827@gmail.com>
2025-12-19 16:52:11 +0530

    custom

--- custom_product_display/models/product_display.py
+++ custom_product_display/models/product_display.py
@@ -1,69 +1,30 @@
-from odoo import models, fields, api, _
-from odoo.exceptions import AccessError, UserError
+from odoo import models
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    def get_inventory_details(self):
-        """Get inventory details for the product - Safe for public users"""
+    def get_inventory_details_safe(self):
+        """Safe method that doesn't require stock permissions"""
         self.ensure_one()
 
-        try:
-            # Check if user has access to stock information
-            if self.env.user.has_group('stock.group_stock_user'):
-                # Get available quantity for users with stock access
-                total_qty = self.qty_available
+        # Use fields that are accessible to all users
+        total_qty = self.qty_available  # This is usually accessible
 
-                # Get locations with stock (only if user has access)
-                quants = self.env['stock.quant'].search([
-                    ('product_id', '=', self.id),
-                    ('quantity', '>', 0)
-                ])
-
-                locations = []
-                for quant in quants:
-                    locations.append({
-                        'location': quant.location_id.complete_name,
-                        'quantity': quant.quantity,
-                    })
-            else:
-                # For public/website users, show simplified info
-                total_qty = self.qty_available  # This field is accessible
-                locations = []
-
-                # Just show if available or not
-                if total_qty > 0:
-                    locations.append({
-                        'location': 'Available',
-                        'quantity': total_qty,
-                    })
-
-            return {
-                'total_quantity': total_qty,
-                'locations': locations[:3],  # Limit to 3 locations
-                'has_inventory': total_qty > 0,
-                'product_name': self.name,
-                'default_code': self.default_code or '',
-            }
-
-        except AccessError:
-            # If access denied, return basic info
-            return {
-                'total_quantity': 0,
-                'locations': [],
-                'has_inventory': False,
-                'product_name': self.name,
-                'default_code': self.default_code or '',
-            }
+        return {
+            'total_quantity': total_qty or 0,
+            'has_inventory': total_qty > 0 if total_qty else False,
+            'product_name': self.name,
+            'default_code': self.default_code or '',
+            'on_hand': total_qty or 0,
+        }
 
 
 class Website(models.Model):
     _inherit = 'website'
 
-    def get_products_with_inventory(self, limit=50):
-        """Get products with inventory details - Safe for all users"""
-        # Search for products that are published and salable
+    def get_products_with_inventory_safe(self, limit=50):
+        """Safe method for website display"""
         products = self.env['product.product'].search([
             ('sale_ok', '=', True),
             ('website_published', '=', True),
@@ -71,22 +32,19 @@ class Website(models.Model):
         ], limit=limit)
 
         product_data = []
-        for product in products:
-            try:
-                inventory_data = product.get_inventory_details()
-                product_data.append({
-                    'product': product,
-                    'inventory': inventory_data,
-                    'price': product.list_price,
-                    'currency': self.env.company.currency_id,
-                    'image_url': '/web/image/product.product/%s/image_1024' % product.id if product.image_1920 else '/website/static/src/img/product_image_placeholder.svg',
-                })
-            except Exception:
-                # Skip products that cause errors
-                continue
+        for product in products.sudo():  # Use sudo for safe access
+            product_data.append({
+                'product': product,
+                'inventory': {
+                    'total_quantity': product.qty_available or 0,
+                    'has_inventory': (product.qty_available or 0) > 0,
+                },
+                'price': product.list_price,
+                'currency': self.env.company.currency_id,
+                'image_url': '/web/image/product.product/%s/image_1024' % product.id if product.image_1920 else '/website/static/src/img/product_image_placeholder.svg',
+            })
 
         return product_data
-
 
 
 

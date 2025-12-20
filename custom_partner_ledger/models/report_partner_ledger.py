6e163f86b7400abf3f86fac8e117dbc4f1b69e8b
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-20 12:51:54 +0530

    odoomates

--- custom_partner_ledger/models/report_partner_ledger.py
+++ custom_partner_ledger/models/report_partner_ledger.py
@@ -33,7 +33,7 @@ class ReportPartnerLedgerCustom(models.AbstractModel):
                   tuple(data['computed']['account_ids'])] + query_get_data[2]
 
         query = """
-            SELECT "account_move_line".id, "account_move_line".date, j.code, acc.code as a_code, acc.name as a_name,
+          SELECT "account_move_line".id, "account_move_line".date, j.code, acc.display_name as a_code, acc.name as a_name,
                    "account_move_line".ref, m.name as move_name, "account_move_line".name,
                    "account_move_line".debit, "account_move_line".credit, "account_move_line".amount_currency,
                    "account_move_line".currency_id, c.symbol AS currency_code,

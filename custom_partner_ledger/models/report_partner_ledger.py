dad6cb2a5ec9d29efab1e5b9b61d5d94db5793cd
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-20 14:40:45 +0530

    odoomates

--- custom_partner_ledger/models/report_partner_ledger.py
+++ custom_partner_ledger/models/report_partner_ledger.py
@@ -341,6 +341,85 @@ class ReportPartnerLedgerCustom(models.AbstractModel):
     _inherit = 'report.accounting_pdf_reports.report_partnerledger'
     _description = 'Custom Partner Ledger Report'
 
+    # def _lines(self, data, partner):
+    #     """
+    #     Override the _lines method to filter by analytic accounts
+    #     """
+    #     full_account = []
+    #     currency = self.env['res.currency']
+    #     query_get_data = self.env['account.move.line'].with_context(
+    #         data['form'].get('used_context', {})
+    #     )._query_get()
+    #     # FIX: Use .get() with default value
+    #     reconcile_clause = "" if data['form'].get('reconciled',
+    #                                               False) else ' AND "account_move_line".full_reconcile_id IS NULL '
+    #
+    #     # Get analytic account filter
+    #     analytic_account_ids = data['form'].get('analytic_account_ids', [])
+    #     analytic_clause = ""
+    #     analytic_params = []
+    #
+    #     if analytic_account_ids:
+    #         analytic_clause = ' AND "account_move_line".analytic_distribution IS NOT NULL '
+    #
+    #     params = [partner.id, tuple(data['computed']['move_state']),
+    #               tuple(data['computed']['account_ids'])] + query_get_data[2]
+    #
+    #     query = """
+    #         SELECT "account_move_line".id, "account_move_line".date, j.code, acc.code as a_code, acc.name as a_name,
+    #                "account_move_line".ref, m.name as move_name, "account_move_line".name,
+    #                "account_move_line".debit, "account_move_line".credit, "account_move_line".amount_currency,
+    #                "account_move_line".currency_id, c.symbol AS currency_code,
+    #                "account_move_line".analytic_distribution
+    #         FROM """ + query_get_data[0] + """
+    #         LEFT JOIN account_journal j ON ("account_move_line".journal_id = j.id)
+    #         LEFT JOIN account_account acc ON ("account_move_line".account_id = acc.id)
+    #         LEFT JOIN res_currency c ON ("account_move_line".currency_id=c.id)
+    #         LEFT JOIN account_move m ON (m.id="account_move_line".move_id)
+    #         WHERE "account_move_line".partner_id = %s
+    #             AND m.state IN %s
+    #             AND "account_move_line".account_id IN %s AND """ + query_get_data[
+    #         1] + reconcile_clause + analytic_clause + """
+    #         ORDER BY "account_move_line".date
+    #     """
+    #
+    #     self.env.cr.execute(query, tuple(params))
+    #     res = self.env.cr.dictfetchall()
+    #
+    #     # Filter by analytic accounts if specified
+    #     if analytic_account_ids:
+    #         filtered_res = []
+    #         for line in res:
+    #             if line.get('analytic_distribution'):
+    #                 import json
+    #                 try:
+    #                     distribution = json.loads(line['analytic_distribution']) if isinstance(
+    #                         line['analytic_distribution'], str) else line['analytic_distribution']
+    #                     line_analytic_ids = [int(k) for k in distribution.keys()]
+    #                     if any(analytic_id in line_analytic_ids for analytic_id in analytic_account_ids):
+    #                         filtered_res.append(line)
+    #                 except:
+    #                     pass
+    #         res = filtered_res
+    #
+    #     sum_debit = sum_credit = 0.0
+    #     lang_code = self.env.context.get('lang') or 'en_US'
+    #     lang = self.env['res.lang']
+    #     lang_id = lang._lang_get(lang_code)
+    #     date_format = lang_id.date_format
+    #
+    #     for r in res:
+    #         sum_debit += r['debit']
+    #         sum_credit += r['credit']
+    #         r['progress'] = sum_debit - sum_credit
+    #         r['displayed_name'] = '-'.join(
+    #             r[field_name] for field_name in ('move_name', 'ref', 'name')
+    #             if r[field_name] not in (None, '', '/')
+    #         )
+    #         r['a_code_name'] = r['a_code'] + ' - ' + r['a_name']
+    #         full_account.append(r)
+    #
+    #     return full_account
     def _lines(self, data, partner):
         """
         Override the _lines method to filter by analytic accounts
@@ -350,14 +429,13 @@ class ReportPartnerLedgerCustom(models.AbstractModel):
         query_get_data = self.env['account.move.line'].with_context(
             data['form'].get('used_context', {})
         )._query_get()
-        # FIX: Use .get() with default value
+        # FIX: Use .get() with default value instead of direct access
         reconcile_clause = "" if data['form'].get('reconciled',
                                                   False) else ' AND "account_move_line".full_reconcile_id IS NULL '
 
         # Get analytic account filter
         analytic_account_ids = data['form'].get('analytic_account_ids', [])
         analytic_clause = ""
-        analytic_params = []
 
         if analytic_account_ids:
             analytic_clause = ' AND "account_move_line".analytic_distribution IS NOT NULL '
@@ -366,7 +444,7 @@ class ReportPartnerLedgerCustom(models.AbstractModel):
                   tuple(data['computed']['account_ids'])] + query_get_data[2]
 
         query = """
-            SELECT "account_move_line".id, "account_move_line".date, j.code, acc.code as a_code, acc.name as a_name, 
+            SELECT "account_move_line".id, "account_move_line".date, j.code, acc.name->>'en_US' as a_name, 
                    "account_move_line".ref, m.name as move_name, "account_move_line".name, 
                    "account_move_line".debit, "account_move_line".credit, "account_move_line".amount_currency,
                    "account_move_line".currency_id, c.symbol AS currency_code,
@@ -416,11 +494,10 @@ class ReportPartnerLedgerCustom(models.AbstractModel):
                 r[field_name] for field_name in ('move_name', 'ref', 'name')
                 if r[field_name] not in (None, '', '/')
             )
-            r['a_code_name'] = r['a_code'] + ' - ' + r['a_name']
+            # REMOVED: r['a_code_name'] = r['a_code'] + ' - ' + r['a_name']
             full_account.append(r)
 
         return full_account
-
     def _sum_partner(self, data, partner, field):
         """
         Override the _sum_partner method to filter by analytic accounts

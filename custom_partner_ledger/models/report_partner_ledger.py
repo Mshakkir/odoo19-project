0ab769989feefad35cfdf4029d84107501c42024
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-20 12:11:56 +0530

    odoomates

--- custom_partner_ledger/models/report_partner_ledger.py
+++ custom_partner_ledger/models/report_partner_ledger.py
@@ -24,7 +24,6 @@ class ReportPartnerLedgerCustom(models.AbstractModel):
         # Get analytic account filter
         analytic_account_ids = data['form'].get('analytic_account_ids', [])
         analytic_clause = ""
-        analytic_params = []
 
         if analytic_account_ids:
             # We'll filter by analytic_distribution in Python since it's a JSON field
@@ -161,79 +160,6 @@ class ReportPartnerLedgerCustom(models.AbstractModel):
         else:
             return debit - credit
 
-    def _get_partner_type_info(self, data, docs):
-        """
-        Determine what type of partners are in the report
-        Returns dict with display information
-        """
-        if not docs:
-            return {
-                'has_customers': False,
-                'has_vendors': False,
-                'display_label': 'Partners',
-                'count': 0
-            }
-
-        # Get account IDs from the data
-        account_ids = data.get('computed', {}).get('account_ids', [])
-
-        if not account_ids:
-            return {
-                'has_customers': False,
-                'has_vendors': False,
-                'display_label': 'Partners',
-                'count': len(docs)
-            }
-
-        # Get the accounts
-        accounts = self.env['account.account'].browse(account_ids)
-
-        # Check account types
-        has_receivable = any(acc.account_type == 'asset_receivable' for acc in accounts)
-        has_payable = any(acc.account_type == 'liability_payable' for acc in accounts)
-
-        # Determine display label
-        if has_receivable and has_payable:
-            display_label = 'Customers & Vendors'
-        elif has_receivable:
-            display_label = 'Customers'
-        elif has_payable:
-            display_label = 'Vendors'
-        else:
-            display_label = 'Partners'
-
-        return {
-            'has_customers': has_receivable,
-            'has_vendors': has_payable,
-            'display_label': display_label,
-            'count': len(docs)
-        }
-
-    def _get_partner_label(self, data, partner):
-        """
-        Get the appropriate label for a specific partner (Customer/Vendor)
-        """
-        # Get account IDs from the data
-        account_ids = data.get('computed', {}).get('account_ids', [])
-
-        if not account_ids:
-            return 'Partner'
-
-        # Get move lines for this partner within the selected accounts
-        move_lines = self.env['account.move.line'].search([
-            ('partner_id', '=', partner.id),
-            ('account_id', 'in', account_ids)
-        ], limit=1)
-
-        if move_lines:
-            account_type = move_lines.account_id.account_type
-            if account_type == 'asset_receivable':
-                return 'Customer'
-            elif account_type == 'liability_payable':
-                return 'Vendor'
-
-        return 'Partner'
-
     @api.model
     def _get_report_values(self, docids, data=None):
         """
@@ -246,94 +172,12 @@ class ReportPartnerLedgerCustom(models.AbstractModel):
         # Get parent report values
         res = super()._get_report_values(docids, data)
 
-        # Get partner type information
-        partner_type_info = self._get_partner_type_info(data, res.get('docs', []))
-
-        # Get analytic account names if filtered
+        # Add analytic account names if filtered (optional, for future use)
         analytic_names = []
         if data and data.get('form', {}).get('analytic_account_ids'):
             analytic_ids = data['form']['analytic_account_ids']
             analytic_accounts = self.env['account.analytic.account'].browse(analytic_ids)
             analytic_names = analytic_accounts.mapped('name')
-
-        # Add custom values to the report context
-        res.update({
-            'custom_title': 'Customized Partner Ledger',
-            'report_date': fields.Date.today(),
-            'partner_type_info': partner_type_info,
-            'get_partner_label': self._get_partner_label,
-            'analytic_account_names': analytic_names,
-        })
+            res['analytic_account_names'] = analytic_names
 
         return res
-
-    def _get_partner_opening_balance(self, data, partner):
-        """
-        Calculate opening balance for a partner
-        """
-        # Get analytic account filter
-        analytic_account_ids = data['form'].get('analytic_account_ids', [])
-        analytic_clause = ""
-
-        if analytic_account_ids:
-            analytic_clause = ' AND "account_move_line".analytic_distribution IS NOT NULL '
-
-        query_get_data = self.env['account.move.line'].with_context(
-            data['form'].get('used_context', {})
-        )._query_get()
-
-        # FIX: Use .get() with default value
-        reconcile_clause = "" if data['form'].get('reconciled', False) else \
-            ' AND "account_move_line".full_reconcile_id IS NULL '
-
-        date_from = data['form'].get('date_from')
-        date_clause = ""
-        params = [partner.id, tuple(data['computed']['move_state']),
-                  tuple(data['computed']['account_ids'])]
-
-        if date_from:
-            date_clause = ' AND "account_move_line".date < %s '
-            params.append(date_from)
-
-        params.extend(query_get_data[2])
-
-        query = """
-            SELECT
-                "account_move_line".id,
-                "account_move_line".analytic_distribution,
-                COALESCE(SUM(debit), 0.0) as total_debit,
-                COALESCE(SUM(credit), 0.0) as total_credit
-            FROM """ + query_get_data[0] + """, account_move AS m
-            WHERE "account_move_line".partner_id = %s
-                AND m.id = "account_move_line".move_id
-                AND m.state IN %s
-                AND account_id IN %s
-                """ + date_clause + """
-                AND """ + query_get_data[1] + reconcile_clause + analytic_clause + """
-            GROUP BY "account_move_line".id
-        """
-
-        self.env.cr.execute(query, tuple(params))
-        results = self.env.cr.dictfetchall()
-
-        # Filter by analytic accounts if specified
-        total_debit = total_credit = 0.0
-        if analytic_account_ids and results:
-            for line in results:
-                if line.get('analytic_distribution'):
-                    import json
-                    try:
-                        distribution = json.loads(line['analytic_distribution']) if isinstance(
-                            line['analytic_distribution'], str) else line['analytic_distribution']
-                        line_analytic_ids = [int(k) for k in distribution.keys()]
-                        if any(analytic_id in line_analytic_ids for analytic_id in analytic_account_ids):
-                            total_debit += line['total_debit']
-                            total_credit += line['total_credit']
-                    except:
-                        pass
-        else:
-            for line in results:
-                total_debit += line['total_debit']
-                total_credit += line['total_credit']
-
-        return total_debit - total_credit

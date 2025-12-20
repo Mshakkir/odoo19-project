e288a182fba8c2a2e21aa4bfd9ce6d8583704076
shakkir <shakkirtshakkirt86827@gmail.com>
2025-12-20 11:43:25 +0530

    custom

---
+++ custom_product_template/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'Custom Product Template',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Custom product page template for eCommerce',
+    'description': """
+        Custom module to inherit and customize the Products page
+        Displays products from inventory module
+    """,
+    'author': 'Your Name',
+    'website': 'https://www.yourwebsite.com',
+    'depends': [
+        'website',
+        'website_sale',
+        'product',
+        'stock',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'custom_product_template/static/src/scss/product_template.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}

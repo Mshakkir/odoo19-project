dc097c60769fafc71c8a41ea4387cdfa16bf0bb8
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-12-23 15:11:25 +0530

    odoomates

---
+++ product_location_column/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Product Location Column',
+    'version': '19.0.2.0.0',
+    'category': 'Inventory',
+    'summary': 'Add warehouse location columns to product list',
+    'depends': ['stock'],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}

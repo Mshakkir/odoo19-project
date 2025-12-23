4f8246a5f9a0cc43a5de56feec7a05dcc9070a94
shakkir <shakkirtshakkirt86827@gmail.com>
2025-12-23 12:43:31 +0530

    custom

---
+++ custom_contact_template/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Airmixs Custom Contact Page",
+    "version": "1.0.0",
+    "category": "Website",
+    "summary": "Custom Contact Us page",
+    "depends": ["website"],
+    "data": [
+        "views/contact_template.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "custom_contact_template/static/src/css/contactstyle.css",
+        ],
+    },
+    "installable": True,
+    "license": "LGPL-3",
+}

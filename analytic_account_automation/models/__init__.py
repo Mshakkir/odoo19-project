7abbff6ae3529457ce1c9c5490e498b68ed72e80
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-05 11:09:41 +0530

    odoomates

--- analytic_account_automation/models/__init__.py
+++ analytic_account_automation/models/__init__.py
@@ -1,4 +1,4 @@
-# from . import sale_order
-# from . import purchase_order
+from . import sale_order
+from . import purchase_order
 from . import account_move
 from . import account_payment
--- custom_auto_select_analytic_account/models/__init__.py
+++ custom_auto_select_analytic_account/models/__init__.py
@@ -1,2 +1,2 @@
-from . import sale_order_line
-from . import purchase_order_line
+# from . import sale_order_line
+# from . import purchase_order_line

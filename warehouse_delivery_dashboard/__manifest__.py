3c71b31c9d5c40d3b7eb4b8ddabfac92197c9f22
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-05 10:39:46 +0530

    odoomates

--- warehouse_delivery_dashboard/__manifest__.py
+++ warehouse_delivery_dashboard/__manifest__.py
@@ -12,6 +12,7 @@
     'website': 'https://www.yourcompany.com',
     'depends': ['stock', 'sale_stock', 'board'],
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/delivery_dashboard_views.xml',
         'views/stock_picking_views.xml',

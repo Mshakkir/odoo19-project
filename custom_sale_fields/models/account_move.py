edd41b932ad4413751b46a9721a01654e3772cf7
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-07 11:55:34 +0530

    odoomates

--- custom_sale_fields/models/account_move.py
+++ custom_sale_fields/models/account_move.py
@@ -1,3 +1,29 @@
+# from odoo import models, fields, api
+#
+#
+# class AccountMove(models.Model):
+#     _inherit = 'account.move'
+#
+#     customer_reference = fields.Char(
+#         string='PO Number',
+#         help='PO number or code',
+#         copy=False
+#     )
+#
+#     delivery_note_number = fields.Char(
+#         string='Delivery Note Number',
+#         help='Delivery note or dispatch number',
+#         copy=False
+#     )
+#
+#     @api.onchange('partner_id')
+#     def _onchange_partner_id_custom_invoice_fields(self):
+#         """Auto-populate customer reference if partner has default reference"""
+#         if self.partner_id and hasattr(self.partner_id, 'ref') and self.move_type in ['out_invoice', 'out_refund']:
+#             if not self.customer_reference:
+#                 self.customer_reference = self.partner_id.ref
+
+
 from odoo import models, fields, api
 
 
@@ -5,14 +31,20 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     customer_reference = fields.Char(
-        string='PO Number',
-        help='PO number or code',
+        string='Customer Reference',
+        help='Customer reference number or code',
         copy=False
     )
 
     delivery_note_number = fields.Char(
         string='Delivery Note Number',
         help='Delivery note or dispatch number',
+        copy=False
+    )
+
+    awb_number = fields.Char(
+        string='AWB Number',
+        help='Air Waybill Number',
         copy=False
     )
 
--- custom_sale_fields/models/sale_order.py
+++ custom_sale_fields/models/sale_order.py
@@ -1,3 +1,34 @@
+# from odoo import models, fields, api
+#
+#
+# class SaleOrder(models.Model):
+#     _inherit = 'sale.order'
+#
+#     customer_reference = fields.Char(
+#         string='PO Number',
+#         help='PO number or code',
+#         copy=False
+#     )
+#
+#     awb_number = fields.Char(
+#         string='AWB Number',
+#         help='Air Waybill Number',
+#         copy=False
+#     )
+#
+#     @api.onchange('partner_id')
+#     def _onchange_partner_id_custom_fields(self):
+#         """Auto-populate customer reference if partner has default reference"""
+#         if self.partner_id and hasattr(self.partner_id, 'ref'):
+#             self.customer_reference = self.partner_id.ref
+#
+#     def _prepare_invoice(self):
+#         """Override to pass customer reference to invoice"""
+#         invoice_vals = super()._prepare_invoice()
+#         if self.customer_reference:
+#             invoice_vals['customer_reference'] = self.customer_reference
+#         return invoice_vals
+
 from odoo import models, fields, api
 
 
@@ -5,8 +36,8 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     customer_reference = fields.Char(
-        string='PO Number',
-        help='PO number or code',
+        string='Customer Reference',
+        help='Customer reference number or code',
         copy=False
     )
 
@@ -23,8 +54,10 @@ class SaleOrder(models.Model):
             self.customer_reference = self.partner_id.ref
 
     def _prepare_invoice(self):
-        """Override to pass customer reference to invoice"""
+        """Override to pass customer reference and AWB to invoice"""
         invoice_vals = super()._prepare_invoice()
         if self.customer_reference:
             invoice_vals['customer_reference'] = self.customer_reference
+        if self.awb_number:
+            invoice_vals['awb_number'] = self.awb_number
         return invoice_vals

41f2cc0e5bbd77b141d688beb788901610d53db0
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-08 15:59:13 +0530

    odoomates

--- custom_invoice_so_link/__manifest__.py
+++ custom_invoice_so_link/__manifest__.py
@@ -7,6 +7,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/account_move_views.xml',
+        'views/sale_order_views.xml',
     ],
     'installable': True,
     'application': False,

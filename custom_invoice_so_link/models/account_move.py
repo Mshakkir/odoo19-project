61696c3f29c40d2a75db7fe8caeacaefc1e6ef12
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-08 14:29:54 +0530

    odoomates

--- custom_invoice_so_link/models/account_move.py
+++ custom_invoice_so_link/models/account_move.py
@@ -208,7 +208,7 @@ class AccountMove(models.Model):
                         'price_unit': line.price_unit,
                         'tax_ids': [(6, 0, line.tax_ids.ids)],
                         'sale_line_ids': [(6, 0, [line.id])],
-                        'product_uom_id': line.product_uom.id,
+                        'product_uom_id': line.product_uom_id.id,
                     }
 
                     account = line.product_id.property_account_income_id or \

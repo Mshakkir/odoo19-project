ed208771f48261a8f45e04a4f337b95982020a8c
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-09 11:18:37 +0530

    odoomates

--- sales_enhancement/__manifest__.py
+++ sales_enhancement/__manifest__.py
@@ -14,7 +14,7 @@
         'views/sale_order_views.xml',
         'views/account_move_views.xml',
         'views/stock_picking_views.xml',
-        "views/purchase_menu_views.xml"
+        "views/purchase_menu_views.xml",
         "views/sales_delivery_menu.xml"
     ],
     'installable': True,

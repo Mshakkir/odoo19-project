9a6a14d8fa3234c1478557194695ad6bd32e0575
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-09 15:40:45 +0530

    odoomates

---
+++ custom_invoice_filter/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Invoice Date Filter',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Add date range filter button for invoices',
+    'description': """
+        This module adds a custom date range filter button in invoice list view.
+        Users can filter invoices by selecting from and to dates.
+    """,
+    'author': 'Your Name',
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/invoice_date_filter_wizard_views.xml',
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}

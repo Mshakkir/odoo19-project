aa057ba91d7953eab5ea175ee95bca554781a15f
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-10 12:10:04 +0530

    odoomates

--- custom_invoice_view/models/account_move.py
+++ custom_invoice_view/models/account_move.py
@@ -33,24 +33,32 @@ class AccountMoveInherit(models.Model):
             shipping_ref = ''
 
             if move.invoice_origin:
-                # Try to get from sale order
-                sale_order = self.env['sale.order'].search([
-                    ('name', '=', move.invoice_origin)
-                ], limit=1)
+                # Try to get delivery orders related to this invoice
+                pickings = self.env['stock.picking'].search([
+                    ('origin', '=', move.invoice_origin),
+                    ('state', '=', 'done')
+                ], order='date_done desc', limit=1)
 
-                if sale_order:
-                    warehouse = sale_order.warehouse_id
+                if pickings:
+                    # Get warehouse from picking
+                    warehouse = pickings.location_id.warehouse_id or pickings.picking_type_id.warehouse_id
+                    delivery_note = pickings.name or ''
+                    # Shipping ref could be the tracking reference or picking name
+                    shipping_ref = pickings.carrier_tracking_ref or pickings.name or ''
+                else:
+                    # If no delivery, try to get from sale order
+                    sale_order = self.env['sale.order'].search([
+                        ('name', '=', move.invoice_origin)
+                    ], limit=1)
 
-                    # Get delivery orders related to this sale order
-                    pickings = self.env['stock.picking'].search([
-                        ('origin', '=', move.invoice_origin),
-                        ('state', '=', 'done')
-                    ], order='date_done desc', limit=1)
-
-                    if pickings:
-                        delivery_note = pickings.name
-                        # Shipping ref could be the picking reference or name
-                        shipping_ref = pickings.carrier_tracking_ref or pickings.name or ''
+                    if sale_order:
+                        # Try to get warehouse from sale order if it has the field
+                        if hasattr(sale_order, 'warehouse_id'):
+                            warehouse = sale_order.warehouse_id
+                        # Alternative: get from picking_ids
+                        elif sale_order.picking_ids:
+                            first_picking = sale_order.picking_ids[0]
+                            warehouse = first_picking.location_id.warehouse_id or first_picking.picking_type_id.warehouse_id
 
             move.warehouse_id = warehouse
             move.delivery_note_number = delivery_note

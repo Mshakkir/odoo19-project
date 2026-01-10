3ece6c449a7bb928550728613497a0e69c00b7cb
shakkir <shakkirtshakkirt86827@gmail.com>
2026-01-10 09:58:18 +0530

    custom

--- purchase_order_custom_report/__manifest__.py
+++ purchase_order_custom_report/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Custom Purchase Order Report',
     'version': '1.0',
-    'depends': ['purchase', 'base'],
+    'depends': ['purchase', 'base','custom_sa_quotation'],
     'data': [
         'report/report.xml',
         'report/report_purchase_order.xml',

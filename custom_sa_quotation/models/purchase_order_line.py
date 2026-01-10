c7b498b093e3ea151342fe1411dc8f60889a19d1
shakkir <shakkirtshakkirt86827@gmail.com>
2026-01-10 10:09:06 +0530

    custom

--- custom_sa_quotation/models/purchase_order_line.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class SaleOrderLine(models.Model):
-    _inherit = "purchase.order.line"
-
-    delivery_period = fields.Char(
-        string="ETA",
-        help="Example: 2-3 Weeks, 1 Week, Immediate"
-    )

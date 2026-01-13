47301c361dfdb772c040eaba23f59944034fb496
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-13 12:45:04 +0530

    odoomates

--- multi_warehouse_delivery/models/__init__.py
+++ multi_warehouse_delivery/models/__init__.py
@@ -1,4 +1,4 @@
 from . import sale_order
 from . import purchase_order
 from . import product_template
-# from . import account_move the warehouse field successfully displayed in the invoice tab that did not need now if any time need un comment this
+from . import account_move

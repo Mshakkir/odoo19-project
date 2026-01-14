126aeda66f7120f45dd4dc5665bd94472c7154f8
shakkir <shakkirtshakkirt86827@gmail.com>
2026-01-14 15:57:50 +0530

    custom

--- custom_asset_post/models/asset_depreciation_line.py
+++ custom_asset_post/models/asset_depreciation_line.py
@@ -4,9 +4,6 @@ class AssetDepreciationLine(models.Model):
     _inherit = "account.asset.depreciation.line"
 
     def action_post_depreciation(self):
-        """
-        Post single depreciation entry
-        """
         for line in self:
             if not line.move_id:
                 line.create_move()

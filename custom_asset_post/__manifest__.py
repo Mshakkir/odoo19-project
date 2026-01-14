76d1d9e3d261ec2fe1a718d44c58e94d541477f1
shakkir <shakkirtshakkirt86827@gmail.com>
2026-01-14 15:41:32 +0530

    custom

---
+++ custom_asset_post/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Asset Depreciation Post Button",
+    "version": "1.0",
+    "depends": [
+        "account",
+        "odoomates_accounting",  # keep this
+    ],
+    "data": [
+        "views/asset_depreciation_view.xml",
+    ],
+    "installable": True,
+}
---
+++ custom_asset_post/models/asset_depreciation_line.py
@@ -0,0 +1,36 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+class AccountAssetDepreciationLine(models.Model):
+    _inherit = "account.asset.depreciation.line"
+
+    def action_post_depreciation(self):
+        for line in self:
+            if line.move_id:
+                raise UserError("This depreciation is already posted.")
+
+            asset = line.asset_id
+            if not asset:
+                raise UserError("Asset not found.")
+
+            move = self.env["account.move"].create({
+                "date": line.depreciation_date,
+                "journal_id": asset.journal_id.id,
+                "line_ids": [
+                    (0, 0, {
+                        "name": asset.name,
+                        "account_id": asset.account_depreciation_expense_id.id,
+                        "debit": line.amount,
+                        "credit": 0.0,
+                    }),
+                    (0, 0, {
+                        "name": asset.name,
+                        "account_id": asset.account_depreciation_id.id,
+                        "debit": 0.0,
+                        "credit": line.amount,
+                    }),
+                ],
+            })
+
+            move.action_post()
+            line.move_id = move.id

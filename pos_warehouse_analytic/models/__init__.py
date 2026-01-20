aad53b20ee2ccef6714886f1b30cc2d980029ad5
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-20 16:31:26 +0530

    odoomates

--- pos_warehouse_analytic/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import pos_session
-from . import pos_config
-from . import pos_order
-from . import account_move
-# from . import pos_order_followers
-# from . import purchase_order_followers

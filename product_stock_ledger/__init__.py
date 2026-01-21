243a11659f8cf719a483a8b3be70e10043168639
shakkir <shakkirtshakkirt86827@gmail.com>
2026-01-21 14:25:07 +0530

    custom

--- product_stock_ledger/__init__.py
+++
@@ -1,3 +0,0 @@
-# product_stock_ledger/__init__.py
-from . import models
-from . import wizard
--- product_stock_ledger/models/__init__.py
+++
@@ -1,2 +0,0 @@
-# product_stock_ledger/controllers/__init__.py
-from . import product_stock_ledger_line

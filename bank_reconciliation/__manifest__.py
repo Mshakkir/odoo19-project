eb4ff8eced01bb73f24b4d64923a25abb6a8bbc8
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-22 14:41:53 +0530

    odoomates

--- bank_reconciliation/__manifest__.py
+++ bank_reconciliation/__manifest__.py
@@ -1,29 +1,3 @@
-# # -*- coding: utf-8 -*-
-# {
-#     'name': 'Manual Bank Reconciliation',
-#     'version': '19.0.1.0.0',
-#     'author': 'Cybrosys Techno Solutions',
-#     'company': 'Cybrosys Techno Solutions',
-#     'website': 'https://www.cybrosys.com',
-#     'category': 'Accounting',
-#     'summary': 'Traditional bank statement reconciliation method',
-#     'description': """Traditional bank statement reconciliation method for Odoo 19""",
-#     'depends': ['account'],
-#     'data': [
-#         'security/ir.model.access.csv',
-#         'data/sequence_data.xml',
-#         'views/account_move_line_view.xml',
-#         # 'views/account_journal_dashboard_view.xml',
-#         'views/bank_statement_wiz_view.xml',
-#     ],
-#     'assets': {},
-#     'images': ['static/description/banner.png'],
-#     'license': 'AGPL-3',
-#     'installable': True,
-#     'auto_install': False,
-#     'application': False,
-# }
-# -*- coding: utf-8 -*-
 {
     'name': 'Manual Bank Reconciliation',
     'version': '19.0.1.0.0',
@@ -41,7 +15,7 @@
         * Dashboard integration
         * Automatic overdue detection
     """,
-    'depends': ['account', 'point_of_sale'],
+    'depends': ['account'],
     'data': [
         'security/ir.model.access.csv',
         'data/sequence_data.xml',

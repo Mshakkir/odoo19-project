3dc3ea8c93b8f3fe1c836dbd85e11f60577720b0
shakkir <shakkirtshakkirt86827@gmail.com>
2026-01-22 16:33:32 +0530

    custom

--- payment_sequence_custom/models/account_journal.py
+++ payment_sequence_custom/models/account_journal.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class AccountJournal(models.Model):
@@ -22,14 +22,31 @@ class AccountJournal(models.Model):
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
+    @api.model
     def _get_sequence(self):
         """Override to use custom sequences based on payment type"""
-        self.ensure_one()
-        journal = self.journal_id
+        # Get the payment from the context or use self
+        payment = self if self else self.env['account.payment']
 
-        if self.payment_type == 'inbound' and journal.inbound_payment_sequence_id:
-            return journal.inbound_payment_sequence_id
-        elif self.payment_type == 'outbound' and journal.outbound_payment_sequence_id:
-            return journal.outbound_payment_sequence_id
+        if hasattr(payment, 'payment_type') and hasattr(payment, 'journal_id'):
+            journal = payment.journal_id
+
+            if payment.payment_type == 'inbound' and journal.inbound_payment_sequence_id:
+                return journal.inbound_payment_sequence_id
+            elif payment.payment_type == 'outbound' and journal.outbound_payment_sequence_id:
+                return journal.outbound_payment_sequence_id
 
         return super()._get_sequence()
+
+    def _post(self):
+        """Override _post to assign sequence from custom sequences"""
+        for payment in self:
+            if not payment.name or payment.name == '/':
+                journal = payment.journal_id
+
+                if payment.payment_type == 'inbound' and journal.inbound_payment_sequence_id:
+                    payment.name = journal.inbound_payment_sequence_id.next_by_id()
+                elif payment.payment_type == 'outbound' and journal.outbound_payment_sequence_id:
+                    payment.name = journal.outbound_payment_sequence_id.next_by_id()
+
+        return super()._post()

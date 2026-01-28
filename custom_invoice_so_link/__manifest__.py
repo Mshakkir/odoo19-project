b6474963f26da8f63dfa51febffcf16480b2200c
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-28 12:33:22 +0530

    odoomates

---
+++ custom_invoice_so_link/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Invoice Sales Order Link',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Link invoices to sales orders with customer filtering',
+    'depends': ['sale_management', 'account'],  # adjust 'account' if using odoo mates module
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}

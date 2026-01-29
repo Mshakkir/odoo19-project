10b037bbdba1b16c10b2708b3b3567b23290d78e
shakkir <shakkirtshakkirt86827@gmail.com>
2026-01-29 15:13:29 +0530

    custom

--- payment_bank_sequence/models/account_payment.py
+++ payment_bank_sequence/models/account_payment.py
@@ -1,64 +1,102 @@
-from odoo import models, fields, api
+from odoo import models, api, _
+from odoo.exceptions import UserError
 
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    def _get_sequence(self):
-        """Override to use bank-specific sequences"""
+    def _get_bank_specific_sequence(self):
+        """Get bank-specific sequence based on journal"""
         self.ensure_one()
 
-        # Get the journal (bank account)
         journal = self.journal_id
+        if not journal:
+            return None
 
-        # Determine sequence based on payment type and bank
+        journal_name = journal.name.upper()
+        journal_code = journal.code.upper()
+
+        # Detect bank type
+        is_snb = 'SNB' in journal_name or 'SNB' in journal_code
+        is_rajhi = 'RAJHI' in journal_name or 'RAJHI' in journal_code
+
+        # Determine sequence code
+        sequence_code = None
         if self.payment_type == 'inbound':  # Customer Payment
-            # Check which bank journal is used
-            if 'SNB' in journal.name.upper() or 'SNB' in journal.code.upper():
+            if is_snb:
                 sequence_code = 'account.payment.customer.snb'
-            elif 'RAJHI' in journal.name.upper() or 'RAJHI' in journal.code.upper():
+            elif is_rajhi:
                 sequence_code = 'account.payment.customer.rajhi'
-            else:
-                sequence_code = 'account.payment.customer'
-
         elif self.payment_type == 'outbound':  # Vendor Payment
-            # Check which bank journal is used
-            if 'SNB' in journal.name.upper() or 'SNB' in journal.code.upper():
+            if is_snb:
                 sequence_code = 'account.payment.supplier.snb'
-            elif 'RAJHI' in journal.name.upper() or 'RAJHI' in journal.code.upper():
+            elif is_rajhi:
                 sequence_code = 'account.payment.supplier.rajhi'
-            else:
-                sequence_code = 'account.payment.supplier'
-        else:
-            return super()._get_sequence()
 
-        # Get or create the sequence
-        return self.env['ir.sequence'].sudo().search([
+        if not sequence_code:
+            return None
+
+        # Find sequence
+        sequence = self.env['ir.sequence'].sudo().search([
             ('code', '=', sequence_code),
-            ('company_id', 'in', [self.company_id.id, False])
+            '|',
+            ('company_id', '=', self.company_id.id),
+            ('company_id', '=', False)
         ], limit=1)
+
+        return sequence
 
     @api.model_create_multi
     def create(self, vals_list):
-        """Override create to set custom sequence number"""
+        """Override create to set bank-specific sequence number"""
+
+        # First, create records without triggering default sequence
+        for vals in vals_list:
+            # If no name is set or it's '/', we'll set our custom sequence
+            if not vals.get('name') or vals.get('name') == '/':
+                vals['name'] = '/'  # Temporarily set to avoid default sequence
+
+        # Create the payments
         payments = super().create(vals_list)
 
+        # Now apply custom sequences
         for payment in payments:
-            if payment.name and payment.name != '/':
-                continue
+            if payment.name == '/' or not payment.name:
+                sequence = payment._get_bank_specific_sequence()
 
-            sequence = payment._get_sequence()
-            if sequence:
-                payment.name = sequence._next()
+                if sequence:
+                    # Use our custom sequence
+                    new_name = sequence._next()
+                    payment.sudo().write({'name': new_name})
+                # If no custom sequence found, let default sequence apply
+                elif payment.name == '/':
+                    # Get default sequence
+                    if payment.payment_type == 'inbound':
+                        seq_code = 'account.payment.customer'
+                    elif payment.payment_type == 'outbound':
+                        seq_code = 'account.payment.supplier'
+                    else:
+                        seq_code = 'account.payment.transfer'
+
+                    default_seq = self.env['ir.sequence'].search([
+                        ('code', '=', seq_code)
+                    ], limit=1)
+
+                    if default_seq:
+                        payment.sudo().write({'name': default_seq._next()})
 
         return payments
 
+    def write(self, vals):
+        """Override write to handle sequence on state change"""
+        result = super().write(vals)
 
-class AccountJournal(models.Model):
-    _inherit = 'account.journal'
+        # If state is being set and name is still '/', apply sequence
+        if vals.get('state') and not vals.get('name'):
+            for payment in self:
+                if payment.name == '/' or not payment.name:
+                    sequence = payment._get_bank_specific_sequence()
+                    if sequence:
+                        payment.sudo().write({'name': sequence._next()})
 
-    payment_sequence_id = fields.Many2one(
-        'ir.sequence',
-        string='Payment Sequence',
-        help='Specific sequence for this bank journal'
-    )
+        return result

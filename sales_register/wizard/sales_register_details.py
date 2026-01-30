8f0a7edd70f488bd3037d9dae024bfcb159ec590
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-01-30 12:17:10 +0530

    odoomates

--- sales_register/wizard/sales_register_details.py
+++ sales_register/wizard/sales_register_details.py
@@ -5,7 +5,6 @@ class SalesRegisterDetails(models.TransientModel):
     _name = 'sales.register.details'
     _description = 'Sales Register Details View'
 
-
     wizard_id = fields.Many2one('sales.register.wizard', string='Wizard Reference')
     date = fields.Date(string='Date')
     document_type = fields.Char(string='Type')
@@ -22,7 +21,7 @@ class SalesRegisterDetails(models.TransientModel):
     addin_cost = fields.Float(string='Additional Cost', digits='Product Price')
     taxes = fields.Char(string='Tax')
     tax_amount = fields.Float(string='Tax Amount', digits='Product Price')
-    invoice_cash_rounding_id= fields.Float(string='Round Off', digits='Product Price')
+    round_off = fields.Float(string='Round Off', digits='Product Price')
     total = fields.Float(string='Total', digits='Product Price')
     paid = fields.Float(string='Received', digits='Product Price')
     balance = fields.Float(string='Balance', digits='Product Price')

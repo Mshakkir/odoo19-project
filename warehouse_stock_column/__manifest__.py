1d17947136741e88b5fd753ae3045995f67a53b5
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-02 12:43:28 +0530

    custom

--- warehouse_stock_column/__manifest__.py
+++ warehouse_stock_column/__manifest__.py
@@ -1,3 +1,16 @@
+# {
+#     'name': 'Product Location Column',
+#     'version': '19.0.2.0.0',
+#     'category': 'Inventory',
+#     'summary': 'Add warehouse location columns to product list',
+#     'depends': ['stock'],
+#     'data': [
+#         'views/product_template.xml',
+#     ],
+#     'installable': True,
+#     'application': False,
+#     'license': 'LGPL-3',
+# }
 {
     'name': 'Product Location Column',
     'version': '19.0.2.0.0',

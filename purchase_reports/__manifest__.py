a3a96fc2d5c3e7ff3495b932cc1e8730530febb2
shakkir <shakkirtshakkirt86827@gmail.com>
2026-02-06 10:16:19 +0530

    custom

--- purchase_reports/__manifest__.py
+++ purchase_reports/__manifest__.py
@@ -5,9 +5,9 @@
     'depends': ['purchase', 'stock'],
     'data': [
         'security/ir.model.access.csv',
+        'wizard/purchase_report_by_product_wizard.xml',  # Load wizard FIRST (contains action)
         'views/purchase_report_views.xml',
-        'views/purchase_report_menus.xml',
-        'wizard/purchase_report_by_product_wizard.xml',
+        'views/purchase_report_menus.xml',  # Load menu LAST (references action)
     ],
     'installable': True,
     'application': False,

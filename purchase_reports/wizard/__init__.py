e81e973071bd8ea906873f9a2b9ae8b7c20f6eca
shakkir <shakkirtshakkirt86827@gmail.com>
2026-02-06 10:01:47 +0530

    custom

--- purchase_reports/wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import purchase_report_by_vouchers_wizard
-from . import purchase_report_by_product_wizard
-from . import purchase_report_by_category_wizard
-from . import purchase_report_by_product_group_wizard
-from . import purchase_report_by_party_wizard
-from . import purchase_report_by_party_group_wizard
-from . import purchase_report_by_warehouse_wizard
-from . import purchase_report_by_salesman_wizard

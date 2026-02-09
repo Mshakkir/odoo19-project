964af3226bc00014765eeca73ca4f1f401633c0b
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-09 16:25:57 +0530

    custom

--- product_purchase_history/models/account_move_line.py
+++ product_purchase_history/models/account_move_line.py
@@ -1,44 +1,5 @@
-# # -*- coding: utf-8 -*-
-# from odoo import api, fields, models
-#
-#
-# class AccountMoveLine(models.Model):
-#     _inherit = 'account.move.line'
-#
-#     @api.model
-#     def get_product_purchase_history(self, product_id):
-#         """
-#         Fetch purchase history for a given product
-#         Returns list of purchase order lines with supplier info
-#         """
-#         if not product_id:
-#             return []
-#
-#         # Search for purchase order lines with this product
-#         purchase_lines = self.env['purchase.order.line'].search([
-#             ('product_id', '=', product_id),
-#             ('order_id.state', 'in', ['purchase', 'done'])
-#         ], order='date_order desc', limit=50)
-#
-#         history = []
-#         for line in purchase_lines:
-#             history.append({
-#                 'id': line.id,
-#                 'order_name': line.order_id.name or '',
-#                 'partner_name': line.order_id.partner_id.name if line.order_id.partner_id else '',
-#                 'date_order': line.date_order.strftime('%Y-%m-%d') if line.date_order else '',
-#                 'product_qty': line.product_qty,
-#                 'product_uom': line.product_uom_id.name if line.product_uom_id else '',  # Changed from product_uom
-#                 'price_unit': line.price_unit,
-#                 'price_subtotal': line.price_subtotal,
-#                 'currency': line.currency_id.symbol if line.currency_id else '',  # Changed from order_id.currency_id
-#                 'state': line.order_id.state,
-#             })
-#
-#         return history
-
 # -*- coding: utf-8 -*-
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class AccountMoveLine(models.Model):
@@ -47,65 +8,32 @@ class AccountMoveLine(models.Model):
     @api.model
     def get_product_purchase_history(self, product_id):
         """
-        Fetch purchase history from:
-        1) Purchase Orders
-        2) Direct Vendor Bills (Purchase Invoices)
+        Fetch purchase history for a given product
+        Returns list of purchase order lines with supplier info
         """
         if not product_id:
             return []
 
-        history = []
-
-        # --------------------------------------------------
-        # 1️⃣ PURCHASE ORDER LINES
-        # --------------------------------------------------
-        po_lines = self.env['purchase.order.line'].search([
+        # Search for purchase order lines with this product
+        purchase_lines = self.env['purchase.order.line'].search([
             ('product_id', '=', product_id),
             ('order_id.state', 'in', ['purchase', 'done'])
-        ], order='order_id.date_order desc', limit=50)
+        ], order='date_order desc', limit=50)
 
-        for line in po_lines:
+        history = []
+        for line in purchase_lines:
             history.append({
-                'id': f'po_{line.id}',
-                'source': 'PO',
-                'reference': line.order_id.name or '',
-                'partner_name': line.order_id.partner_id.name or '',
-                'date': line.order_id.date_order.strftime('%Y-%m-%d') if line.order_id.date_order else '',
-                'qty': line.product_qty,
-                'uom': line.product_uom_id.name or '',
+                'id': line.id,
+                'order_name': line.order_id.name or '',
+                'partner_name': line.order_id.partner_id.name if line.order_id.partner_id else '',
+                'date_order': line.date_order.strftime('%Y-%m-%d') if line.date_order else '',
+                'product_qty': line.product_qty,
+                'product_uom': line.product_uom_id.name if line.product_uom_id else '',  # Changed from product_uom
                 'price_unit': line.price_unit,
-                'subtotal': line.price_subtotal,
-                'currency': line.currency_id.symbol or '',
+                'price_subtotal': line.price_subtotal,
+                'currency': line.currency_id.symbol if line.currency_id else '',  # Changed from order_id.currency_id
                 'state': line.order_id.state,
             })
 
-        # --------------------------------------------------
-        # 2️⃣ DIRECT VENDOR BILLS (PURCHASE INVOICES)
-        # --------------------------------------------------
-        bill_lines = self.env['account.move.line'].search([
-            ('product_id', '=', product_id),
-            ('move_id.move_type', '=', 'in_invoice'),
-            ('move_id.state', '=', 'posted'),
-        ], order='move_id.invoice_date desc', limit=50)
-
-        for line in bill_lines:
-            history.append({
-                'id': f'bill_{line.id}',
-                'source': 'BILL',
-                'reference': line.move_id.name or '',
-                'partner_name': line.move_id.partner_id.name or '',
-                'date': line.move_id.invoice_date.strftime('%Y-%m-%d') if line.move_id.invoice_date else '',
-                'qty': line.quantity,
-                'uom': line.product_uom_id.name or '',
-                'price_unit': line.price_unit,
-                'subtotal': line.price_subtotal,
-                'currency': line.currency_id.symbol or '',
-                'state': 'posted',
-            })
-
-        # --------------------------------------------------
-        # Sort by date (latest first)
-        # --------------------------------------------------
-        history.sort(key=lambda x: x['date'] or '', reverse=True)
-
         return history
+
--- product_purchase_history/static/src/js/purchase_history_dialog.js
+++ product_purchase_history/static/src/js/purchase_history_dialog.js
@@ -1,39 +1,3 @@
-///** @odoo-module **/
-//
-//import { Component } from "@odoo/owl";
-//import { Dialog } from "@web/core/dialog/dialog";
-//import { _t } from "@web/core/l10n/translation";
-//
-//export class PurchaseHistoryDialog extends Component {
-//    static template = "product_purchase_history.PurchaseHistoryDialog";
-//    static components = { Dialog };
-//    static props = {
-//        close: Function,
-//        productName: String,
-//        purchaseHistory: Array,
-//    };
-//
-//    get hasHistory() {
-//        return this.props.purchaseHistory && this.props.purchaseHistory.length > 0;
-//    }
-//
-//    get title() {
-//        return _t("Purchase History - ") + this.props.productName;
-//    }
-//
-//    formatNumber(value, decimals = 2) {
-//        if (value === null || value === undefined) return '0.00';
-//        return parseFloat(value).toFixed(decimals);
-//    }
-//
-//    getStateLabel(state) {
-//        const stateLabels = {
-//            'purchase': _t('Purchase Order'),
-//            'done': _t('Done'),
-//        };
-//        return stateLabels[state] || state;
-//    }
-//}
 /** @odoo-module **/
 
 import { Component } from "@odoo/owl";
@@ -50,7 +14,7 @@ export class PurchaseHistoryDialog extends Component {
     };
 
     get hasHistory() {
-        return this.props.purchaseHistory?.length > 0;
+        return this.props.purchaseHistory && this.props.purchaseHistory.length > 0;
     }
 
     get title() {
@@ -58,12 +22,16 @@ export class PurchaseHistoryDialog extends Component {
     }
 
     formatNumber(value, decimals = 2) {
-        return value ? parseFloat(value).toFixed(decimals) : '0.00';
+        if (value === null || value === undefined) return '0.00';
+        return parseFloat(value).toFixed(decimals);
     }
 
-    getSourceLabel(source) {
-        return source === 'PO'
-            ? _t('Purchase Order')
-            : _t('Vendor Bill');
+    getStateLabel(state) {
+        const stateLabels = {
+            'purchase': _t('Purchase Order'),
+            'done': _t('Done'),
+        };
+        return stateLabels[state] || state;
     }
 }
+

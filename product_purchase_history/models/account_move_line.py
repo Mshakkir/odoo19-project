8219a3a019c9688a5c58c6a2ceee1693a88c4527
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-09 15:48:34 +0530

    custom

--- product_purchase_history/models/account_move_line.py
+++ product_purchase_history/models/account_move_line.py
@@ -15,30 +15,24 @@ class AccountMoveLine(models.Model):
             return []
 
         # Search for purchase order lines with this product
-        # Note: state is on purchase.order, not purchase.order.line
         purchase_lines = self.env['purchase.order.line'].search([
             ('product_id', '=', product_id),
-            ('order_id.state', 'in', ['purchase', 'done'])  # Order state
-        ], order='date_order desc', limit=50)  # date_order works directly in order clause
+            ('order_id.state', 'in', ['purchase', 'done'])
+        ], order='date_order desc', limit=50)
 
         history = []
         for line in purchase_lines:
-            # Safely get the date
-            date_str = ''
-            if line.date_order:
-                date_str = line.date_order.strftime('%Y-%m-%d')
-
             history.append({
                 'id': line.id,
                 'order_name': line.order_id.name or '',
                 'partner_name': line.order_id.partner_id.name if line.order_id.partner_id else '',
-                'date_order': date_str,
+                'date_order': line.date_order.strftime('%Y-%m-%d') if line.date_order else '',
                 'product_qty': line.product_qty,
-                'product_uom': line.product_uom.name if line.product_uom else '',
+                'product_uom': line.product_uom_id.name if line.product_uom_id else '',  # Changed from product_uom
                 'price_unit': line.price_unit,
                 'price_subtotal': line.price_subtotal,
-                'currency': line.order_id.currency_id.symbol if line.order_id.currency_id else '',
-                'state': line.order_id.state,  # Get state from order
+                'currency': line.currency_id.symbol if line.currency_id else '',  # Changed from order_id.currency_id
+                'state': line.order_id.state,
             })
 
         return history

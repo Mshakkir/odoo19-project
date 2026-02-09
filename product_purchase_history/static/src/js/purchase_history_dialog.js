1d0d0e69bbb8901399d22e6a873ecf12be159ed6
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-09 16:22:47 +0530

    custom

--- product_purchase_history/static/src/js/purchase_history_dialog.js
+++ product_purchase_history/static/src/js/purchase_history_dialog.js
@@ -1,3 +1,39 @@
+///** @odoo-module **/
+//
+//import { Component } from "@odoo/owl";
+//import { Dialog } from "@web/core/dialog/dialog";
+//import { _t } from "@web/core/l10n/translation";
+//
+//export class PurchaseHistoryDialog extends Component {
+//    static template = "product_purchase_history.PurchaseHistoryDialog";
+//    static components = { Dialog };
+//    static props = {
+//        close: Function,
+//        productName: String,
+//        purchaseHistory: Array,
+//    };
+//
+//    get hasHistory() {
+//        return this.props.purchaseHistory && this.props.purchaseHistory.length > 0;
+//    }
+//
+//    get title() {
+//        return _t("Purchase History - ") + this.props.productName;
+//    }
+//
+//    formatNumber(value, decimals = 2) {
+//        if (value === null || value === undefined) return '0.00';
+//        return parseFloat(value).toFixed(decimals);
+//    }
+//
+//    getStateLabel(state) {
+//        const stateLabels = {
+//            'purchase': _t('Purchase Order'),
+//            'done': _t('Done'),
+//        };
+//        return stateLabels[state] || state;
+//    }
+//}
 /** @odoo-module **/
 
 import { Component } from "@odoo/owl";
@@ -14,7 +50,7 @@ export class PurchaseHistoryDialog extends Component {
     };
 
     get hasHistory() {
-        return this.props.purchaseHistory && this.props.purchaseHistory.length > 0;
+        return this.props.purchaseHistory?.length > 0;
     }
 
     get title() {
@@ -22,15 +58,12 @@ export class PurchaseHistoryDialog extends Component {
     }
 
     formatNumber(value, decimals = 2) {
-        if (value === null || value === undefined) return '0.00';
-        return parseFloat(value).toFixed(decimals);
+        return value ? parseFloat(value).toFixed(decimals) : '0.00';
     }
 
-    getStateLabel(state) {
-        const stateLabels = {
-            'purchase': _t('Purchase Order'),
-            'done': _t('Done'),
-        };
-        return stateLabels[state] || state;
+    getSourceLabel(source) {
+        return source === 'PO'
+            ? _t('Purchase Order')
+            : _t('Vendor Bill');
     }
 }

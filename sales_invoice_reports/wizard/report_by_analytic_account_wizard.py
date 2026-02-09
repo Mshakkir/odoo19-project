46af6330266f5eed2546c89c3081aadc028e7db5
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-09 10:15:03 +0530

    custom

---
+++ sales_invoice_reports/wizard/report_by_analytic_account_wizard.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ReportByAnalyticAccountWizard(models.TransientModel):
+    _name = 'report.by.analytic.account.wizard'
+    _description = 'Report by Analytic Account Wizard'
+
+    show_all_analytic_accounts = fields.Boolean(string='All Analytic Accounts', default=False)
+    analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account')
+    date_from = fields.Date(string='Date From')
+    date_to = fields.Date(string='Date To', default=fields.Date.today)
+    invoice_state = fields.Selection([
+        ('draft', 'Draft'),
+        ('posted', 'Posted'),
+        ('cancel', 'Cancelled'),
+        ('all', 'All')
+    ], string='Invoice Status', default='all')
+
+    @api.onchange('show_all_analytic_accounts')
+    def _onchange_show_all_analytic_accounts(self):
+        """Clear analytic account selection when showing all"""
+        if self.show_all_analytic_accounts:
+            self.analytic_account_id = False
+
+    def action_apply(self):
+        """Apply filter and show analytic account report"""
+        self.ensure_one()
+
+        # Build domain for the report model
+        domain = []
+
+        # Only filter by analytic account if not showing all
+        if not self.show_all_analytic_accounts:
+            if not self.analytic_account_id:
+                return {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': 'Warning',
+                        'message': 'Please select an analytic account or check "All Analytic Accounts"',
+                        'type': 'warning',
+                        'sticky': False,
+                    }
+                }
+            domain.append(('analytic_account_id', '=', self.analytic_account_id.id))
+
+        # Only filter by state if not 'all'
+        if self.invoice_state and self.invoice_state != 'all':
+            domain.append(('invoice_state', '=', self.invoice_state))
+
+        # Only add date filters if they are set
+        if self.date_from:
+            domain.append(('invoice_date', '>=', self.date_from))
+        if self.date_to:
+            domain.append(('invoice_date', '<=', self.date_to))
+
+        # Set report name
+        if self.show_all_analytic_accounts:
+            report_name = 'Sales Report - All Analytic Accounts'
+        else:
+            report_name = f'Sales Report - {self.analytic_account_id.display_name}'
+
+        return {
+            'name': report_name,
+            'type': 'ir.actions.act_window',
+            'res_model': 'analytic.account.invoice.report',
+            'view_mode': 'list,pivot,graph',
+            'domain': domain,
+            'context': {'search_default_posted': 1},
+            'target': 'current',
+        }
---
+++ sales_invoice_reports/wizard/report_by_invoice_number_wizard.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ReportByInvoiceNumberWizard(models.TransientModel):
+    _name = 'report.by.invoice.number.wizard'
+    _description = 'Report by Invoice Number Wizard'
+
+    show_all_invoices = fields.Boolean(string='All Invoices', default=False)
+    invoice_id = fields.Many2one('account.move', string='Invoice',
+                                  domain="[('move_type', 'in', ['out_invoice', 'out_refund'])]")
+    date_from = fields.Date(string='Date From')
+    date_to = fields.Date(string='Date To', default=fields.Date.today)
+    invoice_state = fields.Selection([
+        ('draft', 'Draft'),
+        ('posted', 'Posted'),
+        ('cancel', 'Cancelled'),
+        ('all', 'All')
+    ], string='Invoice Status', default='all')
+
+    @api.onchange('show_all_invoices')
+    def _onchange_show_all_invoices(self):
+        """Clear invoice selection when showing all"""
+        if self.show_all_invoices:
+            self.invoice_id = False
+
+    def action_apply(self):
+        """Apply filter and show invoice report"""
+        self.ensure_one()
+
+        # Build domain for the report model
+        domain = []
+
+        # Only filter by invoice if not showing all
+        if not self.show_all_invoices:
+            if not self.invoice_id:
+                return {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': 'Warning',
+                        'message': 'Please select an invoice or check "All Invoices"',
+                        'type': 'warning',
+                        'sticky': False,
+                    }
+                }
+            domain.append(('invoice_id', '=', self.invoice_id.id))
+
+        # Only filter by state if not 'all'
+        if self.invoice_state and self.invoice_state != 'all':
+            domain.append(('invoice_state', '=', self.invoice_state))
+
+        # Only add date filters if they are set
+        if self.date_from:
+            domain.append(('invoice_date', '>=', self.date_from))
+        if self.date_to:
+            domain.append(('invoice_date', '<=', self.date_to))
+
+        # Set report name
+        if self.show_all_invoices:
+            report_name = 'Sales Report - All Invoices'
+        else:
+            report_name = f'Sales Report - {self.invoice_id.name}'
+
+        return {
+            'name': report_name,
+            'type': 'ir.actions.act_window',
+            'res_model': 'invoice.number.report',
+            'view_mode': 'list,pivot,graph',
+            'domain': domain,
+            'context': {'search_default_posted': 1},
+            'target': 'current',
+        }

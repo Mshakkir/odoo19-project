9ddc0f1c665b341eed848b2785f7fc17d467e712
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-10 10:05:02 +0530

    custom

--- product_purchase_history/models/account_move_line.py
+++ product_purchase_history/models/account_move_line.py
@@ -1,3 +1,42 @@
+# # -*- coding: utf-8 -*-
+# from odoo import api, fields, models
+#
+#
+# class AccountMoveLine(models.Model):
+#     _inherit = 'account.move.line'
+#
+#     @api.model
+#     def get_product_purchase_history(self, product_id):
+#         """
+#         Fetch purchase history for a given product
+#         Returns list of purchase order lines with supplier info
+#         """
+#         if not product_id:
+#             return []
+#
+#         # Search for purchase order lines with this product
+#         purchase_lines = self.env['purchase.order.line'].search([
+#             ('product_id', '=', product_id),
+#             ('order_id.state', 'in', ['purchase', 'done'])
+#         ], order='date_order desc', limit=50)
+#
+#         history = []
+#         for line in purchase_lines:
+#             history.append({
+#                 'id': line.id,
+#                 'order_name': line.order_id.name or '',
+#                 'partner_name': line.order_id.partner_id.name if line.order_id.partner_id else '',
+#                 'date_order': line.date_order.strftime('%Y-%m-%d') if line.date_order else '',
+#                 'product_qty': line.product_qty,
+#                 'product_uom': line.product_uom_id.name if line.product_uom_id else '',  # Changed from product_uom
+#                 'price_unit': line.price_unit,
+#                 'price_subtotal': line.price_subtotal,
+#                 'currency': line.currency_id.symbol if line.currency_id else '',  # Changed from order_id.currency_id
+#                 'state': line.order_id.state,
+#             })
+#
+#         return history
+
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
@@ -8,31 +47,33 @@ class AccountMoveLine(models.Model):
     @api.model
     def get_product_purchase_history(self, product_id):
         """
-        Fetch purchase history for a given product
-        Returns list of purchase order lines with supplier info
+        Fetch purchase history for a given product from vendor bills
+        Returns list of invoice lines with supplier info
         """
         if not product_id:
             return []
 
-        # Search for purchase order lines with this product
-        purchase_lines = self.env['purchase.order.line'].search([
+        # Search for vendor bill lines with this product
+        invoice_lines = self.env['account.move.line'].search([
             ('product_id', '=', product_id),
-            ('order_id.state', 'in', ['purchase', 'done'])
-        ], order='date_order desc', limit=50)
+            ('move_id.move_type', '=', 'in_invoice'),
+            ('move_id.state', '=', 'posted'),
+            ('display_type', '=', False),
+        ], order='move_id.invoice_date desc', limit=50)
 
         history = []
-        for line in purchase_lines:
+        for line in invoice_lines:
             history.append({
                 'id': line.id,
-                'order_name': line.order_id.name or '',
-                'partner_name': line.order_id.partner_id.name if line.order_id.partner_id else '',
-                'date_order': line.date_order.strftime('%Y-%m-%d') if line.date_order else '',
-                'product_qty': line.product_qty,
-                'product_uom': line.product_uom_id.name if line.product_uom_id else '',  # Changed from product_uom
+                'order_name': line.move_id.name or '',
+                'partner_name': line.move_id.partner_id.name if line.move_id.partner_id else '',
+                'date_order': line.move_id.invoice_date.strftime('%Y-%m-%d') if line.move_id.invoice_date else '',
+                'product_qty': line.quantity,
+                'product_uom': line.product_uom_id.name if line.product_uom_id else '',
                 'price_unit': line.price_unit,
                 'price_subtotal': line.price_subtotal,
-                'currency': line.currency_id.symbol if line.currency_id else '',  # Changed from order_id.currency_id
-                'state': line.order_id.state,
+                'currency': line.currency_id.symbol if line.currency_id else '',
+                'state': 'posted',
             })
 
         return history

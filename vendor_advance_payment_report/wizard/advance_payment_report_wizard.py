adf2a4faabf41b77cf768c7d0ec1fa5008abe24c
shakkir <shakkirtshakkirt86827@gmail.com>
2026-02-12 15:39:13 +0530

    custom

--- vendor_advance_payment_report/wizard/advance_payment_report_wizard.py
+++ vendor_advance_payment_report/wizard/advance_payment_report_wizard.py
@@ -25,7 +25,7 @@ class AdvancePaymentReportWizard(models.TransientModel):
     journal_id = fields.Many2one(
         'account.journal',
         string='Journal',
-        domain="[('type', 'in', ['bank', 'cash'])]"
+        domain="[('type', 'in', ['bank', 'cash','Credit Card'])]"
     )
     all_vendors = fields.Boolean(
         string='All Vendors',

8a2e08d766d30d43242924c13dc905093339a79e
shakkir <shakkirtshakkirt86827@gmail.com>
2026-02-14 12:22:42 +0530

    custom

--- customer_advance_receipt_report/models/customer_advance_receipt_report.py
+++ customer_advance_receipt_report/models/customer_advance_receipt_report.py
@@ -15,3 +15,47 @@ class CustomerAdvanceReceiptReport(models.TransientModel):
     customer_name = fields.Char(string='Customer Name', required=True)
     amount = fields.Float(string='Amount', required=True)
     currency_id = fields.Many2one('res.currency', string='Currency')
+    payment_id = fields.Many2one('account.payment', string='Payment', required=True)
+
+    def action_open_payment(self):
+        """Open the payment record"""
+        self.ensure_one()
+        return {
+            'name': 'Payment Receipt',
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.payment',
+            'res_id': self.payment_id.id,
+            'view_mode': 'form',
+            'target': 'current',
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # -*- coding: utf-8 -*-
+#
+# from odoo import models, fields, api
+#
+#
+# class CustomerAdvanceReceiptReport(models.TransientModel):
+#     _name = 'customer.advance.receipt.report'
+#     _description = 'Customer Advance Receipt Report'
+#     _order = 'date desc, receipt_number desc'
+#
+#     date = fields.Date(string='Date', required=True)
+#     receipt_number = fields.Char(string='Receipt Number', required=True)
+#     journal_name = fields.Char(string='Journal', required=True)
+#     payment_method = fields.Char(string='Payment Method', required=True)
+#     customer_name = fields.Char(string='Customer Name', required=True)
+#     amount = fields.Float(string='Amount', required=True)
+#     currency_id = fields.Many2one('res.currency', string='Currency')
--- vendor_advance_payment_report/models/advance_payment_report.py
+++ vendor_advance_payment_report/models/advance_payment_report.py
@@ -28,29 +28,3 @@ class AdvancePaymentReport(models.TransientModel):
             'view_mode': 'form',
             'target': 'current',
         }
-
-
-
-
-
-
-
-
-
-# # -*- coding: utf-8 -*-
-#
-# from odoo import models, fields, api
-#
-#
-# class AdvancePaymentReport(models.TransientModel):
-#     _name = 'advance.payment.report'
-#     _description = 'Advance Payment Report'
-#     _order = 'date desc, receipt_number desc'
-#
-#     date = fields.Date(string='Date', required=True)
-#     receipt_number = fields.Char(string='Receipt Number', required=True)
-#     journal_name = fields.Char(string='Journal', required=True)
-#     payment_method = fields.Char(string='Payment Method', required=True)
-#     vendor_name = fields.Char(string='Vendor Name', required=True)
-#     amount = fields.Float(string='Amount', required=True)
-#     currency_id = fields.Many2one('res.currency', string='Currency')

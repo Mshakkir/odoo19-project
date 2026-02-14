b36c0cadddf14fc8967d45f352cfe1dc28975aec
shakkir <shakkirtshakkirt86827@gmail.com>
2026-02-14 12:26:00 +0530

    custom

--- customer_advance_receipt_report/models/customer_advance_receipt_report.py
+++ customer_advance_receipt_report/models/customer_advance_receipt_report.py
@@ -29,33 +29,3 @@ class CustomerAdvanceReceiptReport(models.TransientModel):
             'target': 'current',
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # -*- coding: utf-8 -*-
-#
-# from odoo import models, fields, api
-#
-#
-# class CustomerAdvanceReceiptReport(models.TransientModel):
-#     _name = 'customer.advance.receipt.report'
-#     _description = 'Customer Advance Receipt Report'
-#     _order = 'date desc, receipt_number desc'
-#
-#     date = fields.Date(string='Date', required=True)
-#     receipt_number = fields.Char(string='Receipt Number', required=True)
-#     journal_name = fields.Char(string='Journal', required=True)
-#     payment_method = fields.Char(string='Payment Method', required=True)
-#     customer_name = fields.Char(string='Customer Name', required=True)
-#     amount = fields.Float(string='Amount', required=True)
-#     currency_id = fields.Many2one('res.currency', string='Currency')

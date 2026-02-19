8518f79360461a2efada0b9b40c0fd091934023c
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-19 15:27:47 +0530

    custom

--- rack_list/__manifest__.py
+++ rack_list/__manifest__.py
@@ -12,10 +12,15 @@
     'depends': ['sale_management', 'stock'],
     'data': [
         'security/ir.model.access.csv',
-        'views/rack_list_assets.xml',
         'views/rack_list_views.xml',
         'views/rack_list_menus.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'rack_list/static/src/js/rack_list_controller.js',
+            'rack_list/static/src/xml/rack_list_templates.xml',
+        ],
+    },
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',

45df36c296a03489fd9940d7901785994e03a3e6
shakkir <shakkirtshakkirt86827@gmail.com>
2026-02-19 14:21:11 +0530

    custom

--- warehouse_stock_column/__manifest__.py
+++ warehouse_stock_column/__manifest__.py
@@ -1,12 +1,19 @@
 {
     'name': 'Product Location Column',
-    'version': '19.0.2.0.0',
+    'version': '19.0.3.0.0',
     'category': 'Inventory',
-    'summary': 'Add warehouse location columns to product list',
-    'depends': ['stock'],
+    'summary': 'Add warehouse location columns + stock filter bar to product list',
+    'depends': ['stock', 'web'],
     'data': [
         'views/product_template.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'warehouse_stock_column/static/src/css/stock_filter_bar.css',
+            'warehouse_stock_column/static/src/components/product_stock_filter.js',
+            'warehouse_stock_column/static/src/components/product_stock_filter.xml',
+        ],
+    },
     'installable': True,
     'application': False,
     'license': 'LGPL-3',

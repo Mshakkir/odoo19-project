bb28581bc7c19fdf88c58b17063e1ef556c04273
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-02-20 16:46:15 +0530

    custom

--- delivery_invoice_batch/models/stock_picking.py
+++ delivery_invoice_batch/models/stock_picking.py
@@ -1,15 +1,69 @@
-import logging
+# import logging
+# from odoo import models, fields, api, _
+# from odoo.exceptions import UserError
+#
+# _logger = logging.getLogger(__name__)
+# _logger.info("DEBUG delivery_invoice_batch: stock_picking.py module-level code executing")
+#
+#
+# class StockPicking(models.Model):
+#     _inherit = 'stock.picking'
+#
+#     _logger.info("DEBUG delivery_invoice_batch: StockPicking class body executing")
+#
+#     invoice_ids = fields.Many2many(
+#         'account.move',
+#         string='Invoices',
+#         compute='_compute_invoice_ids',
+#         store=False,
+#     )
+#     invoice_count = fields.Integer(
+#         string='Invoice Count',
+#         compute='_compute_invoice_ids',
+#         store=False,
+#     )
+#
+#     @api.depends('name')
+#     def _compute_invoice_ids(self):
+#         for picking in self:
+#             invoices = self.env['account.move'].search([
+#                 ('invoice_origin', 'ilike', picking.name)
+#             ])
+#             picking.invoice_ids = invoices
+#             picking.invoice_count = len(invoices)
+#
+#     def action_create_invoice(self):
+#         _logger.info(
+#             "DEBUG delivery_invoice_batch: action_create_invoice called on pickings: %s",
+#             self.ids
+#         )
+#         return {
+#             'name': _('Create Invoices'),
+#             'type': 'ir.actions.act_window',
+#             'res_model': 'stock.picking.invoice.wizard',
+#             'view_mode': 'form',
+#             'target': 'new',
+#             'context': {
+#                 'active_ids': self.ids,
+#                 'active_model': 'stock.picking',
+#             }
+#         }
+#
+#     def action_view_invoices(self):
+#         self.ensure_one()
+#         return {
+#             'name': _('Invoices'),
+#             'type': 'ir.actions.act_window',
+#             'res_model': 'account.move',
+#             'view_mode': 'list,form',
+#             'domain': [('id', 'in', self.invoice_ids.ids)],
+#         }
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-
-_logger = logging.getLogger(__name__)
-_logger.info("DEBUG delivery_invoice_batch: stock_picking.py module-level code executing")
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-
-    _logger.info("DEBUG delivery_invoice_batch: StockPicking class body executing")
 
     invoice_ids = fields.Many2many(
         'account.move',
@@ -33,10 +87,7 @@ class StockPicking(models.Model):
             picking.invoice_count = len(invoices)
 
     def action_create_invoice(self):
-        _logger.info(
-            "DEBUG delivery_invoice_batch: action_create_invoice called on pickings: %s",
-            self.ids
-        )
+        """Open wizard to create invoice from delivery notes"""
         return {
             'name': _('Create Invoices'),
             'type': 'ir.actions.act_window',
@@ -50,6 +101,7 @@ class StockPicking(models.Model):
         }
 
     def action_view_invoices(self):
+        """View invoices related to this delivery note"""
         self.ensure_one()
         return {
             'name': _('Invoices'),
--- delivery_invoice_batch/wizard/stock_picking_invoice.py
+++ delivery_invoice_batch/wizard/stock_picking_invoice.py
@@ -1,9 +1,108 @@
-import logging
+# import logging
+# from odoo import models, fields, api, _
+# from odoo.exceptions import UserError
+#
+# _logger = logging.getLogger(__name__)
+# _logger.info("DEBUG delivery_invoice_batch: stock_picking_invoice.py module-level code executing")
+#
+#
+# class StockPickingInvoiceWizard(models.TransientModel):
+#     _name = 'stock.picking.invoice.wizard'
+#     _description = 'Create Invoice from Delivery Notes'
+#
+#     partner_id = fields.Many2one('res.partner', string='Customer', readonly=True)
+#     picking_ids = fields.Many2many('stock.picking', string='Delivery Notes')
+#     invoice_date = fields.Date(string='Invoice Date', default=fields.Date.context_today)
+#     journal_id = fields.Many2one('account.journal', string='Journal',
+#                                  domain=[('type', '=', 'sale')])
+#
+#     @api.model
+#     def default_get(self, fields_list):
+#         res = super().default_get(fields_list)
+#         active_ids = self.env.context.get('active_ids', [])
+#         pickings = self.env['stock.picking'].browse(active_ids)
+#
+#         if not pickings:
+#             raise UserError(_('No delivery notes selected.'))
+#
+#         not_done = pickings.filtered(lambda p: p.state != 'done')
+#         if not_done:
+#             raise UserError(_('All delivery notes must be in Done state.'))
+#
+#         partners = pickings.mapped('partner_id')
+#         if len(partners) > 1:
+#             raise UserError(_('All delivery notes must belong to the same customer.'))
+#
+#         not_outgoing = pickings.filtered(lambda p: p.picking_type_code != 'outgoing')
+#         if not_outgoing:
+#             raise UserError(_('Only outgoing delivery orders can be invoiced.'))
+#
+#         res['partner_id'] = partners[0].id if partners else False
+#         res['picking_ids'] = [(6, 0, active_ids)]
+#
+#         journal = self.env['account.journal'].search([('type', '=', 'sale')], limit=1)
+#         res['journal_id'] = journal.id if journal else False
+#
+#         return res
+#
+#     def action_create_invoice(self):
+#         self.ensure_one()
+#
+#         picking_names = self.picking_ids.mapped('name')
+#         delivery_note_value = ', '.join(picking_names)
+#
+#         _logger.info(
+#             "DEBUG delivery_invoice_batch: Creating invoice for pickings: %s, "
+#             "delivery_note_number will be: %s",
+#             picking_names, delivery_note_value
+#         )
+#
+#         invoice_vals = {
+#             'move_type': 'out_invoice',
+#             'partner_id': self.partner_id.id,
+#             'invoice_date': self.invoice_date,
+#             'invoice_origin': delivery_note_value,
+#             'journal_id': self.journal_id.id,
+#             'delivery_note_number': delivery_note_value,
+#             'invoice_line_ids': [],
+#         }
+#
+#         for picking in self.picking_ids:
+#             moves = picking.move_ids if hasattr(picking, 'move_ids') else picking.move_lines
+#
+#             for move in moves:
+#                 if move.state == 'done':
+#                     price_unit = move.product_id.list_price or move.product_id.standard_price
+#                     line_vals = {
+#                         'product_id': move.product_id.id,
+#                         'name': move.product_id.display_name,
+#                         'quantity': move.quantity,
+#                         'product_uom_id': move.product_uom.id,
+#                         'price_unit': price_unit,
+#                         'tax_ids': [(6, 0, move.product_id.taxes_id.ids)],
+#                     }
+#                     invoice_vals['invoice_line_ids'].append((0, 0, line_vals))
+#
+#         if not invoice_vals['invoice_line_ids']:
+#             raise UserError(_('No products found in the selected delivery notes to invoice.'))
+#
+#         invoice = self.env['account.move'].create(invoice_vals)
+#
+#         _logger.info(
+#             "DEBUG delivery_invoice_batch: Invoice created: %s, delivery_note_number: %s",
+#             invoice.name, invoice.delivery_note_number
+#         )
+#
+#         return {
+#             'name': _('Invoice'),
+#             'type': 'ir.actions.act_window',
+#             'res_model': 'account.move',
+#             'res_id': invoice.id,
+#             'view_mode': 'form',
+#             'views': [(False, 'form')],
+#         }
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-
-_logger = logging.getLogger(__name__)
-_logger.info("DEBUG delivery_invoice_batch: stock_picking_invoice.py module-level code executing")
 
 
 class StockPickingInvoiceWizard(models.TransientModel):
@@ -51,12 +150,6 @@ class StockPickingInvoiceWizard(models.TransientModel):
         picking_names = self.picking_ids.mapped('name')
         delivery_note_value = ', '.join(picking_names)
 
-        _logger.info(
-            "DEBUG delivery_invoice_batch: Creating invoice for pickings: %s, "
-            "delivery_note_number will be: %s",
-            picking_names, delivery_note_value
-        )
-
         invoice_vals = {
             'move_type': 'out_invoice',
             'partner_id': self.partner_id.id,
@@ -87,11 +180,6 @@ class StockPickingInvoiceWizard(models.TransientModel):
             raise UserError(_('No products found in the selected delivery notes to invoice.'))
 
         invoice = self.env['account.move'].create(invoice_vals)
-
-        _logger.info(
-            "DEBUG delivery_invoice_batch: Invoice created: %s, delivery_note_number: %s",
-            invoice.name, invoice.delivery_note_number
-        )
 
         return {
             'name': _('Invoice'),

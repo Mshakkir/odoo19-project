a67a1c0ba4ef7fb1ab607bee4c9f0472fecadc8a
shakkir <shakkirtshakkirt86827@gmail.com>
2026-03-03 13:19:14 +0530

    custom

--- custom_delivery_note/__manifest__.py
+++ custom_delivery_note/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Custom Delivery Note",
     "version": "1.0",
-    "depends": ["stock", "sale_stock", "delivery_note_add_po"],
+    "depends": ["stock", "sale_stock"],
     "data": [
         "report/report_action.xml",
         "report/report_delivery_note_custom.xml",

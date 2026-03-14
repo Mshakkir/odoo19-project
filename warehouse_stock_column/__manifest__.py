66e1713d847fe1d4b7124e3b0ac49c19772e5008
shakkir <shakkirtshakkirt86827@gmail.com>
2026-03-14 16:40:35 +0530

    custom

--- warehouse_stock_column/__manifest__.py
+++ warehouse_stock_column/__manifest__.py
@@ -1,26 +1,10 @@
-# {
-#     'name': 'Product Location Column',
-#     'version': '19.0.2.0.0',
-#     'category': 'Inventory',
-#     'summary': 'Add warehouse location columns to product list',
-#     'depends': ['stock'],
-#     'data': [
-#         'views/product_template.xml',
-#     ],
-#     'installable': True,
-#     'application': False,
-#     'license': 'LGPL-3',
-# }
-
-
 {
     'name': 'Product Location Column',
-    'version': '19.0.3.0.0',
+    'version': '19.0.2.0.0',
     'category': 'Inventory',
     'summary': 'Add warehouse location columns to product list',
     'depends': ['stock'],
     'data': [
-        'security/groups.xml',
         'views/product_template.xml',
     ],
     'installable': True,

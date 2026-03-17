62e33e304791d04306c4868befec062d6f9024ba
shakkir <shakkirtshakkirt86827@gmail.com>
2026-03-17 18:29:27 +0530

    custom

--- product_created_date/models/product_template.py
+++ product_created_date/models/product_template.py
@@ -35,28 +35,3 @@ class ProductTemplate(models.Model):
             if not vals.get('x_created_date'):
                 vals['x_created_date'] = today
         return super().create(vals_list)
-
-
-
-
-# from odoo import models, fields, api
-#
-#
-# class ProductTemplate(models.Model):
-#     _inherit = 'product.template'
-#
-#     x_created_date = fields.Date(
-#         string='Created Date',
-#         readonly=True,
-#         copy=False,
-#         default=fields.Date.today,
-#         help='Date when this product was created. Auto-filled on creation.',
-#     )
-#
-#     @api.model_create_multi
-#     def create(self, vals_list):
-#         today = fields.Date.context_today(self)
-#         for vals in vals_list:
-#             if not vals.get('x_created_date'):
-#                 vals['x_created_date'] = today
-#         return super().create(vals_list)

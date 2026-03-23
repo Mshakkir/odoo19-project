727fe623f2c4796a2de11cb950ac8882867d2fc7
shakkir <shakkirtshakkirt86827@gmail.com>
2026-03-23 15:34:16 +0530

    custom

--- purchase_order_awb/models/purchase_order_custom.py
+++ purchase_order_awb/models/purchase_order_custom.py
@@ -19,19 +19,3 @@ class PurchaseOrder(models.Model):
 
     def _get_own_company_partner_id(self):
         return self.env.company.partner_id.id
-
-
-
-
-
-# from odoo import models, fields, api
-#
-# class PurchaseOrder(models.Model):
-#     _inherit = 'purchase.order'
-#
-#     awb_number = fields.Char(
-#         string='Shipping Ref #',
-#         help='Air Waybill Number for tracking shipment',
-#         copy=False,
-#         readonly=False
-#     )

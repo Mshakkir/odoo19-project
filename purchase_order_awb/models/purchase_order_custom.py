b0afd750d8a71d4e0d2cb68c5c42668a2340a504
shakkir <shakkirtshakkirt86827@gmail.com>
2026-03-23 15:00:48 +0530

    custom

--- purchase_order_awb/models/purchase_order_custom.py
+++ purchase_order_awb/models/purchase_order_custom.py
@@ -9,3 +9,27 @@ class PurchaseOrder(models.Model):
         copy=False,
         readonly=False
     )
+
+    delivery_address_id = fields.Many2one(
+        'res.partner',
+        string='Delivery Address',
+        help='Delivery address for this purchase order',
+        domain="[('type', 'in', ['delivery', 'other', 'contact']), ('company_id', '=', company_id)]",
+        copy=True,
+    )
+
+
+
+
+
+# from odoo import models, fields, api
+#
+# class PurchaseOrder(models.Model):
+#     _inherit = 'purchase.order'
+#
+#     awb_number = fields.Char(
+#         string='Shipping Ref #',
+#         help='Air Waybill Number for tracking shipment',
+#         copy=False,
+#         readonly=False
+#     )

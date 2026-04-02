d99fa153be0ba21afb140c8debc75daee5d9d18f
shakkir <shakkirtshakkirt86827@gmail.com>
2026-04-02 11:08:08 +0530

    custom

--- payment_customization/models/account_payment.py
+++ payment_customization/models/account_payment.py
@@ -46,31 +46,3 @@ class AccountPayment(models.Model):
         else:
             # No partner selected: revert to company currency
             self.currency_id = self.company_id.currency_id
-
-
-
-
-
-
-
-# # -*- coding: utf-8 -*-
-#
-# from odoo import fields, models
-#
-#
-# class AccountPayment(models.Model):
-#     _inherit = 'account.payment'
-#
-#     # Add new memo field
-#     memo_new = fields.Text(
-#         string='Memo',
-#         help='Additional memo or notes for this payment'
-#     )
-#
-#     # Add advance payment checkbox
-#     is_advance_payment = fields.Boolean(
-#         string='Advance Payment',
-#         default=False,
-#         help='Check this box if this is an advance payment'
-#     )
-#

b245974abf37b803a1c7dc3656ee2614e7e24e33
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-13 15:37:46 +0530

    custom

--- custom_partner_ledger/__manifest__.py
+++ custom_partner_ledger/__manifest__.py
@@ -47,7 +47,7 @@
         'accounting_pdf_reports',
     ],
     'data': [
-        'security/ir_model_access.csv',
+        # 'security/ir_model_access.csv',
         'reports/report_partner_ledger_template.xml',
         'views/partner_ledger_wizard_view.xml',
         'views/partner_ledger_detail_view.xml',

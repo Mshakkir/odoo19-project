19e9324a169881b70f39b7ce6cb0f53ad58f1dda
jayaraj-kpkp <jayarajkpkp@gmail.com>
2026-04-17 16:31:30 +0530

    custom

--- stock_account_valuation_report/wizards/stock_valuation_history.py
+++ stock_account_valuation_report/wizards/stock_valuation_history.py
@@ -1,51 +1,3 @@
-# # Copyright 2019 Eficent Business and IT Consulting Services, S.L.
-# # Copyright 2019 Aleph Objects, Inc.
-# # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-#
-# import ast
-#
-# from odoo import fields, models
-# from odoo.osv import expression
-#
-#
-# class StockValuationHistory(models.TransientModel):
-#     _name = "stock.valuation.history"
-#     _description = "Stock Valuation History"
-#     _rec_name = "inventory_datetime"
-#
-#     inventory_datetime = fields.Datetime(
-#         "Dual Valuation at Date",
-#         help="Choose a date to get the valuation at that date",
-#         default=fields.Datetime.now,
-#     )
-#
-#     def open_at_date(self):
-#         action = self.env["ir.actions.act_window"]._for_xml_id(
-#             "stock_account_valuation_report.product_valuation_action"
-#         )
-#         domain = [("type", "=", "consu")]
-#         product_id = self.env.context.get("product_id", False)
-#         product_tmpl_id = self.env.context.get("product_tmpl_id", False)
-#         if product_id:
-#             domain = expression.AND([domain, [("id", "=", product_id)]])
-#         elif product_tmpl_id:
-#             domain = expression.AND(
-#                 [domain, [("product_tmpl_id", "=", product_tmpl_id)]]
-#             )
-#         action["domain"] = domain
-#         if self.inventory_datetime:
-#             # Use ast.literal_eval instead of deprecated safe_eval for simple dicts
-#             action_context = ast.literal_eval(action["context"])
-#             action_context["at_date"] = self.inventory_datetime
-#             action["context"] = str(action_context)
-#         return action
-# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
-# Copyright 2019 Aleph Objects, Inc.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
-# Copyright 2019 Aleph Objects, Inc.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 # Copyright 2019 Eficent Business and IT Consulting Services, S.L.
 # Copyright 2019 Aleph Objects, Inc.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -68,41 +20,32 @@ class StockValuationHistory(models.TransientModel):
     )
 
     def open_at_date(self):
-        """Open the SVL list filtered up to inventory_datetime."""
-        # Reference our own action – no dependency on external XML IDs
         action = self.env["ir.actions.act_window"]._for_xml_id(
-            "stock_account_valuation_report.svl_action"
+            "stock_account_valuation_report.product_valuation_action"
         )
-
-        ctx = {}
-        if action.get("context"):
-            try:
-                ctx = ast.literal_eval(action["context"])
-            except Exception:
-                ctx = {}
-
-        if self.inventory_datetime:
-            ctx["at_date"] = self.inventory_datetime
-
-        action["context"] = str(ctx)
-
-        # Build domain
-        domain = []
+        domain = [("type", "=", "consu")]
         product_id = self.env.context.get("product_id", False)
         product_tmpl_id = self.env.context.get("product_tmpl_id", False)
         if product_id:
-            domain = expression.AND([domain, [("product_id", "=", product_id)]])
+            domain = expression.AND([domain, [("id", "=", product_id)]])
         elif product_tmpl_id:
             domain = expression.AND(
-                [domain, [("product_id.product_tmpl_id", "=", product_tmpl_id)]]
+                [domain, [("product_tmpl_id", "=", product_tmpl_id)]]
             )
-
+        action["domain"] = domain
         if self.inventory_datetime:
-            domain = expression.AND(
-                [domain, [("create_date", "<=", self.inventory_datetime)]]
-            )
-
-        if domain:
-            action["domain"] = domain
-
+            # Use ast.literal_eval instead of deprecated safe_eval for simple dicts
+            action_context = ast.literal_eval(action["context"])
+            action_context["at_date"] = self.inventory_datetime
+            action["context"] = str(action_context)
         return action
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 Aleph Objects, Inc.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 Aleph Objects, Inc.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+#
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 Aleph Objects, Inc.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).

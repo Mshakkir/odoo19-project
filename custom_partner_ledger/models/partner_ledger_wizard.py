5dbf97bd6158df481edd14562a1e0c810e518c3a
shakkir <shakkirtshakkirt86827@gmail.com>
2026-04-23 10:34:58 +0530

    custom

--- custom_partner_ledger/models/partner_ledger_wizard.py
+++ custom_partner_ledger/models/partner_ledger_wizard.py
@@ -6,6 +6,19 @@ class AccountPartnerLedgerCustom(models.TransientModel):
     _inherit = "account.report.partner.ledger"
     _description = "Custom Account Partner Ledger"
 
+    show_po_number = fields.Boolean(
+        string='Show PO Number Column',
+        default=False,
+        help="If enabled, the PO Number column will be printed in the report.",
+    )
+
+    def check_report(self):
+        res = super().check_report()
+        # Inject show_po_number into the form data so the template can read it
+        if res.get('data') and res['data'].get('form') is not None:
+            res['data']['form']['show_po_number'] = self.show_po_number
+        return res
+
     def action_show_details(self):
         """
         Show partner ledger details in a dedicated list view
@@ -41,3 +54,59 @@ class AccountPartnerLedgerCustom(models.TransientModel):
             },
             'target': 'current',
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # -*- coding: utf-8 -*-
+# from odoo import fields, models, api, _
+#
+#
+# class AccountPartnerLedgerCustom(models.TransientModel):
+#     _inherit = "account.report.partner.ledger"
+#     _description = "Custom Account Partner Ledger"
+#
+#     def action_show_details(self):
+#         """
+#         Show partner ledger details in a dedicated list view
+#         """
+#         self.ensure_one()
+#
+#         # Prepare wizard data
+#         wizard_data = {
+#             'date_from': self.date_from,
+#             'date_to': self.date_to,
+#             'journal_ids': self.journal_ids.ids if self.journal_ids else [],
+#             'target_move': self.target_move,
+#             'result_selection': self.result_selection,
+#             'partner_ids': self.partner_ids.ids if self.partner_ids else [],
+#             'amount_currency': self.amount_currency,
+#             'reconciled': self.reconciled,
+#         }
+#
+#         # Generate detail records
+#         detail_model = self.env['partner.ledger.detail']
+#         detail_model.get_partner_ledger_details(wizard_data)
+#
+#         # Return action to open detail view
+#         return {
+#             'name': _('Partner Ledger Details'),
+#             'type': 'ir.actions.act_window',
+#             'res_model': 'partner.ledger.detail',
+#             'view_mode': 'list',
+#             'views': [(self.env.ref('custom_partner_ledger.view_partner_ledger_detail_tree').id, 'list')],
+#             'search_view_id': [self.env.ref('custom_partner_ledger.view_partner_ledger_detail_search').id],
+#             'context': {
+#                 'search_default_group_by_partner': 1,
+#             },
+#             'target': 'current',
+#         }
